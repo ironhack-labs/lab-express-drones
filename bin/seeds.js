9d3e177d1ef9a391382dda66f79f841894277f22
Juan Romero <jrg89@hotmail.es>
2020-07-04 11:18:49 +0200

    iteration 1 in progress

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,16 @@
+const Drone = require("../models/Drone.model");
+
 // Iteration #1
+const drones = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+  ];
+
+  Drone.deleteMany({})
+    .then(() => {
+        Drone.insertMany(drones)
+        .then(() => console.log('Todo bien'))
+        .catch((error) => error )})
+
+
