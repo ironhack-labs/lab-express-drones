2f52a8c143da1560b009fe482c90a4a12980492b
Irma Blanco <irblancofdz@gmail.com>
2020-07-04 11:39:13 +0200

    Db config and drones routes

--- configs/db.config.js
+++ configs/db.config.js
@@ -10,3 +10,10 @@ mongoose
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
   .catch(err => console.error('Error connecting to mongo', err));
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
--- routes/drones.js
+++ routes/drones.js
@@ -1,10 +1,22 @@
 const express = require('express');
 
 // require the Drone model here
-
+const Drone = require('../models/Drones.model')
 const router = express.Router();
 
+/**
+ * Find the /drones GET route in routes/drones.js.
+Use the Mongoose .find() method to retrieve all the drones. Display all the drones on the drones/list.hbs view. Make sure you catch the error and output it to the terminal.
+In the drones/list.hbs file, use a forEach loop to display tags with each drone's name, propellers, and speed.
+Add the link that goes to /drones route in the layout.hbs file to easier navigate to the list of drones.
+ */
+
+
 router.get('/drones', (req, res, next) => {
+  Drone.find({})
+    .then((dronesFromDB) => {
+      res.render('/drones/list', dronesFromDB)
+    })
   // Iteration #2: List the drones
   // ... your code here
 });
