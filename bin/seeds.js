878446a2360cc462c09f17849cb13cb78534e4df
Jaime Baro <jaimebaroval@gmail.com>
2020-07-04 11:13:21 +0200

    Interaction 1

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,20 @@
 // Iteration #1
+
+// require database configuration
+require('../configs/db.config');
+
+// Import Schema
+const Drone = require('../models/Drones.model')
+
+const drones = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.deleteMany({})
+    .then(() => Drone.create(drones))
+    .then((dronesFromDb) => {
+        console.log('Insert to BBDD', dronesFromDb)
+    })
+    .catch(error => console.log('An error happened while insert drones:', error));
