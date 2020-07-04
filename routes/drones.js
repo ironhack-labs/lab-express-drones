639ab60462c0748c5c7ed7b41b2815f4b54379a0
mateomp <mateomadariaga96@gmail.com>
2020-07-04 12:03:04 +0200

    Iteration 3 completed

--- routes/drones.js
+++ routes/drones.js
@@ -15,11 +15,14 @@ router.get('/drones', (req, res, next) => {
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render("create-form")
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  const reqBody = req.body;
+  Drone.create(reqBody)
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
