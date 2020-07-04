c71b5ff0500200f6fc7222af1929b412319bdffb
Old <onelifedesigning@gmail.com>
2020-07-04 11:38:09 +0200

    iteration 1 add seeds

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,19 @@
 // Iteration #1
+require('../configs/db.config')
+
+const Drone = require('../models/Drone.model')
+const mongoose = require('mongoose');
+
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.deleteMany({})
+  .then(Drone.insertMany(drones))
+  .then(() => {
+    console.log('Created registers correctly')
+    mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))
