523df393ca10afcdeb3b61299858c548e5518658
Andres Martinez <andresmartgonz@gmail.com>
2020-07-04 12:01:17 +0200

    interaction 2

--- routes/drones.js
+++ routes/drones.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express')
 
 // require the Drone model here
 const Drone = require('../models/Drones.model')
-const router = express.Router();
+const router = express.Router()
 
 /**
  * Find the /drones GET route in routes/drones.js.
@@ -11,39 +11,37 @@ In the drones/list.hbs file, use a forEach loop to display tags with each drone'
 Add the link that goes to /drones route in the layout.hbs file to easier navigate to the list of drones.
  */
 
-
 router.get('/drones', (req, res, next) => {
-  Drone.find({})
-    .then((dronesFromDB) => {
-      res.render('/drones/list', dronesFromDB)
-    })
+  Drone.find({}).then((dronesFromDB) => {
+    res.render('drones/list', { dronesFromDB })
+  })
   // Iteration #2: List the drones
   // ... your code here
-});
+})
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
-});
+})
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
-});
+})
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-});
+})
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-});
+})
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
-});
+})
 
-module.exports = router;
+module.exports = router
