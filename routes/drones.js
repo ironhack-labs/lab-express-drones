c0b04fbbc306038bcb05790b2db642b0544f5053
Old <onelifedesigning@gmail.com>
2020-07-10 15:51:12 +0200

    DONE - modified to mongoose method Model.finByIdAndUpdate/findbyIdAndRemove and promises band callback - XD

--- routes/drones.js
+++ routes/drones.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const router = express.Router();
 
 // require the Drone model here
 const Drone = require('../models/Drone.model');
 
-const router = express.Router();
 
 router.get('/', (req, res, next) => {
   // Iteration #2: List the drones
@@ -31,31 +31,24 @@ router.get('/:id/edit', (req, res, next) => {
     .catch(e => console.error(e))
 });
 
+
 router.post('/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  Drone.findOneAndUpdate(
+  Drone.findByIdAndUpdate(
     {_id: req.params.id},
-    req.body,
-    (err) => {
-      if (err) {
-        res.redirect(`/drones/${req.params.id}/edit`)
-      }
-      res.redirect('/drones')
-    }
+    req.body
   )
+  .then(() => res.redirect('/drones') )
+  .catch(() => res.redirect(`/drones/${req.params.id}/edit`))
 });
 
 router.post('/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  Drone.findOneAndDelete(
-    {_id: req.params.id},
-    (err) => {
-      if (err) {
-        next(err);
-      }
-      res.redirect('/drones')
-    }
+  Drone.findByIdAndDelete(
+    {_id: req.params.id}
   )
+  .then(() => res.redirect('/drones'))
+  .catch(() => res.redirect(`/drones/${req.params.id}/edit`))
 });
 
 module.exports = router;
