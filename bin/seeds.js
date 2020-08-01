bfb4a5cd0b81a51eab44354c73c61d144c48c6b7
Rafael de Lazaro <rafaeldelazaro@gmail.com>
2020-08-02 01:55:29 +0200

    added drone list and creation. Edit midway.

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,19 @@
-// Iteration #1
+const mongoose = require('mongoose')
+require('../configs/db.config')
+
+let droneModel = require('../models/Drone.model')
+
+const Drones = [
+    {name: 'Mavic 2', propellers: 4, maxSpeed: 20},
+    {name: 'Agras T-16', propellers: 6, maxSpeed: 10},
+    {name: 'Anafi', propellers: 4, maxSpeed: 15}
+];
+
+droneModel.insertMany(Drones)
+    .then(()=> console.log(`Database updated. ${Drones.length} drones added.`))
+        .then(()=>{
+            mongoose.connection.close()
+                .then(()=>{console.log ('Connection closed')})
+                .catch(() => (`Failed to close the connection to the database`))
+        })
+    .catch(()=> console.log (`Failed to update database. Insert promise rejected.`))
--- routes/drones.js
+++ routes/drones.js
@@ -1,26 +1,30 @@
 const express = require('express');
-
-// require the Drone model here
+const droneModel = require('../models/Drone.model')
 
 const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  droneModel.find()
+    .then((drones) => res.render('drones/list.hbs', {drones}))
+    .catch(() => console.log (`Cannot find the drone collection`))
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  droneModel.create(req.body)
+    .then(()=>res.redirect('drones'))
+    .catch(()=>res.render('drones/create-form.hbs', {error:true}))
 });
 
+
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
+  droneModel.findById(req.params.id)
+    .then((drone)=> res.render('drones/update-form.hbs', {drone}))
+    .catch(() => `Could not find the drone in the database`)
+
   // ... your code here
 });
 
