a111a26051ae2847660dd1bc67108f6395fa5f1a
AlvaroSL <alvaro.sanchez.lamadrid@gmail.com>
2020-08-03 08:06:28 +0200

    lab done

--- routes/drones.js
+++ routes/drones.js
@@ -60,8 +60,12 @@ router.post('/drones/:id/edit', (req, res, next) => {
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   dronesModel.findByIdAndDelete(req.params.id)
-    .then(() => res.redirect('/drones'))
-    .catch((err) => console.log('ERROR!!!', err))
+    .then(() => {
+      res.redirect('/drones')
+  })
+    .catch((err) => {
+      console.log('ERROR!!!', err)
+    })
 });
 
 module.exports = router;
