fb7acf8ebe2f793c0f9321bcd8402c53c2860afc
Andrew <andre.mastercode@gmail.com>
2020-08-07 23:17:41 +0000

    done

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
@@ -8,14 +7,11 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
-
 const app_name = require('./package.json').name;
 const debug = require('debug')(
   `${app_name}:${path.basename(__filename).split('.')[0]}`
 );
-
 const app = express();
-
 // require database configuration
 require('./configs/db.config');
 
@@ -23,10 +19,11 @@ require('./configs/db.config');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,11 +31,15 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Drez Shop';
 
 const index = require('./routes/index');
 app.use('/', index);
 
 const droneRoutes = require('./routes/drones');
 app.use('/', droneRoutes);
+app.use('/drones/', droneRoutes);
+
+
 
 module.exports = app;
