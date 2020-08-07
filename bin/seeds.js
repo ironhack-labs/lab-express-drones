4729d846c64a3d5bed9a9f874361dbd72a034f32
Lynn W-B <l.welshboucard@yahoo.co.uk>
2020-08-07 14:40:25 +0200

    Iteration 1 complete

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,28 @@
-// Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+require('../configs/db.config');
+
+const drones = [
+    {
+    name: 'Terminator',
+    propellers: 19,
+    maxSpeed: 123
+}, {
+    name: 'Widelens Wendy',
+    propellers: 5,
+    maxSpeed: 15
+}, {
+    name: 'To Infinity And Beyond',
+    propellers: 234,
+    maxSpeed: 349
+}
+];
+
+Drone.create(drones)
+.then(dronesFromDB => {
+    console.log(`Created ${dronesFromDB.length} drones`);
+    mongoose.connection.close();
+})
+.catch(error => console.log(`An error occurred while creating drones: ${error}`));
+
---
+++ models/Drone.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const droneSchema = new Schema(
+    {
+        name: String,
+        propellers: Number,
+        maxSpeed: Number
+    }
+);
+
+module.exports = model('Drone', droneSchema);
