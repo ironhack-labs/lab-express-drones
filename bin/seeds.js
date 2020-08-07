49fd1f608337fe6ef3d8d7389feb0d43bfc670fe
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-08-06 21:27:37 -0400

    iteration 1: created drone model and seeded database

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,29 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+const drones = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+  ];
+
+  mongoose
+  .connect('mongodb://localhost/express-drones-dev', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+
+    Drone.create(drones)
+        .then(dronesFromDB => {
+            console.log(`Seeded ${dronesFromDB.length} into the database.`)
+        }).catch(err => console.log(`Error seeding database with drones: ${err}`))
+
+    setTimeout(() => {
+        mongoose.disconnect();
+    }, 5000);
+  }).catch(err => console.error('Error connecting to mongo', err));
---
+++ models/Drone.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const droneSchema = new Schema (
+    {
+        name: { type: String },
+        propellors: { type: Number },
+        maxSpeed: { type: Number }
+    }
+);
+
+const Drone = model('Drone', droneSchema);
+
+module.exports = Drone;
