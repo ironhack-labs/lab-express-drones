a86946ad28844dcc9a89c27f2b01936614a75ca7
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-09 17:07:13 +0100

    did iteration 4

--- routes/drones.js
+++ routes/drones.js
@@ -35,13 +35,39 @@ router.post('/drones/create', (req, res, next) => {
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const {
+    id
+  } = req.params;
+  Drone.findById(id)
+    .then((droneToEdit) => res.render('drones/update-form',
+      droneToEdit
+    ))
+    .catch((error) => `Error while getting a drone to edit: ${error}`);
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const {
+    id
+  } = req.params;
+  const {
+    name,
+    propellors,
+    maxSpeed
+  } = req.body;
+
+  Drone.findByIdAndUpdate(id, {
+      name,
+      propellors,
+      maxSpeed
+    }, {
+      new: true
+    })
+    .then(() => res.redirect("/drones"))
+    .catch((error => {
+      `An error occurred while creating ${error}`,
+      res.render(`/drones/${id}/edit`)
+    }))
+
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
