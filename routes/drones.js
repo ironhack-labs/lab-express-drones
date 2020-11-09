1df46d3796b4fd61301d1bcb3c5a98d86c380c82
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-11-09 19:48:12 +0100

    Completed iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -15,13 +15,19 @@ router.get("/drones/list", (req, res, next) => {
 });
 
 router.get("/drones/create", (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form");
 });
 
 router.post("/drones/create", (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+
+  Drone.create({
+    name,
+    propellers,
+    maxSpeed,
+  })
+    .then(() => res.redirect("/drones/list"))
+    .catch((error) => `Error while creating a new book: ${error}`);
 });
 
 router.get("/drones/:id/edit", (req, res, next) => {
