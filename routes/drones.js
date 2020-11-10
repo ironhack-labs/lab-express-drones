bb4e50f01e8992a413764eca82a53f3f72adea6a
Maya Ausset <mayaausset@gmail.com>
2020-11-10 15:05:10 +0100

    iteration 4 and 5 done

--- routes/drones.js
+++ routes/drones.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-
-
 // require the Drone model here
 const Drone = require('../models/Drone.model')
 const router = express.Router();
@@ -27,18 +25,35 @@ router.post('/drones/create', (req, res, next) => {
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  console.log(`the id is ${id}`);
+
+  Drone.findById(id)
+    .then((droneToEdit) => {
+      res.render("drones/update-form", droneToEdit)
+    })
+    .catch((err) => console.log(`Could not render the editing page for the drone: ${err}`))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+    const { id } = req.params;
+    const { name, propellers, maxSpeed } = req.body;
+
+    Drone.findByIdAndUpdate(
+      id,
+      { name, propellers, maxSpeed},
+      { new: true}
+    )
+      .then((updatedDrone) => res.redirect("/drones"))
+      .catch((err) => console.log(`Could not render the drones page : ${err}`))
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+
+  Drone.findByIdAndDelete(id)
+    .then(() => res.redirect("/drones"))
+    .catch((err) => console.log(`Could not render the drones page : ${err}`))
 });
 
 module.exports = router;
