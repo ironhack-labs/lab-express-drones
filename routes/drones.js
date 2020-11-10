3d7f76c1abf18718d6d8621e3bed8a8e3755c180
Alex Pliakas <pliakasbenitez@gmail.com>
2020-11-10 15:16:26 +0100

    done

--- routes/drones.js
+++ routes/drones.js
@@ -27,18 +27,31 @@ router.post('/drones/create', (req, res, next) => {
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+
+  Drone.findById(id)
+    .then((drone) => {
+      console.log(drone);
+      res.render('drones/update-form', drone);
+    })
+    .catch((error) => `Error while editing drone: ${error}`);
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body
+
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed }, { new: true })
+    .then(() => res.redirect('/drones'))
+    .catch((error) => `Error while editing drone: ${error}`);
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+
+  Drone.findByIdAndDelete(id)
+  .then(() => res.redirect('/drones'))
+  .catch((error) => `Error while deleting drone: ${error}`);
 });
 
 module.exports = router;
