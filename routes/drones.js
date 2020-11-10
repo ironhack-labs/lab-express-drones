32f38cf18d760afaffce2e40c5278b684cec1f57
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-10 17:41:30 +0100

    Iteration 4 complete

--- routes/drones.js
+++ routes/drones.js
@@ -25,13 +25,22 @@ router.post("/drones/create", (req, res, next) => {
     .catch((error) => console.log(error), res.render("drones/create-form"));
 });
 
-//router.get("/drones/:id/edit", (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
+  const { id } = req.params;
 
-//});
+  Drone.findById(id).then((editDrone) => {
+    res.render("drones/update-form", editDrone);
+  });
+});
 
-//router.post("/drones/:id/edit", (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
 
-//);
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed }, { new: true })
+    .then(() => res.redirect("/drones"))
+    .catch((error) => console.log(error));
+});
 
 //router.post("/drones/:id/delete", (req, res, next) => {
 // Iteration #5: Delete the drone
