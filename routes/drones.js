70d4cdc9c327a0317196662264e5149bef0807e0
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-10 15:49:31 +0100

    Completed Iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -16,13 +16,14 @@ router.get('/drones', (req, res, next) => {
 
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form");
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  Drone.create({ name, propellers, maxSpeed })
+    .then(() => res.redirect("/drones"))
+    .catch((err) => console.err(err), res.render("drones/create-form"));
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
