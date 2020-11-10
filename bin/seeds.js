e4c05211ff77080a8471e3d5a6155e71a89dbd4b
Rich Alossi <alossix@gmail.com>
2020-11-10 13:12:36 +0100

    Iteration 1 done

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,41 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("./../models/Drone.model");
+const expressDB = "mongodb://localhost:27017/express-drones";
+
+mongoose
+  .connect(expressDB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Database connection successful: ${expressDB}`))
+  .catch((err) => console.error(err));
+
+const testDrones = [
+  {
+    name: "Drone1",
+    propellers: 6,
+    maxSpeed: 10,
+  },
+  {
+    name: "Drone2",
+    propellers: 4,
+    maxSpeed: 8,
+  },
+  {
+    name: "Drone3",
+    propellers: 8,
+    maxSpeed: 12,
+  },
+];
+
+Drone.create(testDrones)
+  .then((results) => {
+    console.log(results);
+  })
+  .then(() => {
+    mongoose.disconnect();
+  })
+  .catch((err) => console.error(err));
---
+++ models/Drone.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const droneSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  propellers: {
+    type: Number,
+    required: true,
+  },
+  maxSpeed: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Drone = mongoose.model("Drone", droneSchema);
+module.exports = Drone;
