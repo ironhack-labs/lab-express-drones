416d4ccae04298641c100c0bf3e6b2785fd9f0dd
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-11-10 18:49:56 +0100

    Added some styling, far from finished.

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+const path = require("path");
 
 const droneSchema = new Schema(
   {
     name: String,
     propellers: Number,
     maxSpeed: Number,
+    image: {
+      type: String,
+      default: "/images/defaultdrone.png",
+    },
   },
   {
     timestamps: true,
