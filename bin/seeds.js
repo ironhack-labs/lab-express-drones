0e1b69994a5c84a0eadb93ff61d4e60ba15c1c34
teof95 <teof95@gmail.com>
2020-11-11 09:34:22 +0100

    all iterations done without styling

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,40 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+
+const drone = require("../models/Drone.model");
+const DB_NAME = 'express-drones-dev';
+
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+const drones = [{
+        name: 'xiaomi',
+        propellers: 4,
+        maxSpeed: 25,
+    },
+    {
+        name: 'DJI',
+        propellers: 6,
+        maxSpeed: 35,
+    },
+    {
+        name: 'eachine',
+        propellers: 5,
+        maxSpeed: 15,
+    },
+
+];
+
+drone.create(drones)
+    .then((dronesFromDB) => {
+        console.log(`Created ${dronesFromDB.length} drones`);
+        mongoose.connection.close();
+    })
+    .catch((err) =>
+        console.log(`An error occurred while getting drones from the DB: ${err}`)
+    );
--- routes/drones.js
+++ routes/drones.js
@@ -1,37 +1,96 @@
 const express = require('express');
 
 // require the Drone model here
+const drone = require('../models/Drone.model')
+
 
 const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+    // Iteration #2: List the drones
+    // ... your code here
+    drone.find({})
+        .then((dbDrones) => {
+            res.render('drones/list', { dbDrones })
+        })
+        .catch((error) => `Error while fetching all drones: ${error}`)
 });
 
+
+//GET route in order to create and render the form for the new data.
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+    // Iteration #3: Add a new drone
+    // ... your code here
+    drone.find()
+        .then((dbDrones) => {
+            res.render("drones/create-form", { dbDrones });
+        })
+        .catch((err) =>
+            console.error(`Err while displaying post input page: ${err}`)
+        );
+
 });
 
+//POST route in order to get the data submited from the previous form used by the user and create a new drone in the database
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+    // Iteration #3: Add a new drone
+    // ... your code here
+    let { name, propellers, maxSpeed } = req.body;
+    drone.create({ name, propellers, maxSpeed })
+        .then(() => {
+            res.redirect('/drones');
+        })
+        .catch((err) =>
+            console.error(`Error while creating drone: ${err}`, ));
+
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+    // Iteration #4: Update the drone
+    // ... your code here
+    let { id } = req.params;
+    drone.findById(id)
+        .then((edit) =>
+            res.render('drones/update-form', edit))
+        .catch((err) =>
+            console.error(`Error while getting a drone to edit: ${err}`));
 });
+
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+    // Iteration #4: Update the drone
+    // ... your code here
+    let { id } = req.params;
+    let { name, propellers, maxSpeed } = req.body;
+    drone.findByIdAndUpdate(id, { name, propellers, maxSpeed }, { new: true })
+        .then(() => res.redirect("/drones"))
+        .catch((err) =>
+            console.error(`An error occurred while updating drone ${err}`, ));
 });
 
+//iteration 5 to find
+
+// router.post('/drones/:id/delete', (req, res, next) => {
+//     // Iteration #5: Delete the drone
+//     // ... your code here
+//     let { id } = req.params;
+//     drone.findById(id)
+//         .then(() =>
+//             console.log(`the id is: ${id}`));
+// });
+
+//iteration 5 to delete
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+    let { id } = req.params;
+    drone.findByIdAndDelete(id)
+        .then(() =>
+            res.redirect("/drones"))
+        .catch((err) =>
+            console.log(`Error while deleting drone: ${err}`));
 });
+
+
+
+
 
 module.exports = router;
