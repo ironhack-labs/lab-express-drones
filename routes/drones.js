7e85bc32506966b58a65e86673fc1e46b6481306
codebroker <utuman@windowslive.com>
2020-11-13 17:54:58 +0100

    done

--- routes/drones.js
+++ routes/drones.js
@@ -22,18 +22,35 @@ router.post('/drones/create', (req, res) => {
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+
+  Drone.findById( id )
+    .then((droneToEdit) => {
+      res.render("drones/update-form", droneToEdit);
+    })
+    .catch((error) => `Error while getting a drone for edit: ${error}`);
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post("/drones/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+
+  Drone.findByIdAndUpdate(
+    id,
+    { name, propellers, maxSpeed },
+    { new: true } 
+  )
+    .then((updatedDrone) => res.redirect(`/${updatedDrone._id}`))
+    .catch((error) =>
+      console.log(`Error while updating a single drone: ${error}`)
+    );
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+  Drone.findByIdAndDelete( id )
+  .then(() => res.redirect("/drones"))
+  .catch(error => `Error occured while deleting drones: ${error}`)
 });
 
 module.exports = router;
