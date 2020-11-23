ad2d857cceb9d3335660fea463cc8a589b2dcd18
Dirk Kelderman <dirkkelderman@gmail.com>
2020-11-23 12:32:12 +0100

    iteration 5, no styling

--- routes/drones.js
+++ routes/drones.js
@@ -53,7 +53,13 @@ router.post('/drones/:id/edit', (req, res, next) => {
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+
+  Drone.findByIdAndDelete(id)
+  .then(() => {
+    res.redirect('/drones');
+  })
+  .catch(err => { console.log(`Error: ${err}`)});
 });
 
 module.exports = router;
