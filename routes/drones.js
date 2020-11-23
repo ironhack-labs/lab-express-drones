7043e627626ca10c61c37d00dd3e3f5f74053601
Dirk Kelderman <dirkkelderman@gmail.com>
2020-11-23 12:26:33 +0100

    iteration 4

--- routes/drones.js
+++ routes/drones.js
@@ -20,8 +20,8 @@ router.get('/drones/create', (req, res, next) => {
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  const {name, propellers, speed} = req.body;
-  Drone.create({name, propellers, speed})
+  const {name, propellers, maxSpeed} = req.body;
+  Drone.create({name, propellers, maxSpeed})
   .then(() => {
     res.redirect('/drones');
   })
@@ -30,12 +30,25 @@ router.post('/drones/create', (req, res, next) => {
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  Drone.findById(id)
+  .then(droneToEdit => {
+    res.render('drones/update-form', droneToEdit);
+  })
+  .catch(err => { console.log(`Error: ${err}`)});
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const {name, propellers, maxSpeed} = req.body;
+
+  Drone.findByIdAndUpdate(id,{name, propellers, maxSpeed}, {new: true})
+  .then(updatedDrone => {
+    res.redirect(`/drones`);
+  })
+  .catch(err => { console.log(`Error: ${err}`)});
+
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
