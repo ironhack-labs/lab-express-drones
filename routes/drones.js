2091ee65e81e290ee9903d49186944c1890f2ccf
Ashley Vermaas <ashleyvermaas1@gmail.com>
2020-11-24 15:41:02 +0100

    complete iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -5,7 +5,6 @@ const Drone = require('../models/Drone.model');
 router.get('/drones', (req, res, next) => {
   Drone.find()
     .then((dronesFromDB) => {
-      console.log(dronesFromDB);
       res.render('drones/list', {
         dronesFromDB
       });
@@ -14,13 +13,14 @@ router.get('/drones', (req, res, next) => {
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form');
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  Drone.create(req.body)
+  .then((droneFromDB) => res.redirect('/drones'))
+  .catch((error) => res.redirect('/drones/create'));
+
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
