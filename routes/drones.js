292ab5e76adfb3367081901f6fa9f760a0b83c62
Ashley Vermaas <ashleyvermaas1@gmail.com>
2020-11-24 15:58:39 +0100

    complete iteration 5

--- routes/drones.js
+++ routes/drones.js
@@ -35,14 +35,15 @@ router.post('/drones/:id/edit', (req, res, next) => {
   Drone.findByIdAndUpdate(req.params.id, req.body)
     .then(() => res.redirect('/drones'))
     .catch((error) => {
-      res.redirect('/drones/update-form');
+      res.redirect('drones/update-form');
       console.log(`Could not edit the drone due to an error: ${error}`);
     });
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  Drone.findByIdAndDelete(req.params.id)
+  .then(() => res.redirect('/drones'))
+  .catch((error) => console.log(error));
 });
 
 module.exports = router;
