66bb21f1cd9bcdc6fdcfe99cec4e2b1f877f81ce
Christian <christian.garba@gmail.com>
2021-01-16 10:51:09 -0700

    done

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,20 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+
+require('../configs/db.config');
+
+const Drone = require('../models/Drone.model');
+
+const drones = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.create(drones)
+.then(dronesFromDB => {
+    console.log(`Successfully added ${dronesFromDB.length} to the database.`);
+    mongoose.connection.close();
+})
+.catch(err => console.log(`Error occurred - seeding unsuccessful: ${err}`));
--- routes/drones.js
+++ routes/drones.js
@@ -1,37 +1,64 @@
 const express = require('express');
 
 // require the Drone model here
+const Drone = require('../models/Drone.model');
 
 const router = express.Router();
 
+// GET Drones List
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+  .then(dronesFromDB =>
+    res.render('drones/list.hbs', { dronesFromDB }))
+    .catch(err => console.log(`Error occurred while retrieving drones: ${err}. `))
 });
 
+// GET Create Drones
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  Drone.find()
+  .then(dronesFromDB =>
+    res.render('drones/create-form.hbs', { dronesFromDB }))
+    .catch(err => console.log(`Error occurred while navigating to create drones: ${err}. `))
 });
 
+// POST Create Drones
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  Drone.create({ name, propellers, maxSpeed })
+  .then(() => 
+    res.redirect("/drones"))
+    .catch(err => console.log(`Error occured while creating drones: ${err}.`))
 });
 
+// GET Edit Drones
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findById(req.params.id)
+  .then(droneToUpdate => 
+    res.render("drones/update-form", { droneToUpdate }))
+    .catch(err => console.log(`Error occured while navigating to edit page: ${err}.`))
 });
 
+// POST Edit Drones
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findByIdAndUpdate(req.params.id, req.body)
+  .then(() =>
+    res.redirect('/drones'))
+    .catch(err => console.log(`Error occurred while updating drone: ${err}.`))
+    
+    // Re-rendering to update-form with fields still populated: Cleaner way to do this?
+    // Causes POST header error.
+    .then(() => 
+      Drone.findById(req.params.id))
+      .then(droneToUpdate => 
+      res.render("drones/update-form", { droneToUpdate }))
 });
 
+// POST Delete Drones
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  Drone.findByIdAndDelete(req.params.id)
+  .then(() =>
+  res.redirect('/drones'))
+  .catch(err => console.log(`Error occurred while deleting drone: ${err}.`))
 });
 
 module.exports = router;
