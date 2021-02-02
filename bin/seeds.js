2d2475666e5bd5b7195a829d4e8dbf1b3e96f421
Pedro <pedroscampoy@gmail.com>
2021-02-03 00:03:20 +0100

    Iteration #1

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,24 @@
 // Iteration #1
+const mongoose = require('mongoose');
+require("dotenv").config();
+require("../configs/db.config");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.deleteMany()
+  .then(() => {
+    Drone.create(drones)
+      .then((drones) => {
+        console.log(`${drones.length} drones added to the database`);
+      })
+      .then(() => {
+        mongoose.connection.close();
+        console.log("Mongoose disconected")
+      })
+  })
+  .catch((error) => console.log(error))
---
+++ models/Drone.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const droneSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  propellers: {
+    type: Number,
+    min: 1
+  },
+  maxSpeed: {
+    type: Number,
+    max: 40
+  }
+})
+
+const Drone = mongoose.model('Drone', droneSchema)
+module.exports = Drone 
