5513e098cd0a563f0ba6d72dd30139d465b53d56
Eduardo García <edugarciasb16@gmail.com>
2021-02-04 00:22:10 +0100

    Lab done

--- routes/drones.js
+++ routes/drones.js
@@ -25,22 +25,42 @@ router.post('/drones/create', (req, res, next) => {
     .then(droneFromDB => console.log(`New drone created: ${droneFromDB.name}.`))
     .then(() => res.redirect("/drones"))
     .catch((e) => next(e));
-
 })
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findById(req.params.id)
+    .then(droneToEdit =>{
+      //console.log(droneToEdit)
+      res.render('drones/update-form', droneToEdit)
+    })
+    .catch(e => console.error(`Error while getting a single book for edit: `, e))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body
+
+  Drone.findByIdAndUpdate(id, {name, propellers, maxSpeed},{new:true})
+    .then(() => res.redirect("/drones"))
+    .catch(error =>
+      console.log(`Error while updating a single book: ${error}`)
+    );
+
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  Drone.findByIdAndDelete(req.params.id)
+    .then((d) =>{
+      console.log(`Drone ${d.name} has been eliminated`)
+      res.redirect('/drones')
+    })
+    .catch(
+      error =>
+        console.log(`Error while deleting a single book: ${error}`)
+    )
 });
 
 module.exports = router;
