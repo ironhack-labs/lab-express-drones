c9f85077031830b65a7592a11462f3250397d7a6
Hugo Copca <copcal79@gmail.com>
2021-02-09 00:15:46 -0600

    Iteracion 2

---
+++ models/Drone.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+
+const droneSchema = new Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+
+const Drone = model("Drone", droneSchema);
+module.exports = Drone;
--- routes/drones.js
+++ routes/drones.js
@@ -1,37 +1,45 @@
 const express = require('express');
+// const router = express.Router();
+const Drone = require("../models/Drone.model.js")
+    // require the Drone model here
 
-// require the Drone model here
 
 const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+    // Iteration #2: List the drones
+    // ... your code here
+
+    Drone.find()
+        .then(drones => {
+            console.log(drones)
+            res.render("drones/list", { drones })
+        }).catch(error => { next(error) })
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+    // Iteration #3: Add a new drone
+    // ... your code here
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+    // Iteration #3: Add a new drone
+    // ... your code here
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+    // Iteration #4: Update the drone
+    // ... your code here
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+    // Iteration #4: Update the drone
+    // ... your code here
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+    // Iteration #5: Delete the drone
+    // ... your code here
 });
 
 module.exports = router;
