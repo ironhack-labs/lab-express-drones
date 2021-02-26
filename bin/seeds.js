a6cf3fd48793e8dd532d547b87b9a79e0590ccb4
Anna Lefour <anna.lefour@laposte.net>
2021-02-26 18:15:51 +0100

    It 1 en cours

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,20 @@
 // Iteration #1
+require("../configs/db.config");
+
+const DroneModel = require("../models/Drone.models");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+//Add the script that will run to seed the DB
+
+DroneModel.create(drones)
+.then((droneInfo) => {
+    console.log(droneInfo);
+})
+.catch((err) => {
+    console.log(err);
+})
