3e2c978ff9c7358200a110f53c80b07b27da7c38
Magali <fort_magali@hotmail.fr>
2021-02-26 22:25:16 +0100

    done

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Dalek", propellers: 10, maxSpeed: 1000 },
+];
+
+require("./../configs/db.config");
+const DroneModel = require("./../models/Drone.model");
+const mongoose = require('mongoose');
+
+DroneModel.create(drones)
+  .then((dbsuccess) => {
+    console.log(dbsuccess.length, " drones have been created");
+    mongoose
+      .disconnect()
+      .then((ok) => console.log("mongoDB disconnected"))
+      .catch((err) => console.log("error disconnecting mongoDB ", err));
+  })
+  .catch((dbError) => console.log(dbError));
+
--- routes/drones.js
+++ routes/drones.js
@@ -1,37 +1,62 @@
-const express = require('express');
+const express = require("express");
 
 // require the Drone model here
-
+const DroneModel = require("./../models/Drone.model");
 const router = express.Router();
 
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  DroneModel.find()
+    .then((drones) => res.render("drones/list", { drones }))
+    .catch((err) => console.log(err), next);
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  // const { name, propellers, maxSpeed } = req.body;
+  // { name, propellers, maxSpeed }
+  DroneModel.create(req.body)
+  .then(newDrone=> {
+    console.log("new drone was created: ",newDrone);
+    res.redirect("/drones")
+  })
+  .catch(err=> {
+    res.render("drones/create-form");
+    console.log(err)}) 
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  DroneModel.findById(req.params.id)
+  .then(drone =>
+    res.render("drones/update-form", {drone}))
+  .catch(err=> console.log("couldn't find the drone: ", err))
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  DroneModel.findByIdAndUpdate(req.params.id, req.body, {new:true})
+  .then(drone=> {
+    res.redirect("/drones");
+    console.log("drone updated!", drone)
+  })
+  .catch(err=> {
+    res.render("drones/update-form", {drone})
+    console.log("error updating drone: ",err)})
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  DroneModel.findByIdAndDelete(req.params.id)
+  .then(success=> {
+    console.log("drone exterminated!");
+    res.redirect("/drones")})
+  .catch(err=>console.log("extermination failed", err))
 });
 
 module.exports = router;
