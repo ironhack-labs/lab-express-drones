a994da8ff6402d4fcee6f54ef9ee841023c69779
Alexis Verthuy <alexis.verthuy@gmail.com>
2021-02-27 12:35:41 +0100

    lab done but I always have a 500 error when trying to access my views

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,35 @@
-// Iteration #1
+require("./../configs/db.config")
+
+const droneModel = require("./../models/droneModel")
+const MongoosE = require("mongoose")
+
+const drones = [
+    {
+        name: 'Creeper XL 500',
+        propellers: 3,
+        maxSpeed: 12
+    },
+    {
+        name: 'Racer 57',
+        propellers: 4,
+        maxSpeed: 20
+    },
+    {
+        name: 'Courier 3000i',
+        propellers: 6,
+        maxSpeed: 18
+    }
+]
+
+droneModel.create(drones)
+.then(dbSuccess => {
+    console.log(dbSuccess);
+})
+.catch(dbError => {
+    console.log(dbError);
+})
+.then( dbclose => {
+    MongoosE.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination')})
+   
+})
--- routes/drones.js
+++ routes/drones.js
@@ -1,37 +1,71 @@
 const express = require('express');
+const { Mongoose } = require('mongoose');
+const { findById } = require('./../models/droneModel');
 
-// require the Drone model here
+const droneModel = require("./../models/droneModel")
 
 const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  droneModel.find()
+    .then(dronesFromdb => {
+      res.render("list", { drones: dronesFromdb })
+    })
+    .catch((err) => {
+      next(err)
+    });
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('create-form')
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post('/drones/create', async (req, res, next) => {
+  const { name, propellers, maxSpeed } = req.body;
+  try {
+    await droneModel.create({
+      name,
+      propellers,
+      maxSpeed,
+    });
+    res.redirect("/drones");
+  } catch (err) {
+    next(err);
+  }
+
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  droneModel.findById(req.params.id)
+    .then((drone) => {
+      res.render('update-form', { drone })
+    })
+    .catch((err) => {
+      next(err);
+    })
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post('/drones/:id/edit', async (req, res, next) => {
+  const { name, propellers, maxSpeed } = req.body;
+  try {
+    await droneModel.findByIdAndUpdate(req.params.id, {
+      name,
+      propellers,
+      maxSpeed,
+    });
+    res.redirect("/drones");
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+router.post('/drones/:id/delete', async (req, res, next) => {
+  try {
+    await droneModel.findByIdAndDelete(req.params.id);
+    res.redirect("/drones");
+  } catch (err) {
+    next(err); 
+  }
 });
 
 module.exports = router;
