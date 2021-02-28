ed7a1a9333da4f37298652572f126cd4f0c16329
Alexis Verthuy <alexis.verthuy@gmail.com>
2021-02-28 10:09:20 +0100

    Model modif

--- models/droneModel.js
+++ models/droneModel.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const droneModel = new Schema ({
-    name: String,
-    propellers: Number,
-    maxSpeed: Number
+    name: `String`,
+    propellers: `Number`,
+    maxSpeed: `Number`
 })
 
 const DroneModel = mongoose.model("drones", droneModel)
