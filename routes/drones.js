fe8cbf928506f69c46f977ced52fa0f53c8496bc
Mohammed Adib <adibmohammed95@gmail.com>
2021-03-02 10:15:33 +0100

    Second push Drones Labs

--- routes/drones.js
+++ routes/drones.js
@@ -1,17 +1,27 @@
 const express = require('express');
 
-// require the Drone model here
+const myDronesModel = require('./../models/Drone.model')
 
 const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  myDronesModel.find()
+  .then((dbResponse) => {
+    res.render('./../views/drones/list.hbs', {myDrones: dbResponse})
+  })
+  .catch((dbError) => {
+    next(dbError);
+  })
+
 });
+
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+res.render('./../views/drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
