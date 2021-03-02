a7de317fc5c6e8d74b1114beafd45df09cffa687
Yassine <yassineabouzaid@hotmail.com>
2021-03-02 10:43:18 +0100

    all the iterations without CSS it worked for a sec, and then i got an error

--- routes/drones.js
+++ routes/drones.js
@@ -10,28 +10,36 @@ router.get('/drones', (req, res, next) => {
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
-
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+  DroneModel.create({name, propellers, maxSpeed})
+    .then(() => res.redirect('/drones'))
+    .catch(error => {res.render('drones/create-form')});
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const {id} =req.params;
+
+  DroneModel.findById(id)
+  .then(droneToEdit =>{res.render('drones/update-form', {drone: droneToEdit});})
+  .catch(error => next(error))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params;
+  const {name, propellers, maxSpeed} = req.body;
+  console.log(req.body)
+
+  DroneModel.findByIdAndUpdate(id, {name, propellers, maxSpeed}, {new:true})
+  .then(updatedDrone => res.redirect('/drones'))
+  .catch(error => next(error));
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  const {id} = req.params;
+
+  DroneModel.findByIdAndDelete(id)
+    .then(() => res.redirect('/drones'))
+    .catch(error => next(error));
 });
 
 module.exports = router;
