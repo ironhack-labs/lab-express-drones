6c9f22f5c7371d4f028a51dab49341b3bdd8d28a
Paula Nicioli <paulanicioli@gmail.com>
2021-03-20 16:06:02 -0300

    Lab completed

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,25 @@
 // Iteration #1
+
+// require Drone model
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+// require database configuration
+require('../configs/db.config');
+
+const dronesToAdd = [
+  { name: 'General Atomics MQ-9 Reaper', propellers: 4, maxSpeed: 18 },
+  { name: 'Challenger MTX8', propellers: 2, maxSpeed: 10 },
+  { name: 'XYZ Flyer S-9', propellers: 1, maxSpeed: 6 },
+];
+
+Drone.create(dronesToAdd)
+  .then((drones) => {
+    console.log(
+      `Seeding completed in the database: ${drones.length} drones added!`
+    );
+    mongoose.disconnect();
+  })
+  .catch((err) => {
+    console.log('Error while seeding the database ==> ', err);
+  });
