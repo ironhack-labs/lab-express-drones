f8a5c810ec0e678700a641fc3cee3deec3a68125
Jorge <jorgeberrizbeitia@gmail.com>
2021-04-14 14:06:54 +0200

    added ironlauncher code

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+  console.log('lab-express-drones JS imported successfully!');
 
 }, false);
