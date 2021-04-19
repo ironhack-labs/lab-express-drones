d34757805e163f11e65c20475d01712c4bf97acf
Bea Avis <bea.avis.p@gmail.com>
2021-04-19 21:08:17 +0200

    Iteration 4 done

--- routes/drones.js
+++ routes/drones.js
@@ -28,13 +28,18 @@ router.post('/drones/create', (req, res, next) => {
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  Drone.findById(id)
+  .then( drone => res.render('drones/update-form', drone))
+  .catch( error => next(error));  
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed })
+  .then(() => res.redirect('/drones'))
+  .catch(error => res.render('drones/update-form', { error }));
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
