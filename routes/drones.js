38238350fa409330db46a393f29804ed1f48e4cd
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-19 17:52:52 +0200

    Done all iterations

--- routes/drones.js
+++ routes/drones.js
@@ -33,17 +33,33 @@ router.post("/drones/create", (req, res, next) => {
 
 router.get("/drones/:id/edit", (req, res, next) => {
 	// Iteration #4: Update the drone
-	// ... your code here
+	Drone.findById(req.params.id)
+    .then((drone) => {
+      console.log(req.params.id);
+      res.render('drones/update-form', drone);
+    })
+    .catch(err => next(err));
 });
 
 router.post("/drones/:id/edit", (req, res, next) => {
 	// Iteration #4: Update the drone
-	// ... your code here
+	const { name, propellers, maxSpeed } = req.body;
+  const {id} = req.params;
+
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed })
+    .then(() => {
+      res.redirect('/drones');
+    })
+    .catch((err) => next(err));
 });
 
 router.post("/drones/:id/delete", (req, res, next) => {
 	// Iteration #5: Delete the drone
-	// ... your code here
+	Drone.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect('/drones');      
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
