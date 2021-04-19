fa3c64b37c7fcff280fbbcfeba479f9ed119910e
Bea Avis <bea.avis.p@gmail.com>
2021-04-19 20:24:02 +0200

    Iteration 1 done

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,20 @@
 // Iteration #1
+const dronesData = require('../data');
+const mongoose = require('mongoose');
+const express = require('express');
+const Drone = require('../models/Drone.model');
+const DB_URI = 'mongodb://localhost/express-drones-dev';
+
+mongoose.connect(DB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    Drone.create(dronesData)
+    .then(() => {
+        console.log(`${dronesData.length} inserted`)
+        mongoose.disconnect();
+    })
+  })
+  .catch(error => console.error('Error connecting to mongo', err))
---
+++ data.js
@@ -0,0 +1,7 @@
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+module.exports = drones;
