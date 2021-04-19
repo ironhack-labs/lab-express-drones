747ef663609683d226bfb6321f87410f26bfb179
Pablo del Barrio <pablodbb@gmail.com>
2021-04-19 19:51:31 +0200

    First iteration :sparkles:

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const drones = require("../data");
+const Drone = require("../models/Drone.model");
+const mongoose = require("mongoose");
+const DB_NAME = "express-drones-dev";
+
+mongoose
+  .connect(`mongodb://localhost/${DB_NAME}`)
+  .then(() => {
+    console.log("Connected to database only to create the first information");
+
+    Drone.insertMany(drones)
+    .then((drones) => {
+      console.log(`${drones.length} drones inserted.`);
+      
+      mongoose.disconnect();
+    });
+  })
+  .catch((error) => console.error(error));
+
+
