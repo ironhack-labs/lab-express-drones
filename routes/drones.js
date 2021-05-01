e201150073dd995fac3dcaf0fbf5a5183509f914
Luciano Colabraro <luciano.colabraro@gmail.com>
2021-05-01 09:44:39 +0200

    add iteration 5, iteration 1,2,3,4 done

--- routes/drones.js
+++ routes/drones.js
@@ -10,11 +10,10 @@ router.get("/drones", (req, res, next) => {
   // ... your code here
   Drone.find({})
     .then((dronesArray) => {
-      res.render("drones/list", { drones: dronesArray });
+      res.render("drones/list", { drones: dronesArray }); // if the value i pass to the view is the sa me i used to the then promise, dronesArray, only noe value
     })
     .catch((error) => {
       console.log("No drones to show", error);
-      //next(error);
     });
 });
 
@@ -28,7 +27,12 @@ router.get("/drones/create", (req, res, next) => {
 router.post("/drones/create", (req, res, next) => {
   const { name, propellers, maxSpeed } = req.body;
 
-  Drone.create({ name, propellers, maxSpeed })
+  // Drone.create ({
+  // name: name,
+  // propellers: propellers,
+  // maxSpeed: maxSpeed,
+  // })
+  Drone.create({ name, propellers, maxSpeed }) // same as above, this one has been destructured
     .then((newDrone) => {
       console.log("New Drone created", newDrone);
       res.redirect("/drones");
@@ -43,11 +47,35 @@ router.post("/drones/create", (req, res, next) => {
 });
 
 router.get("/drones/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  Drone.findById(id)
+    .then((foundedDrone) => {
+      //console.log("Drone Updated", foundedDrone);
+      res.render("drones/update-form", foundedDrone);
+    })
+    .catch((error) => {
+      console.log("No drones to show", error);
+    });
   // Iteration #4: Update the drone
   // ... your code here
 });
 
 router.post("/drones/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  //const name = req.body.name
+  //const propelers = req.body.propellers
+  //const maxSpeed = req.body.maxSpeed
+  const { name, propellers, maxSpeed } = req.body; // same as above, this is the destructured form
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed }, { new: true })
+    .then((updateDrone) => {
+      console.log("Drone Updated", updateDrone);
+      res.redirect("/drones");
+    })
+    .catch((error) => {
+      console.log("No drones to show", error);
+      res.render("drones/update-form");
+    });
+
   // Iteration #4: Update the drone
   // ... your code here
 });
