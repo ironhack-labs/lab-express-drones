cb372520d5c80d48a92d7ce938006d6549e5d11f
Jordan Caley <jordancaley10@gmail.com>
2021-05-07 15:22:55 +0200

    Iteration 2

--- routes/drones.js
+++ routes/drones.js
@@ -1,18 +1,24 @@
 const express = require('express');
 
-// require the Drone model here
+const DroneModel = require("./../models/Drone.model");
 
 const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  DroneModel.find()
+    .then((dbResult) => {
+      res.render("drones/list", { title: "Drones", drones: dbResult });
+    })
+    .then((dbErr) => {
+      console.log(dbErr);
+      next(dbErr);
+    });
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.get('/drones/create', function (req, res, next) {
+  
 });
+
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
