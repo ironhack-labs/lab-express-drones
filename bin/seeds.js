4b433d92c05df94944426c8247939ead86bfde1e
Jordan Caley <jordancaley10@gmail.com>
2021-05-07 16:26:59 +0200

    Add disconnect to seed file

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,6 +1,7 @@
 // Iteration #1
 require("./../configs/db.config"); // fetch the db connection
 const DroneModel = require("./../models/Drone.model"); // fetch the model to validate our user document before insertion (in database)
+const mongoose = require("mongoose");
 
 const drones = [
   { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
@@ -15,6 +16,8 @@ const drones = [
     console.log(
       `${inserted.length} drones have been entered in the database !`
     );
+    await mongoose.connection.close();
+    console.log("mongoose disconnected");
   } catch (err) {
     console.error(err);
   }
