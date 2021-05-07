fce577ecb2425971fd0c6b0dd717dd574f93d7e3
Jordan Caley <jordancaley10@gmail.com>
2021-05-07 16:07:08 +0200

    Iteration 4

--- routes/drones.js
+++ routes/drones.js
@@ -31,13 +31,24 @@ router.post('/drones/create', (req, res, next) => {
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  DroneModel.findById(req.params.id)
+  .then((dbResult) => {
+    res.render("drones/update-form", {
+      drone: dbResult,
+    });
+  })
+  .catch((dbErr) => next(dbErr));
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  DroneModel.findByIdAndUpdate(req.params.id, req.body)
+    .then((dbResult) => {
+      res.redirect("/drones");
+    })
+    .catch((dbErr) => {
+      res.redirect("/drones/:id/edit");
+      next(dbErr);
+    });
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
