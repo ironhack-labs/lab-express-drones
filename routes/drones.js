2e6d2bb01c8c81d0aa488ecdd95e0c9d9fd35e52
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2021-05-08 16:28:28 +0200

    Completed the Lab

--- routes/drones.js
+++ routes/drones.js
@@ -7,47 +7,62 @@ const router = express.Router();
 
 router.get("/drones", (req, res, next) => {
   DroneModel.find()
-  .then((drones) => {
-    res.render("drones/list.hbs", { title: "drones crud", drones });
-  })
-  .catch(next);});
-
+    .then((drones) => {
+      res.render("drones/list.hbs", { title: "drones crud", drones });
+    })
+    .catch(next);
+});
+//Iteration 3: Create a new drone
 router.get("/drones/create", async function (req, res, next) {
-    try {
-      await DroneModel.create(req.body);
-      res.redirect("/drones/create-form.hbs");
-    } catch {
-      next(err);
-    }
-  }); 
+  try {
+    await res.render("drones/create-form.hbs");
+  } catch {
+    next(err);
+  }
+});
 
 router.post("/drones/create", (req, res, next) => {
-    const newDrone=req.body;
-    DroneModel.create(newDrone)
-      .then((dbResult) => {
-      res.send("Well done, new Drone created");
-      })
-      .catch(next);}); 
+  const NewDrone = req.body;
+  console.log(req.body);
+  DroneModel.create(NewDrone)
+    .then((dbResult) => {
+      res.redirect("/drones");
+    })
+    .catch(next);
+});
 
+//Iteration 4: Update a drone
 router.get("/drones/:id/edit", (req, res, next) => {
   DroneModel.findById(req.params.id)
-  .then((dbResult) => {
-    res.render("drones/update-form.hbs", {
-      drone: dbResult,
-    });
-  })
-  .then((dbErr) => {
-    next(dbErr);
-  });
+    .then((dbResult) => {
+      res.render("drones/update-form.hbs", {
+        drone: dbResult,
+      });
+    })
+    .catch((dbErr) => next(dbErr));
 });
 
 router.post("/drones/:id/edit", (req, res, next) => {
-//Droneodel findbyId and Update  // ... your code here
+  //DroneModel findbyId and Update  // ... your code here
+  console.log('--- req body => ------');
+  console.log(req.body); // will contain the posted informations
+  // it's an object : the keys are the names declared in the form
+  DroneModel.findByIdAndUpdate(req.params.id, req.body)
+    .then((dbResult) => {
+      res.redirect("/drones");
+    })
+    .catch((dbError) => next(dbError));
 });
 
-router.post("/drones/:id/delete", (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+//Iteration 5: Delete a given drone
+router.get("/drones/:id/delete", (req, res, next) => {
+  DroneModel.findByIdAndDelete(req.params.id)
+  .then((dbSuccess) => {
+    res.redirect("/drones");
+  })
+  .catch((dbErr) => {
+    next(dbErr);
+  });
 });
 
 module.exports = router;
