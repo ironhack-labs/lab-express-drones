197582ee1e8fda9dd82d063f003777caafbaa547
Marcos Salvo <marcossalvo95@gmail.com>
2021-06-19 10:48:16 +0200

    Done iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,22 @@
 // Iteration #1
+
+const mongoose = require("mongoose")
+
+const DroneSchema = new mongoose.Schema(
+    {name: {
+        type: String,
+        required: true,
+    },
+    propellers: {
+        type: Number,
+        required: true
+    },
+    maxSpeed: {
+        type: Number,
+        required: true,
+    }
+}) 
+
+const Drone = mongoose.model ("Drone", DroneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,39 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+
+const Drone = require("../models/Drone.model.js");
+
+require("../db/index")
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+mongoose.connection.once("connected", () => {
+    mongoose.connection.db.dropDatabase()
+    .then(() => {
+        console.log("Database cleared");
+        return Drone.insertMany(drones)
+    })
+    .then((dronesCreated) => {
+        console.log(`${dronesCreated.length} drones have been created`)
+    })
+    .catch((e) => {
+        console.log(e)
+    })
+    .finally(() => {
+        mongoose.connection.close()
+    })
+    .then(() => {
+        console.log("finnished seeds.js")
+    })
+    .catch((e) => {
+        console.log(e)
+    })
+    .finally(() => {
+        process.exit(0)
+    })
+})
