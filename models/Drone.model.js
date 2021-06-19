fa29645682d5af1d06499e5478c9971cb9914be1
Israel Zambrana <zambrana90@gmail.com>
2021-06-19 10:47:44 +0200

    iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,27 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+const droneSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    propellers: {
+      type: Number,
+      required: true,
+    },
+    maxSpeed: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+const Drone = mongoose.model("Drone", droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,30 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+const data = require("../drones.json");
+
+require("../db/index");
+
+mongoose.connection.once("connected", () => {
+  mongoose.connection.db
+    .dropDatabase()
+    .then(() => {
+      console.log("Database cleared");
+
+      return Drone.insertMany(data);
+    })
+    .then((dronesCreated) =>
+      console.log(`${dronesCreated.length} posts have been created`)
+    )
+    .catch((e) => console.error(e))
+    .finally(() => {
+      mongoose.connection
+        .close()
+        .then(() => console.log("Finish seeds.js"))
+        .catch((e) => console.error(e))
+        .finally(() => {
+          process.exit(0);
+        });
+    });
+});
