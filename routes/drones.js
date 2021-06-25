a31c7d2d02c426cde56d80c07c989e28e0d016fa
emmyuxui <emmyuxui@gmail.com>
2021-06-25 19:45:38 +0200

    Emmy & Kalande - Drones Lab

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,75 @@
 const express = require('express');
 const router = express.Router();
 
+const droneModel = require('../models/Drone.model')
+
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+   droneModel.find()
+  .then((drones) => {
+    res.render('drones/list.hbs', {drones})
+  })
+  .catch(() => {
+      next('Cannot list drones')
+  })
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body
+  droneModel.create({name, propellers, maxSpeed})
+        .then(() => {
+          res.redirect('/drones')
+        })
+        .catch(() => {
+          next('Did not receive drone info')
+        })
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  let dynamicDroneId = req.params.id
+
+  droneModel.findById(dynamicDroneId)
+    .then((drone) => {
+
+        res.render('drones/update-form.hbs', {drone})
+    })
+    .catch(() => {
+        next('Cannot find drone')
+    })
 });
+
+
+
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  let dynamicDroneId = req.params.id
+  const {name, propellers, maxSpeed} = req.body
+
+  droneModel.findByIdAndUpdate(dynamicDroneId, {name, propellers, maxSpeed})
+    .then(() => {
+        res.redirect('/drones')
+    })
+    .catch(() => {
+        next('Edit failed')
+    })
 });
 
+
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  let dynamicDroneId = req.params.id
+
+  droneModel.findByIdAndDelete(dynamicDroneId)
+    .then(() => {
+        res.redirect('/drones') 
+    })
+    .catch(() => {
+        next('Deleting specific todo failed')
+    })
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,22 @@
 // Iteration #1
+require('../db')
+
+const droneModel = require('../models/Drone.model')
+const mongoose = require('mongoose')
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+  
+droneModel.insertMany(drones)
+.then(() => {
+    console.log('Drones added')
+    mongoose.connection.close()
+})
+.catch(() => {
+    console.log('Drones not added')
+    mongoose.connection.close()
+})
+  
