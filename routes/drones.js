4331c866556f6e2ed1d4581973e3095d77d83270
beta <beta@kusarigama.local>
2021-06-26 21:56:12 +0200

    edit and delete are not working + bonus still needs to done

--- routes/drones.js
+++ routes/drones.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const DroneModel = require('../models/Drone.model');
-// require the Drone model here
-// let dynamicDroneId = req.params.id
-// const { name,propellers, maxSpeed } = req.body
 
+//get home page
 router.get('/drones', (req, res, next) => {
 
   DroneModel.find()
@@ -16,29 +14,81 @@ router.get('/drones', (req, res, next) => {
     })
 });
 
+// handling GET request to /drones/create
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
+// handling POST request to /drones/create
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body
+
+  DroneModel.create({name, propellers, maxSpeed})
+    .then(()=>{
+      res.redirect('/drones') 
+    })
+    .catch(()=>{
+      next('Create failed')
+    })
+
 });
 
+
+
+
+
+
+
+//asdasd
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  let dynamicDroneId = req.params.id
+
+  // grab all the drone details
+  // show them in a new edit form
+  DroneModel.findById(dynamicDroneId)
+    .then ((drones)=> {
+      //pass the drones value to the edit form
+      req.render('drones/update-form.hbs', {drones})
+    })
+    .catch(()=> {
+      next('Cannot find the damn drone details')
+    })
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  let dynamicDroneId = req.params.id
+  //destructure drone
+  const {name, propellers, maxSpeed } = req.body
+
+  DroneModel.findByIdAndUpdate(dynamicDroneId,{name, propellers,maxSpeed})
+  .then (()=> {
+    //redirect to start with slash from Guns n' Roses
+    res.redirect('/drones')
+  })
+  .catch (()=>{
+    next('Edit failed')
+  })
+  
+  
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  //create variable for dynamic ID
+  let dynamicDroneId = req.params.id
+
+  DroneModel.findByIdAndDelete(dynamicDroneId)
+    .then(()=>{
+      res.redirect('/drones')
+    })
+    .catch(()=>{
+      next('Failed to delet drone')
+    })
+  
 });
 
 module.exports = router;
