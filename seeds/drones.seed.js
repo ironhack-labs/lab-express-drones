08f1d2702f807d750e8d02448fc65dd31e2953e4
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-06-28 17:17:32 +0200

    "create model/seed"

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,6 @@
 // Iteration #1
+const drones = [
+    { name: "Madafacka Killa", propellers: 3, maxSpeed: 12 },
+    { name: "Speedy Gonzales", propellers: 4, maxSpeed: 20 },
+    { name: "Super Mistos", propellers: 6, maxSpeed: 18 }
+  ];
