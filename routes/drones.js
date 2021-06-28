317efe3401b32c2da1c2b67b501c738e3ff53417
Tiago Pascoa <tiagompascoa@gmail.com>
2021-06-28 18:52:28 +0100

    done

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,53 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model");
 
-router.get('/drones', (req, res, next) => {
+router.get('/drones', async (req, res) => {
   // Iteration #2: List the drones
-  // ... your code here
+  const drones = await Drone.find();
+  res.render("drones/list", { drones });
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get('/drones/create', async (req, res) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+
+  await Drone.create({
+    name,
+    propellers,
+    maxSpeed
+  });
+
+  res.redirect("/drones");
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:id/edit', async (req, res,) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const editDrone = await Drone.findByIdAndUpdate(req.params.id);
+  res.render("drones/update-form", editDrone);
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+  await Drone.findByIdAndUpdate(req.params.id, {
+    name,
+    propellers,
+    maxSpeed
+  });
+  res.redirect("/drones");
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete', async (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  await Drone.findByIdAndDelete(req.params.id);
+  res.redirect("/drones");
 });
 
 module.exports = router;
