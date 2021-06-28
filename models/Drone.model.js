0fd112b81a1d8f8089a12cf57da5f47dd88a1cb4
Benjamin Bertelsen <benjaminbertelsen@gmail.com>
2021-06-28 16:57:38 +0100

    solution

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+//Remember, the SCHEMA 📝 enforces a certain standard for how we want the dataMODEL 📊 to be in our DB (I think!)
+
+const { Schema, model } = require("mongoose");
+
+const droneSchema = new Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+  },
+  {
+  timestamps: true,
+})
+
+module.exports = model("Drone", droneSchema);
--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,47 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model");
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+
+router.get("/drones", async (req, res) => {
+  const drones = await Drone.find();
+  res.render("drones/list", { drones });
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.get("/drones/create", (req, res) => {
+  res.render("drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post("/drones/create", async (req, res) => {
+  const { name, propellers, maxSpeed } = req.body;             //we pass with .post using this method
+  await Drone.create({
+      name,
+      propellers,
+      maxSpeed,
+  });
+  res.redirect("/drones");
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.get('/drones/:id/edit', async (req, res, next) => {   
+  const droneFound = await Drone.findById(req.params.id);
+  res.render("drones/update-form", droneFound);
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post("/drones/:id/edit", async (req, res) => {                    
+  const { name, propellers, maxSpeed } = req.body;             
+  await Drone.findByIdAndUpdate(req.params.id, {
+      name,
+      propellers,
+      maxSpeed,
+  });
+  res.redirect("/drones");  
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+
+router.post("/drones/:id/delete", async (req, res) => {                  //we are already on the page, therefore no router.get first                   
+  await Drone.findByIdAndDelete(req.params.id)   
+  res.redirect("/drones");
 });
 
 module.exports = router;
