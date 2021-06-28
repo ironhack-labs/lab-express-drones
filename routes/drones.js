bac428fffa7a79725d33daefeb3392e3f115c31f
Gabriel Santos Dev <selvagedev@gmail.com>
2021-06-28 17:38:56 +0100

    iron may 21 solved

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require('../models/Drone.model')
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+router.get('/drones', async (req, res, next) => {
+  const drones = await Drone.find().sort({title: 1}); //-> ordem alfabética
+  res.render('drones/list', {drones});
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  console.log('create');
+  res.render("drones/create-form");
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+  Drone.create({ //create -> mongoose
+    name,
+    propellers,
+    maxSpeed,
+});
+res.redirect("/drones");  
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.get('/drones/:droneId/edit', async (req, res, next) => {
+    const droneToEdit = await Drone.findById(req.params.droneId);
+    console.log(droneToEdit);
+    res.render("drones/update-form", droneToEdit);
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post('/drones/:droneId/edit', async (req, res, next) => {
+  const {name, propellers, maxSpeed} = req.body;
+  await Drone.findByIdAndUpdate(req.params.droneId, {
+    name,
+    propellers,
+    maxSpeed,
+  });
+  res.redirect('/drones');
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+router.post('/drones/:droneId/delete', async (req, res, next) => {
+  await Drone.findByIdAndRemove(req.params.droneId);
+  res.redirect('/drones');
 });
 
 module.exports = router;
