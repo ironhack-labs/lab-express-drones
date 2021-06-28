8e2ea0ad84fb9cdef8d7c1a20ccd0efd6456a028
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-06-28 17:35:36 +0200

    model and seeds

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,18 @@
 // Iteration #1
+let dronesArray = [
+    {
+        name: "Drone number 1 - First Iteration",
+        propellers: 4,
+        maxSpeed: 40,
+    },
+    {
+        name: "Drone number 2 - First Iteration",
+        propellers: 5,
+        maxSpeed: 50,
+    },
+    {
+        name: "Drone number 3 - First Iteration",
+        propellers: 6,
+        maxSpeed: 60,
+    }
+]
