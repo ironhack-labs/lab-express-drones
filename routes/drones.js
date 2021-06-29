e9d7090fd01303c8d288fc8569a9afbd63706599
Fran Lara <francesc.lara@hotmail.com>
2021-06-29 12:01:42 +0200

    iteration 2 and 3

--- routes/drones.js
+++ routes/drones.js
@@ -4,18 +4,27 @@ const router = express.Router();
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+  .then(allDrones => res.render('./drones/list', {allDrones}))
+  .catch (err => console.log(err))
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  router.get('/create', (req, res, next) => {
+  res.render('./drones/create-form')
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body
+  Drone.create({name, propellers, maxSpeed})
+  .then (CreateNewDrone => {
+    console.log(CreateNewDrone)
+    res.redirect('/drones')
+  })
+  .catch (err => {
+    console.log(err)
+    res.redirect('/drones/create')
+  })
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
