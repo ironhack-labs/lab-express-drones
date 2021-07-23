0c706a35b32ca445d808358b8db3e3c34af6b3a9
Ghada <ghadamannai9@gmail.com>
2021-07-23 16:22:55 +0200

    iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,13 @@
-// Iteration #1
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const droneSchema = new Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+
+});
+
+const Drone = mongoose.model("Drones", droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,36 @@
-// Iteration #1
+const mongoose = require("mongoose");
+const Drones = require("../models/Drone.model");
+
+const droneData = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+    .then(async() => {
+        await Drones.deleteMany();
+        Drones.create(droneData)
+            .then((result) => {
+                console.log(result);
+                mongoose.connection.close().then(() => {
+                    console.log("Connection closed");
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    });
