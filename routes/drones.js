4297fda578da48509f1e95949206e82db84db92b
Chenjia Li <cligrenet@gmail.com>
2021-07-23 17:03:39 +0200

    init commit

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,69 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model');
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then((dbRes) => {
+      res.render('drones/list.hbs', {
+        drones: dbRes,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs');
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  Drone.create(req.body)
+    .then((createdDrone) => {
+      console.log(createdDrone);
+      res.redirect('/drones');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findById(req.params.id)
+    .then((dbRes) => {
+      res.render('drones/update-form.hbs', { drone: dbRes });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect('/drones');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  Drone.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect('/drones');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,40 @@
+const Drone = require('../models/Drone.model');
+const mongoose = require('mongoose');
+
 // Iteration #1
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+];
+
+const MONGO_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/lab-express-drones';
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    return Drone.deleteMany();
+  })
+  .then(() => {
+    Drone.create(drones)
+      .then((createdDrones) => {
+        console.log(createdDrones);
+        // mongoose.connection.close(() => {
+        //   console.log(`Successfully disconnected DB.`);
+        // });
+        console.log('I qm here');
+        mongoose.disconnect();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
