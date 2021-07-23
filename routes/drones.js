d1b8babef4c4f6f4f8057549cf69acebb75ea0e9
Jessie Panganiban <jessiepanganiban@gmail.com>
2021-07-23 18:14:55 +0200

    iteration 1 - 4

--- routes/drones.js
+++ routes/drones.js
@@ -1,31 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require('../models/Drone.model');
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+Drone.find()
+    .then((droneList) => {
+      res.render('drones/list.hbs', {
+        drones: droneList
+      });
+    })
+
+    .catch(e => console.log(e))
+  
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+Drone.create(req.body)
+    .then(() => {
+      res.redirect('/drones');
+
+    })
+
+    .catch(e => console.log(e))
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  //Drone.findOneAndUpdate
+Drone.findById(req.params.id)
+    .then((oneDrone) => {
+    res.render('drones/update-form.hbs', {
+      drone: OneDrone
+    })
+})
+
+    .catch(e => console.log(e))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+Drone.findbyIdAndUpdate(req.params.id, req.body)
+.then(() => {
+    res.redirect('/drones')
+})
+
+.catch(e => console.log(e))
+  res.redirect('/drones' + req.params.id + '/edit')
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,42 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+mongoose
+	.connect("mongodb://localhost/lab-express-drones", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+
+	.then((dbConnection) => {
+		console.log(`Connected to ${dbConnection.connection.name}`);
+
+		Drone.create(drones)
+			.then((createDrone) => {
+				console.log(createDrone.length)
+			})
+
+			.catch((error) => {
+				console.log(error);
+			});
+
+	})
+
+	.catch((error) => {
+		console.log("there is an error", error);
+
+
+
+
+	});
+
+
+
