13389dea2bb9f225332f73c9ab940e5c722d9b80
unknown <olivierakesso@gmail.com>
2021-07-24 22:03:13 +0200

    iteration 3 manque la derniere partie

--- routes/drones.js
+++ routes/drones.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Drone = require("../models/Drone.model");
 
 // require the Drone model here
-
+// get /drones then data from db then render .hbs file.
 router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
   Drone.find()
@@ -18,14 +18,14 @@ router.get("/drones", (req, res, next) => {
     });
 });
 
+// get /drones/create then render /drones/create-forms.hbs
 router.get("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form.hbs");
 });
 
 router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
 });
 
 router.get("/drones/:id/edit", (req, res, next) => {
