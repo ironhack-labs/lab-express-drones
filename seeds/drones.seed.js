ddce7ffcd5e698d2bf323ecbd69af3efcd9b81f6
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-08-02 19:17:14 +0200

    add seed file

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,24 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+const connectDB = require("../db/index");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+connectDB()
+  .then(() => {
+    Drone.DronedeleteMany().then(() => {
+      Drone.create(drones).then((drone) => {
+        console.log(`Created ${drone.length} Drones.`);
+        mongoose.connection.close();
+      });
+    });
+  })
+  .catch((err) => {
+    console.log("Error occured while inserting the drones", err);
+  });
