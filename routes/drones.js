6601ef6856b8688e7ff0a049444c05f9cfb7d9f9
samox_ <samirasteinmayer@web.de>
2021-08-04 11:38:17 +0200

    first steps prepared but cannot connect to local host...

--- routes/drones.js
+++ routes/drones.js
@@ -2,21 +2,38 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+// const DroneModel = require('../models/Drone.model');
+const Drone = require('../models/Drone.model')
 
+
+//GET /drones
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then((allTheDronesFromDB) => {
+      console.log('Retrieved drones from DB:', allTheDronesFromDB)
+      res.render('./drones/list', { allDrones: allTheDronesFromDB })
+    })
+    .catch((err) => {
+      console.log('error with finding DroneDB', err)
+    });
 });
 
+
+//GET /drones/create
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('rones/create-form',)
 });
 
+//POST /drones/create
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
-});
+  console.log(req.body)
+  Drone.create({ title: req.body.title, propellers: req.body.propellers, maxSpeed: req.body.maxSpeed })
+  .then(res.redirect('/drones'))
+})
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
