f49a72d70abf5c477dcab662ccbb1f30121ed60c
Nikhil shukla <nikhilshukla@Nikhils-MacBook-Pro.local>
2021-08-05 18:27:01 +0200

    done

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,61 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model')
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  Drone.find().then(allTheDronesFromDB => {
+    res.render('./drones/list', { allDrones: allTheDronesFromDB })
+  })
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render("./drones/create-form");
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  //
+  Drone.create({
+    name: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed,
+  }).then(() => {
+    res.redirect("/drones");
+  });
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  Drone.findById(req.params.id).then((drone) => {
+    res.render("./drones/update-form", { drone: drone });
+  });
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+
+  Drone.findByIdAndUpdate(req.params.id, {
+    name: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed,
+  }).then(() => {
+    res.redirect("/drones");
+  });
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  Drone.findByIdAndDelete(req.params.id).then(() => {
+    res.redirect("/drones");
+  });
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,24 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+  
+  Drone.create(drones).then((drones) => {
+      console.log("Created drones");
+
+      mongoose.connection.close();
+  });
