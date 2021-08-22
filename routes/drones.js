566c2106ac52c7a937e4c91bfc546b2a89544610
“Michael <michaelrodiles@gmail.com>
2021-08-22 13:19:03 -0400

    drones

--- routes/drones.js
+++ routes/drones.js
@@ -22,17 +22,21 @@ router.get('/drones', (req, res, next) => {
 
 router.get('/drones/create', (req, res, next) => {
 
-  res.render('drones/create-form.hbs')
+  res.render('drones/create-form')
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  Drone.create(req.body)
+    .then(() => {
+      res.redirect('/drones')
+    })
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findById(req.params.id)
+    .then((droneToBeEdited) => {
+      res.render("drones/update-form.hbs", droneToBeEdited);
+    })
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
@@ -40,9 +44,14 @@ router.post('/drones/:id/edit', (req, res, next) => {
   // ... your code here
 });
 
+
+
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  Drone.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect("/drones");
+    })
+    .catch(err => console.log("An error occurred while trying to delete drone", err))
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Drone = require("../models/Drone.model")
 
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
