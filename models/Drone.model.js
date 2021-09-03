7caeccfc1e24167427b5026ed988766e066af6a5
Juandi Mena <juandimena34@gmail.com>
2021-09-03 23:23:33 +0200

    lab done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
 // Iteration #1
+const { Schema, model} = require("mongoose");
+
+const droneSchema = new Schema({
+    name: String,
+    propeellers: Number,
+    maxSpeed: Number
+});
+
+const Drone = model("Drone", droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,38 @@
 // Iteration #1
+require ('../db')  //reuse the the same DataBase connection from our main app
+
+//Create Drone items to add to our collection
+
+const drones = [
+    { 
+    name: "Creeper XL 500",
+    propellers: 3,
+    maxSpeed: 12
+     },
+
+    { 
+    name: "Racer 57",
+    propellers: 4,
+    maxSpeed: 20
+     },
+
+    { 
+    name: "Courier 3000i",
+    propellers: 6,
+    maxSpeed: 18
+     }
+  ];
+
+// Insert Drone items to the DB
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model.js')
+
+Drone.create(drones)
+.then(() =>{
+    console.log("all is good");
+    mongoose.connection.close()
+})
+.catch(() => {
+console.log("something was wrong adding drones")
+})
+
