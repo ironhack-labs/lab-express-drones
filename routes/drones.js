08585ef969419e9154b48384ed1ddd82bfffccf7
Ana Martins <ana.af.martins@gmail.com>
2021-09-04 17:14:20 +0200

    lab done!
    Co-authored-by: Theadejong <Theadejong@users.noreply.github.com>
    Co-authored-by: monikageiger <monikageiger@users.noreply.github.com>

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,74 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model");
 
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  Drone.find()
+    .then((allDrones) => {
+      res.render("./drones/list", { allDrones });
+    })
+    .catch((err) => {
+      console.log("not a drone here!");
+    });
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("./drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  console.log({ name, propellers, maxSpeed });
+  console.log(req.body);
+  Drone.create({
+    name,
+    propellers,
+    maxSpeed,
+  })
+    .then((response) => res.redirect("/drones"))
+    .catch(() => console.log("infos not transmitted"));
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  Drone.findById(id)
+    .then((drone) => {
+      res.render("./drones/update-form", { drone });
+    })
+    .catch((err) => console.log("whoopsie!"));
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+  Drone.findByIdAndUpdate(id, {
+    name,
+    propellers,
+    maxSpeed,
+  })
+    .then((response) => res.redirect("/drones"))
+    .catch(() => console.log("infos not transmitted"));
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+
+  Drone.findByIdAndRemove(id)
+    .then((result) => {
+      res.redirect("/drones");
+    })
+    .catch((err) => {
+      console.log("oh noooooo");
+    });
 });
 
 module.exports = router;
