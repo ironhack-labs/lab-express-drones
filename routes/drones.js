fcf7a2050ec92ea35fee13db8a6415b31c985c8b
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-09-07 01:25:26 +0200

    Features: Iteration 2

--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require ("../models/Drone.model.js")
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  Drone.find ()
+    .then (dronesArray => {
+      console.log ("dronesArray", dronesArray);
+      res.render ("drones/list.hbs", {dronesArray});
+    })
+    .catch(error => {
+      console.log('Error while getting the books from the DB: ', error);
+    
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
 });
 
 router.get('/drones/create', (req, res, next) => {
