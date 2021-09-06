e047a215e32a1fe064199dd1555200ac89dbe3c9
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-09-07 01:16:15 +0200

    Features: iteration 1 done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+const mongoose = require('mongoose');
+require('../db');
+const Drone = require('../models/Drone.model');
+
+const drones = [
+	{name: "R2D2",
+	propellers: 4,
+    maxSpeed: 6
+	}, 
+	{name: "Balondefutbol",
+	propellers: 2,
+    maxSpeed: 166
+	}, 
+	{name: "RegaloDeTania",
+	propellers: 14,
+    maxSpeed: 2006
+
+	}
+]
+
+
+// Add here the script that will be run to actually seed the database (feel free to refer to the previous lesson)
+Drone.create(drones)
+.then((dronesArray) => {
+  console.log(`Created ${dronesArray.length} drones`);
+
+  // Once created, close the DB connection
+  mongoose.connection.close();
+})
+.catch((err) => console.log(`An error occurred while creating movies from the DB: ${err}`));
