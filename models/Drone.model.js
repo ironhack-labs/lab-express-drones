2d96049373ed728276c6c15a964472a81105e6df
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-06 14:58:29 +0200

    Iteration 1 completed

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,17 @@
 // Iteration #1
+// models/Drone.model.js
+const { Schema, model } = require('mongoose');
+
+const droneSchema = new Schema(
+	{
+		name: String,
+		propellers: Number,
+		maxSpeed: Number
+	},
+	{
+		timestamps: true
+	}
+);
+
+// Name of the DB and the schema
+module.exports = model('Drones', droneSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,24 @@
 // Iteration #1
+// To insert in "seeds/drones.seed.js"
+
+// to solve the mongoose problem that is not defined here but in the /db/index.js
+const mongoose = require('mongoose');
+// To create the DB
+// node ./seeds/drones.seed.js
+const drones = [
+	{ name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+	{ name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+	{ name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+require('dotenv/config');
+require('../db');
+
+const Drone = require('./../models/Drone.model');
+
+Drone.create(drones)
+	.then((dronesFromDB) => {
+		console.log(`Created ${dronesFromDB.length} drones into the DB`);
+		mongoose.connection.close();
+	})
+	.catch((err) => console.log(`An error occurred while creating drones from the DB: ${err}`));
