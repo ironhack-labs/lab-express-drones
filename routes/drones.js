0493622c2068f989a24c4cd8d758ecd4dcf94f33
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-09-07 11:55:46 +0200

    Features: Iterations done. Must improve styling

--- routes/drones.js
+++ routes/drones.js
@@ -13,7 +13,7 @@ router.get('/drones', (req, res, next) => {
       res.render ("drones/list.hbs", {dronesArray});
     })
     .catch(error => {
-      console.log('Error while getting the books from the DB: ', error);
+      console.log('Error while getting the drones from the DB: ', error);
     
       // Call the error-middleware to display the error page to the user
       next(error);
@@ -23,26 +23,64 @@ router.get('/drones', (req, res, next) => {
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render ("drones/create-form.hbs")
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+  //console.log (name, propellers, speed);
+  Drone.create({name,propellers,maxSpeed})
+    .then (drone => {
+      res.redirect("/drones");
+    })
+    .catch(error => {
+      console.log('Error while creating the drone: ', error);
+      res.redirect ("drones/create-form.hbs");
+    })
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  const droneToUpdate = req.params.id;
+  //console.log("droneToUpdate: ", droneToUpdate)
+  Drone.findById (droneToUpdate)
+    .then (drone => {
+      //console.log ("estedron: ", drone)
+      res.render ("drones/update-form.hbs", drone)
+    })
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  const droneToUpdate = req.params.id;
+  const { name, propellers, maxSpeed } = req.body;
+
+  Drone.findByIdAndUpdate (droneToUpdate, {name,propellers,maxSpeed} )
+    .then (drone => {
+      res.redirect("/drones")
+    })
+    .catch(error => {
+      console.log('Error while updating the drone: ', error);
+      res.redirect ("drones/update-form.hbs");
+    })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  const droneToDelete = req.params.id;
+  Drone.findByIdAndDelete (droneToDelete)
+    .then (drone => {      
+      res.redirect("/drones")
+    })
+   .catch(error => {
+      console.log('Error while deleting the drone: ', error);
+      res.redirect ("/drones");
+   })
 });
 
 module.exports = router;
