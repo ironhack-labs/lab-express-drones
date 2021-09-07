869494c893dc255f38ef861ffcb7ef832e5303bb
Ruben Corral <rubencorralsola@gmail.com>
2021-09-07 18:11:42 +0200

    lab finished without styling

--- routes/drones.js
+++ routes/drones.js
@@ -50,27 +50,24 @@ router.get("/drones/:id/edit", (req, res, next) => {
 
 router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  const droneToModId = req.params.id;
+  const { id } = req.params;
   const { name, propellers, maxSpeed } = req.body;
 
-  Drone.findByIdAndUpdate(
-    droneToModId,
-    { name, propellers, maxSpeed },
-    { new: true }
-  )
-    .then((modDrone) => {
-      console.log(`Drone ${modDrone.name}  modified`);
-      res.redirect("/drones");
-    })
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed }, { new: true })
+    .then(() => res.redirect("/drones"))
     .catch((error) => {
       console.log(error);
-      res.redirect("/drones/:id/edit");
+      res.redirect("/drones/{id}/edit");
     });
 });
 
-router.post("/drones/:id/delete", (req, res, next) => {
+router.get("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+
+  Drone.findByIdAndDelete(id)
+    .then(() => res.redirect("/drones"))
+    .catch((error) => next(error));
 });
 
 module.exports = router;
