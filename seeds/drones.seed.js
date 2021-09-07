ec7565c27c5536e75adcb6016908fe323f37fb0c
lauraAlabau <arlaura3@gmail.com>
2021-09-07 11:15:40 +0200

    Iteration 1 | Seed the database (View Compas)

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,56 @@
 // Iteration #1
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+const Drone = require('../models/Drone.model');
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+
+  const drones = [
+    { 
+        name: "Creeper XL 500", 
+        propellers: 3, 
+        maxSpeed: 12 
+    },
+    { 
+        name: "Racer 57", 
+        propellers: 4, 
+        maxSpeed: 20 
+    },
+    { 
+        name: "Courier 3000i", 
+        propellers: 6, 
+        maxSpeed: 18 
+    }
+  ];
+
+
+  Drone.create(drones)
+  .then(dronesFromDB => {
+    console.log(`Created ${dronesFromDB.length} drones`);
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(`An error occurred while creating books from the DB: ${err}`));
+
+
+
