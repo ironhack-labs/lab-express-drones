9ff4b20cf0fda77fe6b91ed6a2b08283a27cb26e
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-09-07 12:19:02 +0200

    It. 0-2

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,23 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+require("dotenv/config");
+require("../db");
+
+Drone.create(drones)
+  .then((dronesFromDB) => {
+    console.log(`Created ${dronesFromDB.length} drones`);
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch((err) =>
+    console.log(`An error occurred while creating drones from the DB: ${err}`)
+  );
