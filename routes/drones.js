747e7ef7242673ef37baba97a460342f57ee5a81
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-09-08 09:35:05 +0200

    WD082021 - Iván Romero Oyola
    done

--- routes/drones.js
+++ routes/drones.js
@@ -34,17 +34,37 @@ router.post('/drones/create', (req, res, next) => {
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+ 
+  Drone
+    .findById(id)
+    .then(droneToEdit => {
+      console.log(droneToEdit)
+      res.render('drones/update-form.hbs', { drone: droneToEdit }); // <-- add this line
+    })
+    .catch(error => next(error));
 });
+
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+ 
+  Drone
+    .findByIdAndUpdate(id, {  name, propellers, maxSpeed }, { new: true })
+    .then(() => res.redirect('/drones'))
+    
+    .catch(error => next(error));
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+ 
+  Drone.findByIdAndDelete(id)
+    .then(() => res.redirect('/drones'))
+    .catch(error => next(error));
 });
 
 module.exports = router;
