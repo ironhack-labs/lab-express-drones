2eb75840e3c03bdf8f1dcfec4d14c7944faf1d3c
alerodriguez162 <alerodriguez162@gmail.com>
2021-09-20 17:58:38 -0500

    fix deploy

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGODB_URL, {
 });
 
 const drones = [
-  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12, image: "/images/dron1.png" },
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12, image: "/images/dron1.jpg" },
   { name: "Racer 57", propellers: 4, maxSpeed: 20, image: "/images/dron2.jpg" },
   { name: "Courier 3000i", propellers: 6, maxSpeed: 18, image: "/images/dron3.jpg" },
 ];
