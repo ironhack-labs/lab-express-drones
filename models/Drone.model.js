581ea471ddb006551fd9f77f1f47e0d1be716307
faxvargas <fab.med1011@gmail.com>
2021-09-20 11:37:28 -0500

    iteracion 1 y 2 hecha

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,17 @@
 // Iteration #1
+// IMPORTACIONES 
+const mongoose = require("mongoose")
+// SCHEMA 
+const dronSchema = mongoose.Schema({
+    name:String,
+    propellers:Number,
+    maxSpeed:Number,
+},{
+    timestamps:true
+})
+
+// MODEL 
+const Drone = mongoose.model("Drone",dronSchema)
+
+// EXPORTACION
+module.exports= Drone
--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require("./../models/Drone.model")
 
-// require the Drone model here
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  Drone.find({})
+  .then((dbDrones)=>{
+    res.render("drones/list",{
+      DroneList : dbDrones
+    })
+ 
+  })
 });
 
 router.get('/drones/create', (req, res, next) => {
