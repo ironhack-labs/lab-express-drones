c6768c87fa4853c4cf4cbb342a62cd2c4836cade
Carlos Fung <carlos.fung@me.com>
2021-11-13 00:25:13 +0100

    testing

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,5 +1,5 @@
 // Iteration #1
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const Drone = require("../models/Drone.model");
 
@@ -10,32 +10,27 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
-
   })
-.then(() => {
-
-    Drone.insertMany([ 
-
-        { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
-        { name: "Racer 57", propellers: 4, maxSpeed: 20 },
-        { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  .then(() => {
+    Drone.insertMany([
+      { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+      { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+      { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
     ])
-    .then((names) => {
-
-        for (let i =0; i < names.length; i++){
-            console.log(names[i].name)
+      .then((names) => {
+        for (let i = 0; i < names.length; i++) {
+          console.log(names[i].name);
         }
-    })
-    .catch((error) => {
-        console.error("error creating drones", error)
-    }) 
-
-})
-.then(() => {
+      })
+      .catch((error) => {
+        console.error("error creating drones", error);
+      });
+  })
+  .then(() => {
     console.log(`Disconnecting from the database... `);
 
     mongoose.disconnect((err) => {
@@ -43,6 +38,4 @@ mongoose
         console.log("failed to disconnect");
       }
     });
-})
-
-
+  });
