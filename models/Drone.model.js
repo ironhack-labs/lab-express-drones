497592e7467ebd8dc25ea9128e2fb41a4bcfbffa
Greg Munro <gregmunro90@gmail.com>
2021-11-13 16:42:06 +0100

    iteration 1.1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const droneSchema = new Schema({
+  name: { type: String },
+  propellers: { type: Number },
+  maxSpeed: { type: Number },
+});
+
+const Drone = mongoose.model("Drone", droneSchema);
+module.exports = Drone;
