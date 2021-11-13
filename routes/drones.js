08a7cce4453828c0a8555e5ccb450bdb18ce5ef7
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-11-13 20:07:07 +0100

    iteration 2

--- routes/drones.js
+++ routes/drones.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model");
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+    Drone.find()
+    .then((drones) => {
+      console.log(drones);
+      res.render("drones/list", { drones });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
 });
 
 router.get('/drones/create', (req, res, next) => {
