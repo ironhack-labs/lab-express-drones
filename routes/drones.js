c5d6e939654ef47d8c3f56954eea60527e7f31aa
Amy Davies <amyloudav@gmail.com>
2021-11-14 15:51:14 +0100

    upto iteration 2

--- routes/drones.js
+++ routes/drones.js
@@ -1,34 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model");
 
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find({})
+    .then((drones) => {
+      console.log(drones);
+      res.render("drones/list", { drones });
+    })
+    .catch((error) => {
+      console.log("There is an error displaying the drones");
+    });
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,33 @@
-// Iteration #1
+const mongoose = require("mongoose");
+const DroneModel = require("../models/Drone.model");
+
+const MONGO_URI = "mongodb+srv://amyloudav:Littledevil1@cluster0.khlek.mongodb.net/test"
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+
+  const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+  
+  DroneModel.create(drones)
+  .then((x) => {
+    console.log(`Connected to Mongo!${x}`)
+  })
+  .then(() => mongoose.connection.close())
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+  newDrone();
+
+
+
+
