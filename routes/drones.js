4e9f8251aa5cc4e94ec740bae34ef79e7020c62e
Carlos Fung <carlos.fung@me.com>
2021-11-14 15:18:19 +0100

    Iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -5,6 +5,8 @@ const Drone = require("../models/Drone.model");
 const { db } = require("../models/Drone.model");
 
 
+
+
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
@@ -14,7 +16,7 @@ Drone.find( function(err, result) {
   
   if (err) { return console.log(err) }
             
-  res.render('drones/list', result)
+  res.render('drones/list', {dronesfound: result})
   
 })
 
@@ -24,12 +26,31 @@ Drone.find( function(err, result) {
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
-});
+  res.render('drones/create-form.hbs')
+})
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
-});
+  const {name, propellers, maxSpeed} = req.body
+
+  Drone.create( {name, propellers, maxSpeed} )
+  .then(newDrone => {
+    console.log(newDrone)
+  })
+  .catch((err) => console.log(err))
+  
+  Drone.find( function(err, result) {
+  
+    if (err) { return console.log(err) }
+              
+    res.render('drones/list', {dronesfound: result})
+    
+  })
+
+
+
+})
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
