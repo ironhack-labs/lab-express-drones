ffd2b5fc9386c00a128fc1ac31e54a087ac76284
phimanim <aliker.imane@gmail.com>
2021-11-20 11:13:36 +0100

    it 1&2 done

--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require('../models/Drone.model');
+
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+  .then((drones) => {
+    console.log(drones);
+    res.render("drones/list", { drones });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 });
 
 router.get('/drones/create', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,38 @@
 // Iteration #1
+const Drone = require('../models/Drone.model');
+const { create } = require('hbs');
+const mongoose = require("mongoose");
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+
+// the connexion fail when we put mongodb://localhost/lab-express-drones
+// replacing localhost with 127.0.0.1 solve the problem
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/lab-express-drones";
+  
+mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    })
+    .then((x) => {
+      console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    })
+    .then(() => Drone.create(drones))
+    .then((drones) => {
+        console.log(`Successfully created ${drones.length} drones`);
+        console.log(drones);
+      })
+    .then(() => {
+        mongoose.connection.close();
+      })
+    .catch((err) => {
+      console.error("Error connecting to mongo: ", err);
+    });
+  
