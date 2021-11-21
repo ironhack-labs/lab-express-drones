e5f7c01a5353fcb835d511ca2c153c33caeabf78
Philipp Kastl <picastle@posteo.de>
2021-11-21 21:51:40 +0100

    done lab. add iteration 5

--- routes/drones.js
+++ routes/drones.js
@@ -43,32 +43,36 @@ router.get("/:id/edit", async (req, res, next) => {
   // ... your code here
   try {
     const droneId = req.params.id;
-    const droneToEdit =  await Drone.findById(droneId)
-    res.render('drones/update-form', {droneToEdit})
-    
+    const droneToEdit = await Drone.findById(droneId);
+    res.render("drones/update-form", { droneToEdit });
   } catch (error) {
     console.error(`Error while routing to update-form-hbs: ${error}`);
   }
-
-  
 });
 
 router.post("/:id/edit", async (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
   try {
-    const droneId = req.params.id
+    const droneId = req.params.id;
     const newDrone = req.body;
     const droneToEdit = await Drone.findByIdAndUpdate(droneId, newDrone);
-    res.redirect('/')
+    res.redirect("/");
   } catch (error) {
-    console.error('Error while updating drone', error)
+    console.error("Error while updating drone", error);
   }
 });
 
-router.post("/:id/delete", (req, res, next) => {
+router.post("/:id/delete", async (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  try {
+    const droneId = req.params.id;
+    await Drone.findByIdAndRemove(droneId);
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error while trying to delete Drone from DB.", error);
+  }
 });
 
 module.exports = router;
