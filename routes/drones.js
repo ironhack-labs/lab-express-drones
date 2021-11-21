3bda80abdb0322775c090020e6193e3d2c58c6d9
Joana Brito <jobritomar@gmail.com>
2021-11-21 17:42:02 +0000

    final

--- routes/drones.js
+++ routes/drones.js
@@ -3,34 +3,55 @@ const router = express.Router();
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+const Drone = require('../models/Drone.model');
+
+//Use the Mongoose .find() method to retrieve all the drones. 
+//Display all the drones on the drones/list.hbs view.
+// Make sure you catch the error and output it to the terminal.
+
+router.get('/drones', async (req, res, next) => {
+  const drones = await Drone.find()
+  res.render('drones/list', {drones})
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+//Find the /drones/create GET route in routes/drones.js and render the drones/create-form.hbs view.
+
+router.get('/drones/create', async (req, res, next) => {
+  res.render("drones/create-form.hbs")
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+//Locate the /drones/create POST route in routes/drones.js and using req.body get all the info user submitted through the form.
+// Use this info to create a new drone in the database in the drones collection. 
+//Make sure you redirect to /drones if the new drone is successfully created. 
+//If there is an error, render again the view so the user can try again to create a drone.
+
+router.post('/drones/create', async (req, res, next) => {
+  const droneData = req.body;
+  await Drone.create(droneData);
+  res.redirect("/drones")
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+
+//Find the /drones/:id/edit GET route in routes/drones.js and render the drones/update-form.hbs view.
+ //Make sure you get the right drone from the database using the available id (hint: .findById()) and pass the drone object to the view.
+
+router.get('/drones/:id/edit', async (req, res, next) => {
+  const {id}= req.params;
+  const drone = await Drone.findById(id)
+ res.render("drones/update-form.hbs", {id,drone})
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
+router.post('/drones/:id/edit', async (req, res, next) => {
+  const {id}= req.params;
+  const updateUser= req.body;
+  const drone= await Drone.findByIdAndUpdate(id,updateUser);
+  res.redirect("/drones")
+})
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+router.post('/drones/:id/delete',async  (req, res, next) => {
+  const {id}= req.params;
+  const drone = await Drone.findByIdAndDelete(id)
+  res.redirect("/drones")
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,35 @@
 // Iteration #1
+
+//Use Mongoose schema and make sure that the Drone model has all the properties listed above 
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+ 
+//Establish a connection to the database. You can use the same code in db/index.js
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/lab-express-drones';
+ 
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+//Create an array of 3 objects, each with name, propellers and maxSpeed as our initial drones
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+//call the Drone model's .create() method with the array as an argument.
+//the .create() method successfully creates the drones collection, output (using console.log()) how many drones have been created. 
+//In case, the seeding of the database fails, catch the error and output it.
+
+Drone.create(drones)
+    .then(dronesFromDB => {
+        console.log(`Created ${dronesFromDB.length} drones`);
+        mongoose.connection.close();
+    })
+    .catch(err => console.log(`An error occurred while creating books from the DB: ${err}`));
+  
