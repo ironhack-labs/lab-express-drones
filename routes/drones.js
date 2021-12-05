1bdbe6a6177087e1512d13087b0f9c0b49d17e88
David Castillo <david@castillog.com>
2021-12-05 19:00:37 -0400

    iteration 2

--- routes/drones.js
+++ routes/drones.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-// require the Drone model here
+// require the Drone model
+const Drone = require('../models/drone.model');
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then(drones => {
+      console.log('Drones', drones);
+      res.render('drones/list.hbs', { drones });
+  })
+  .catch(err => {
+      console.log('Error',err);
+      res.send("Error al listar los drones");
+  })
+
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  
 });
 
 router.post('/drones/create', (req, res, next) => {
