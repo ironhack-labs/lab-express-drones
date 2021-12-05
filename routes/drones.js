83a9fdcc0dab52f873e73520fb0ed20c6f3612e7
Oswaldo Jose Aviles Ibarra <oswaldoavilesib>
2021-12-05 17:00:34 -0600

    Iteration 4 done

--- routes/drones.js
+++ routes/drones.js
@@ -39,11 +39,29 @@ router.post('/drones/create', (req, res, next) => {
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  const {id} = req.params;
+  Drone.findById(id)
+  .then((droneToUpdate)=>{
+    console.log(droneToUpdate)
+    res.render('drones/update-form',{drone:droneToUpdate})
+  })
+  .catch(error=>console.log('ERROR EN GETTING THE FORM TO UPDATE A DRONE',error))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  const {id} = req.params
+  const {name,propellers,maxSpeed} = req.body;
+  Drone.findByIdAndUpdate(id,{name,propellers,maxSpeed},{new:true})
+  .then((updatedDrone)=>{
+    console.log("UPDATED DRONE:",updatedDrone)
+    res.redirect('/drones')
+  })
+  .catch((err)=>{
+    console.log("ERROR AL UPDATEAR UN DRON: ",err)
+    res.render('drones/update-form')
+  })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
