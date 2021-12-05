f2776ef4bcd94afe8f5ed1fa77ee22ac12b15229
SerchPro <proyects_serch@hotmail.com>
2021-12-04 19:02:46 -0600

    iteration one done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+
+const { Schema, model } = require("mongoose");
+
+const DronSchema = new Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: {
+        type: Number,
+        max: 18
+    }
+}, { timestamps: true })
+
+
+module.exports = model("Drone", DronSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,29 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+const Drone = require("../models/Drone.model")
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    });
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 18 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+Drone.create(drones, err => {
+    if (err) {
+        throw err;
+    }
+
+    mongoose.connection.close()
+})
