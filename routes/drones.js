5555ec2605351f35fb4a9cdc5b68901d6892f3af
Sarah Ortega <sarah.ortega.fuentes@gmail.com>
2021-12-06 18:52:33 -0600

    iteration 1, 2, 3

--- routes/drones.js
+++ routes/drones.js
@@ -2,20 +2,43 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model.js");
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then(data => {
+      console.log("Data", data);
+      res.render("drones/list", {
+        "drones": data
+      })
+    }).catch(error => {
+      console.log('Error while getting drones from the DB: ', error);
+      next(error);
+    })
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {
+    name,
+    propellers,
+    maxSpeed,
+  } = req.body;
+
+  Drone.create({
+      name,
+      propellers,
+      maxSpeed
+    })
+    .then(() => res.redirect('/drones'))
+  console.log('Error while getting drones from the DB: ', error);
+  next(error);
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
