d7490fefb67ad850111b3b81cb8daa819088126e
SantiagoCortina <cortina.zapfe@gmail.com>
2021-12-06 20:26:23 -0600

    Primer commit, iteracion 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+const {Schema, model} = require('mongoose');
+
+const droneSchema = new Schema({
+    name: {
+        type:String
+    },
+    propellers:Number,
+    maxSpeed:{
+        type: Number,
+        min:0
+    }
+}, {timestamps:true})
+
+module.exports = model('Drone', droneSchema);
