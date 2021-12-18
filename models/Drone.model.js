87b954a570cd209ae8bb5da964ac6c2757be0bfd
carloscordon82 <carloscordon82@gmail.com>
2021-12-18 15:23:44 -0500

    Done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
-// Iteration #1
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const droneSchema = new Schema({
+  name: String,
+  propellers: Number,
+  maxSpeed: Number,
+});
+
+//   name - String (name of the drone model, like General Atomics MQ-9 Reaper)
+//   propellers - Number (amount of propellers, like 4)
+//   maxSpeed - Number (meters per second for the drone's maximum speed, like 18)
+
+const Drone = mongoose.model("Drone", droneSchema);
+module.exports = Drone;
--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Drone = require("../models/Drone.model");
 
-// require the Drone model here
-
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  console.log("DRONES");
+  Drone.find()
+    .then((results) => {
+      console.log("These are the results", results);
+      res.render("drones/list", { drones: results });
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.get("/drones/create", (req, res, next) => {
+  res.render("drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post("/drones/create", (req, res, next) => {
+  Drone.create(req.body)
+    .then((results) => {
+      console.log("These are the results", results);
+      res.redirect("/drones");
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+      res.render("drones/create-form", { error: "Error, please try again" });
+    });
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.get("/drones/:id/edit", (req, res, next) => {
+  Drone.findById(req.params.id)
+    .then((results) => {
+      console.log("These are the results", results);
+      res.render("drones/update-form", { drone: results });
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post("/drones/:id/edit", (req, res, next) => {
+  Drone.findByIdAndUpdate(req.params.id, req.body)
+    .then((results) => {
+      console.log("These are the results", results);
+      res.redirect("/drones");
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+      res.render("drones/update-form", { error: "Error, please try again" });
+    });
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+router.post("/drones/:id/delete", (req, res, next) => {
+  console.log("post");
+  Drone.findByIdAndDelete(req.params.id)
+    .then((results) => {
+      console.log("These are the results", results);
+      res.redirect("/drones");
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
 });
 
 module.exports = router;
