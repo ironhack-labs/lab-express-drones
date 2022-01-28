deac2a190ac497ff848e519523d4e561e281694b
Vitor Afonso <vittokm@hotmail.com>
2022-01-28 11:02:31 +0000

    iteration1 completed

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,19 @@
 // Iteration #1
+//jshint esversion:8
+
+const { Schema, model } = require("mongoose");
+
+const droneSchema = new Schema(
+  {
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+
+module.exports = model("Drone", droneSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,35 @@
+//jshint esversion:8
+
 // Iteration #1
+
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+const DroneModel = require("../models/Drone.model");
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+const listOfDrones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+DroneModel.create(listOfDrones)
+    .then(createdDrones => {
+        console.log('Successfully created',createdDrones.length,'drones.');
+        mongoose.connection.close();
+    })
+    .catch(err => console.log('Something went wrong while creating the list of drones, Error =>', err));
