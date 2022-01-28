a98bd917805d06e7d07aacdf31d18e951792c2b4
Vitor Afonso <vittokm@hotmail.com>
2022-01-28 11:22:11 +0000

    iteration2 completed

--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,18 @@
+//jshint esversion:8
 const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const DroneModel = require("../models/Drone.model");
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  DroneModel.find()
+    .then(dronesFromDB => {
+      console.log(dronesFromDB);
+      res.render('drones/list', {drones: dronesFromDB});
+    })
+    .catch(err => console.log('Ops!! Something went wrong while retrieving the drones from DB.'));
 });
 
 router.get('/drones/create', (req, res, next) => {
