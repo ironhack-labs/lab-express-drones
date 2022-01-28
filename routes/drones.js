70663d735eb976bb6aa25a30a3177dfd196626ba
Vitor Afonso <vittokm@hotmail.com>
2022-01-28 12:09:48 +0000

    iteration3 completed

--- routes/drones.js
+++ routes/drones.js
@@ -9,20 +9,31 @@ router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   DroneModel.find()
     .then(dronesFromDB => {
-      console.log(dronesFromDB);
+      //console.log(dronesFromDB);
       res.render('drones/list', {drones: dronesFromDB});
     })
-    .catch(err => console.log('Ops!! Something went wrong while retrieving the drones from DB.'));
+    .catch(err => console.log('Ops!! Something went wrong while retrieving drones from DB.'));
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form');
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  //console.log({name, propellers, maxSpeed});
+  DroneModel.create({name, propellers, maxSpeed})
+    .then( droneFromDB => {
+      console.log(`Drone name: ${droneFromDB.name} created.`);
+    })
+    .then(() => res.redirect('/drones'))
+    .catch(err => {
+
+      console.log('Ops!! Something went wrong while creating drone', err);
+      res.render('/drones/create');
+    });
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
