2f84358599ae6dd38d19ac23de648bc863cffdd2
Maria Gallego <mariagestionredes@gmail.com>
2022-01-29 11:29:44 +0100

    iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,19 @@
-// Iteration #1
+const mongoose = require("mongoose");
+
+const dronesSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    propellers : {
+        type : Number,
+        required : true
+    },
+    maxSpeed : {
+        type: Number,
+        required : true
+    }
+});
+
+const Drone = mongoose.model("Drone", dronesSchema);
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,16 @@
-// Iteration #1
+require("dotenv").config();
+require("../db/index");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+Drone.create(drones)
+  .then((drones) => console.log(`${drones.length} drones sucessfully created`))
+  .catch((err) => console.log(`Error while creating a new drone ${err}`))
+  .finally(() => {
+    process.exit();
+  })
