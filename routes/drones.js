6e67bd1d3c3d686af2f346c3b0e55b9be1ce1142
Sergio Villabrille <sergiovillabrille@hotmail.com>
2022-01-29 14:59:37 +0100

    Lab donete

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require("../models/Drone.model.js")
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then((drones) => {
+      res.render("drones/list", { drones });
+      console.log("drones were created")
+    })
+    .catch((e) => next(e));
+
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/createForm");
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  console.log(req.body);
+
+  Drone.create(req.body)
+    .then((drone) => {
+      res.redirect(`/drones`);
+    })
+    .catch((e) => next(e));
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  Drone.findById(req.params.id)
+  .then((drone) => {
+    res.render("drones/updateForm", drone);
+  })
+  .catch((e) => next(e));
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  Drone.findByIdAndUpdate(req.params.id, req.body)
+  .then((drone) => {
+    res.redirect(`/drones`);
+  })
+  .catch((e) => next(e));
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  Drone.findByIdAndDelete(req.params.id)
+  .then(() => {
+    res.redirect("/drones");
+  })
+  .catch((e) => next(e));
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,18 @@
 // Iteration #1
+
+require("dotenv").config();
+require("../db");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+Drone
+    .create(drones)
+    .then(() => {
+        console.log(`${drones.length} drones created `)
+    })
+    .catch((err) => console.log("Connection failed", err))
