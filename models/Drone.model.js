f3ced25129b1c4fa85c9a10fc9fecdb415c02936
AlejandroGC1990 <alejandro.garcia.carmona@gmail.com>
2022-01-29 11:19:23 +0100

    interaccion1Hecha

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,21 @@
-// Iteration #1
+const { Schema, model } = require("mongoose");
+
+const dronSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  propellers: {
+    type: Number,
+    required: true,
+  },
+  maxSpeed: {
+    type: Number,
+    required: true,
+  },
+  
+});
+
+const Drone = model("Dron", dronSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,25 @@
-// Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+//Establecer una conexión con la base de datos
+require('../db/index');
+
+
+//Cree una matriz de 3 objetos, cada uno con name, propellersy 
+//maxSpeedcomo nuestros drones iniciales.
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+//Una vez que se establece la conexión con la base de datos, 
+//llame al método Dronedel modelo .create()con la matriz como argumento.
+
+Drone.create(drones)
+    .then((results) => {
+      console.log('Success', results);
+        mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log('Something went wrong', err);
+    });
