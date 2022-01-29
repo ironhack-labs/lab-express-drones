18dd24b8e593745cbd4aa03dce3f496f7b08c76f
Robert Cabrera <cabrera.rch@gmail.com>
2022-01-29 11:13:01 +0100

    Iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,10 @@
-// Iteration #1
+const { Schema, model } = require("mongoose");
+
+const droneSchema = new Schema ({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+
+const Drone = model("Drone", droneSchema); // => "Drone" es el nombre del schema. El nombre de la variable puede ser cualquiera pero lo llamamos igual que el schema para entenderlo mejor.
+module.exports = Drone;
