b15acc79cd3de903f15d4956a18571cbc43c3c50
Oscar Sangrador <oscarsangrador@gmail.com>
2022-01-29 14:41:54 +0100

    iteration 2 done

--- routes/drones.js
+++ routes/drones.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model')
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+ 
+  Drone.find()
+  .then((result) => {
+    console.log(result);
+    res.render('drones/list', { drones: result })
+})
+  .catch((err) => console.log(err))
 });
 
 router.get('/drones/create', (req, res, next) => {
