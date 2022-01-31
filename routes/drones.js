9f6da1da7bffd0a11c9ac6f0765e8f20166519ce
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-01-31 17:29:19 +0100

    Iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // require the Drone model here
 const Drone = require("../models/Drone.model.js"); 
 
-//GET routw to retrieve and display all the drones
+//GET route to retrieve and display all the drones
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   Drone.find()
@@ -22,14 +22,19 @@ router.get('/drones', (req, res, next) => {
     });
 });
 
+// GET route to display the form
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs');
 });
 
+// POST get all the info user submitted through the form. Use this info to create a new drone in the database in the drones collection
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propeller, maxSpeed } = req.body;
+  Drone.create({ name, propeller, maxSpeed })
+    .then(() => res.redirect("/drones"))
+    .catch((error) => next(error));
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
