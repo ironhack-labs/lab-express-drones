d921edd3cc753e9956fa91d92133fde1e2247af8
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-01-31 21:22:57 +0100

    Edit is working, but not redirecting the /drones page

--- routes/drones.js
+++ routes/drones.js
@@ -8,15 +8,11 @@ router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   Drone.find()
   .then(allTheDronesFromDB => {
-    // -> allTheBooksFromDB is a placeholder, it can be any word
-    //console.log('Retrieved drones from DB:', allTheDronesFromDB);
 
     res.render('drones/list.hbs', { drones: allTheDronesFromDB});
   })
   .catch(error => {
     console.log('Error while getting the droness from the DB: ', error);
-
-    // Call the error-middleware to display the error page to the user
     next(error);
   });
 });
@@ -28,7 +24,7 @@ router.get('/drones/create', (req, res, next) => {
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  console.log(req.body);
+  //console.log(req.body);
   const { name, propellers , maxSpeed } = req.body;
   Drone.create({ name, propellers , maxSpeed })
   .then(() => res.redirect('/drones'))
@@ -48,21 +44,25 @@ router.get('/drones/:id/edit', (req, res, next) => {
       .catch(error => next(error));
 });
 
-/*router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  const { droneId } = req.params;
-    const {  name, propellers , maxSpeed  } = req.body;
+  const { id } = req.params;
+  const {  name, propellers , maxSpeed  } = req.body;
    
-    Drone.findByIdAndUpdate(droneId, {  name, propellers , maxSpeed  }, { new: true })
-      .then(updatedDrone => res.redirect(`/drones/${updatedDrone.id}`)) // go to the details page to see the updates
-      .catch(error => next(error));
-  });*/
-   
+  Drone.findByIdAndUpdate(id, { name, propellers , maxSpeed }, { new: true })
+  .then(updatedDrone => res.redirect(`'/drones'${updatedDrone.id}`))
+  .catch(error => next(error));
+});
   
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+   
+  Drone.findByIdAndDelete(id)
+    .then(() => res.redirect('/drones'))
+    .catch(error => next(error));
+  
 });
 
 module.exports = router;
