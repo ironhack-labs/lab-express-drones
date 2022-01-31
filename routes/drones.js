09a0e72ef902f71ab8d136015d3b302d63642a49
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-01-31 16:59:38 +0100

    Iteration 2

--- routes/drones.js
+++ routes/drones.js
@@ -2,10 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model.js"); 
 
+//GET routw to retrieve and display all the drones
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then((allDronesFromDB) => {
+      console.log("Retrieved drones from DB:", allDronesFromDB);
+
+      // we call the render method after we obtain the drones data from the database
+      res.render("drones/list.hbs", { drones: allDronesFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting the books from the DB: ", error);
+
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
 });
 
 router.get('/drones/create', (req, res, next) => {
