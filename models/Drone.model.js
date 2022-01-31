d7e3deeff1a2ac5675da916e8cc70f2d43d65c9d
Clara Villa <Clara.Villa@lego.com>
2022-01-31 13:52:13 +0000

    iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,25 @@
 // Iteration #1
+
+const { Schema, model } = require("mongoose");
+
+const dronesSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    propellers: {
+      type: Number,
+      required: true,
+    },
+    maxSpeed: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Drones", dronesSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,26 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+const Drones = require("../models/Drone.model");
+
+//connection with DB
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+1;
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+Drones.create(drones)
+  .then((insertedDrones) => {
+    console.log(`Created ${insertedDrones.length} drones.`);
+    mongoose.disconnect().then(() => console.log("connection closed"));
+  })
+  .catch((error) => console.log(error));
