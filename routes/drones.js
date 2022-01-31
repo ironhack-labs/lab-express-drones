41b331fc6b283775da43aa4463134003126af23f
Chris Fagg <christopher.james.fagg@lego.com>
2022-01-31 15:27:51 +0000

    done

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,65 @@
 const express = require('express');
+const res = require('express/lib/response');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model.js');
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+  .then(responseFromDB => {
+    console.log(responseFromDB);
+    res.render('drones/list.hbs', { drones: responseFromDB});
+  })
+  .catch(error => {
+    console.log('Error while retrieving drones list from db: ', error);
+
+    next(error);
+  })
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs');
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+
+  Drone.create({name, propellers, maxSpeed})
+  .then(() => res.redirect("/drones"))
+  .catch(error => next(error));
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  /*const { droneId } = req.params.id; //why do I need the .id bit here when I didn't for books? Gets a cast to object failed for value error */ 
+  
+  Drone.findById(req.params.id)
+  .then(responseFromDB => {
+    res.render('drones/update-form.hbs', { drone: responseFromDB});
+  })
+  .catch(error => next(error));  
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  //const { droneId } = req.params.id; //this method will not work either using params or id
+  //console.log(droneId); // returns undefined
+  const {name, propellers, maxSpeed} = req.body;
+  
+  Drone.findByIdAndUpdate(req.params.id, {name, propellers, maxSpeed}, {new: true})
+    .then(updatedDrone => res.redirect("/drones"))
+    .catch(error => res.render(`/drones/${req.params.id}/edit`));
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  Drone.findByIdAndRemove(req.params.id)
+  .then(() => res.redirect("/drones"))
+  .catch(error => next(error));
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,28 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/lab-express-drones';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //useFindAndModify: false, //these have been deprecated
+  //useCreateIndex: true
+});
+
+const drones = [
+    { name: "Chris the Drone", propellers: 8, maxSpeed: 50 },
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  Drone.create(drones)
+  .then(dronesFromDB => {
+    console.log(`Created ${dronesFromDB.length} drones`);
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(`An error occurred while creating drones from the DB: ${err}`));
