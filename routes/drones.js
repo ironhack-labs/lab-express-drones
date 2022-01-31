4ffad48a53aa6d489d90b8c65597919eae99c85f
David Vekony <dave.vekony@gmail.com>
2022-01-31 14:50:51 +0000

    iteration 4 done

--- routes/drones.js
+++ routes/drones.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// require the Drone model here
 const Drone = require("../models/Drone.model");
 
 router.get("/drones", (req, res, next) => {
@@ -28,14 +27,27 @@ router.post("/drones/create", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-router.get("/drones/:id/edit", (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.get("/drones/:droneId/edit", (req, res, next) => {
+  const { droneId } = req.params;
+
+  Drone.findById(droneId)
+    .then((droneToEdit) => {
+      res.render("drones/update-form.hbs", { drone: droneToEdit });
+    })
+    .catch((error) => next(error));
 });
 
-router.post("/drones/:id/edit", (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post("/drones/:droneId/edit", (req, res, next) => {
+  const { droneId } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+
+  Drone.findByIdAndUpdate(
+    droneId,
+    { name, propellers, maxSpeed },
+    { new: true }
+  )
+    .then(() => res.redirect("/drones")) // go to the details page to see the updates
+    .catch((error) => next(error));
 });
 
 router.post("/drones/:id/delete", (req, res, next) => {
