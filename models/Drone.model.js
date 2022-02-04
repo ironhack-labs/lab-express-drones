3f57cbabaeb06988795ddd1eefe9a8106bd2296d
Mario Diaz <mariodzls@gmail.com>
2022-02-04 15:58:37 +0100

    Paso 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+const {Schema, model} = require('mongoose')
+
+const dronSchema = new Schema (
+    {
+        name: String,
+        propellers: Number,
+        maxSpeed: Number
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = model('Dron', dronSchema)
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,29 @@
 // Iteration #1
+const mongoose = require('mongoose')
+const Dron = require('../models/Drone.model.js')
+
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/lab-express-drones'
+
+mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+        console.log(
+            `Connected to Mongo! Database name: "${x.connections[0].name}"`
+        );
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    })
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+]
+
+Dron.create(drones)
+    .then(dronesFromDB => {
+        console.log(`Created ${dronesFromDB.length} drones`)
+        mongoose.connection.disconnect()
+    })
+    .catch(err => console.log(`An error occurred while creating drones from the DB: ${err}`))
