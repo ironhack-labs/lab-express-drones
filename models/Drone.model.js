3d49b39b06d894351ab717c54013d5d5f0d0cf04
Emanueljuarez <zerauleuname@gmail.com>
2022-02-04 16:50:18 -0600

    Iteracion 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,25 @@
 // Iteration #1
+
+//importacion
+const mongoose = require("mongoose")
+
+// schema
+
+const droneSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    propellers:{
+        type: Number,
+        required: true
+    },
+    maxSpeed:{
+        type: Number,
+        required: true
+    }
+})
+
+const Drone = mongoose.model("Drone", droneSchema)
+
+module.exports = Drone
