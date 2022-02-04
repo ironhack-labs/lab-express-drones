d09128008d024e4330399138ef4361e64c8153b1
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-02-04 16:51:51 -0600

    Iteration 3 completed

--- routes/drones.js
+++ routes/drones.js
@@ -12,18 +12,28 @@ router.get("/drones", async (req, res) => {
       data: allDrones,
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
 
-router.get("/drones/create", (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.get("/drones/create", async (req, res) => {
+  try {
+    res.render("drones/create-form");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.post("/drones/create", (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post("/drones/create", async (req, res) => {
+  console.log("Sending data to form");
+  const { name, propellers, maxSpeed } = req.body;
+  try {
+    const newDrone = await Drone.create({ name, propellers, maxSpeed });
+    console.log(newDrone)
+    return res.redirect("/drones");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/drones/:id/edit", (req, res, next) => {
