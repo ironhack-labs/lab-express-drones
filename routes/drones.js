4d0ec3e16b0b8528698ae35a76535ae5d3c390fd
Guillermo Rodríguez Calero <guillermorodriguezcalero@MacBook-Air-de-Guillermo.local>
2022-02-05 13:26:13 +0100

    lab

--- routes/drones.js
+++ routes/drones.js
@@ -3,19 +3,30 @@ const router = express.Router();
 
 // require the Drone model here
 
+const Drone = require('../models/Drone.model')
+
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone
+    .find()
+    .then(drones => res.render('drones/list', { drones }))
+    .catch(err => console.log(err))
+
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get('/list/drone', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body
+
+  Drone
+    .create({ name, propellers, maxSpeed })
+    .then(() => res.redirect('/drones'))
+    .catch(err => console.log(err))
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,40 @@
 // Iteration #1
+const { process_params } = require('express/lib/router')
+const mongoose = require('mongoose')
+
+const Drone = require('../models/Drone.model')
+
+const MONGO_URI = 'mongodb://localhost/drones22';
+
+mongoose
+
+    .connect(MONGO_URI)
+    .then((Drone) => {
+        console.log('drones connected to Mongo database name:"${drone.connections[0].name} ')
+    })
+
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    });
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+
+Drone
+
+    .create(drones)
+    .then(dronesFromDB => {
+
+        console.log(`Created ${dronesFromDB.length} drones`)
+
+        mongoose.connection.close()
+    })
+
+
+
+
+
