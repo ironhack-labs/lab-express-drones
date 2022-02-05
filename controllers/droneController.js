3c25a5b0feba72820e6f19f8d92fbb14deca62b3
Emanueljuarez <zerauleuname@gmail.com>
2022-02-04 19:04:11 -0600

    Iteracion 3

--- controllers/droneController.js
+++ controllers/droneController.js
@@ -16,3 +16,33 @@ exports.getDrones = async (req, res) => {
   
       }
   };
+
+exports.createDrones = async (req,res) => {
+    return res.render("drones/create-form")
+}
+
+
+exports.createDronesForm = async (req, res) => {
+
+	// 1. VERIFICAR QUE LOS DATOS DEL FORMULARIO LLEGUEN AL SERVIDOR
+	const { name, propellers, maxSpeed } = req.body
+
+	// 2. CREAR EL DOCUMENTO EN BASE DE DATOS
+
+	try {
+		
+		await Drone.create({ name, propellers, maxSpeed })
+
+		return res.redirect("/drones")	
+
+	} catch (error) {
+		
+		console.log(error)
+
+		return
+
+	}
+	
+
+
+}
--- routes/drones.js
+++ routes/drones.js
@@ -5,24 +5,37 @@ const router = express.Router();
 const droneController = require("./../controllers/droneController")
 // require the Drone model here
 
+
+
+router.get("/", droneController.getDrones);
+
+router.get("/create", droneController.createDrones);
+
+router.post("/create", droneController.createDronesForm);
+
+// router.get("/", droneController.getDrones);
+
+// router.get("/", droneController.getDrones);
+
+// router.get("/", droneController.getDrones);
+
+
+
+
 // router.get('/drones', async (req, res, next) => {
 //   // Iteration #2: List the drones
 //   // ... your code here
 // });
 
-router.get("/", droneController.getDrones);
+// router.get('/drones/create', (req, res, next) => {
+//   // Iteration #3: Add a new drone
+//   // ... your code here
+// });
 
-
-
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
-
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
+// router.post('/drones/create', (req, res, next) => {
+//   // Iteration #3: Add a new drone
+//   // ... your code here
+// });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
