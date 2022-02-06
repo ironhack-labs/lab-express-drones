1fb99602a4871bf7195778c2d91a5697f0da56c1
Diego V <egovaldel@gmail.com>
2022-02-06 13:35:08 +0100

    done

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,54 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // require the Drone model here
+const Drone = require('../models/Drone.model')
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
-});
+  Drone.find()
+    .then((listDrones) => {
+      // console.log(listDrones)
+      res.render('drones/list', { listDrones })
+    })
+    .catch((err) => console.error(err))
+})
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
-});
+  res.render('drones/create-form')
+})
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
-});
+  const { name, propellers, maxSpeed } = req.body
+  Drone.create({ name, propellers, maxSpeed })
+    .then(res.redirect('/drones'))
+    .catch((err) => console.log(err))
+})
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
-});
+  Drone.findById(req.params.id)
+    .then((drone) => {
+      res.render('drones/update-form', drone)
+    })
+    .catch((err) => console.log(err))
+})
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
-});
+  const { name, propellers, maxSpeed } = req.body
+  Drone.findByIdAndUpdate(req.params.id, { name, propellers, maxSpeed })
+    .then(res.redirect('/drones'))
+    .catch((err) => console.log(err))
+})
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
-});
+  Drone.findByIdAndDelete(req.params.id)
+    .then(res.redirect('/drones'))
+    .catch((err) => console.log(err))
+})
 
-module.exports = router;
+module.exports = router
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,22 @@
-// Iteration #1
+const mongoose = require('mongoose')
+const Drone = require('../models/Drone.model')
+
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/drones'
+
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+]
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => 'Connected')
+  .catch((err) => console.log(err))
+
+Drone.create(drones)
+  .then((drone) => {
+    console.log('Importados ', drone.length)
+    mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))
