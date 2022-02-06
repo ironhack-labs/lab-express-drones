b937d375ed233833eae44d2eba72ac4711783618
Estefania Lamas <esteflamas@gmail.com>
2022-02-06 21:19:44 +0100

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,14 @@
 // Iteration #1
+
+const { Schema, model } = require("mongoose");
+
+//conectarse a la base de datos
+
+const droneSchema = new Schema({
+  name: String,
+  propellers: Number,
+  maxSpeed: Number,
+});
+
+//Hay que exportar el modelo
+module.exports = model("drone", droneSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,37 @@
-// Iteration #1
+
+
+// Requerimos mongoose e importamos el modelo
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+//Conexión a la BBDD
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Soy el seed Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+//Seed the database
+
+Drone.create(drones)
+  .then((x) => {
+    console.log(`Created ${x.length} drones`);
+    mongoose.connection.close();
+  })
+  .catch((err) =>
+    console.log(`An error occurred while creating movies from the DB: ${err}`)
+  );
