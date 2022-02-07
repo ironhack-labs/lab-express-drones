3b335a5e9fed1abc8eb75101302952ac16622d20
Miguel <info.miguelbanos@gmail.com>
2022-02-07 21:33:51 +0100

    iteration 1, 2, 3, 4, and 5 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,20 @@
 // Iteration #1
+const { Schema, model } = require("mongoose");
+
+const droneSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    propellers:{
+        type: Number,
+        required: true
+    },
+    maxSpeed:{
+        type: Number,
+        required: true
+    }
+});
+
+const Drone = model("Drone", droneSchema);
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,17 @@
 // Iteration #1
+require("dotenv").config();
+require("../db/index");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  Drone.deleteMany()
+  .then(() => Drone.create(drones))
+  .then((drones) => console.log(`${drones.length} created`))
+  .finally(() => {
+    process.exit();
+  });
