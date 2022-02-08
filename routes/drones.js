3102efc4133fde04a09cb75e009b1c2c64097c17
Chris Castle <chrisjcastle93@gmail.com>
2022-02-08 19:52:21 +0100

    Initial Commit

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,87 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Drone = require("../models/Drone.model");
+const mongoose = require('mongoose');
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then((results) => res.render("drones/list", { results }))
+    .catch((err) => console.log(err));
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, speed } = req.body;
+  Drone.create({
+    name: name,
+    propellers: propellers,
+    speed: speed,
+  })
+    .then((result) => {
+      console.log("CREATED NEW DRONE CALLED", name);
+      res.redirect("/drones");
+      // mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("drones/create-form");
+    });
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  Drone.findById(id)
+    .then((result) => {
+      res.render("drones/update-form", { result });
+      // mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("back");
+    });
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id, name, propellers, speed } = req.body;
+  Drone.findOneAndUpdate(
+    { id: id },
+    { name: name, propellers: propellers, maxSpeed: speed },
+    { new: true }
+  )
+    .then((result) => {
+      res.redirect("/drones");
+      console.log('Updated drone with (new) name', name);
+      // mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("back");
+    });
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+  Drone.findOneAndDelete(id)
+    .then((result) => {
+      console.log("Removed drone with name", result.name);
+      res.redirect("/drones");
+      // mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("back");
+    });
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,32 @@
 // Iteration #1
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/lab-express-drones", {
+    useNewUrlParser: true,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
+
+Drone.deleteMany({}).then(console.log("Deleted all drones."));
+
+Drone.create(drones)
+  .then((result) => {
+    return console.log(result.length);
+  })
+  .then((result) => mongoose.connection.close())
+  .catch((err) => console.log(err));
