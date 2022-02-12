66c122ee1c4c9782ff15e4979e98c7e0fe4486f8
ricomasgu <ricard.comas.gomez@gmail.com>
2022-02-12 12:19:20 +0100

    Iteration 4

--- routes/drones.js
+++ routes/drones.js
@@ -43,11 +43,28 @@ router.post('/drones/create', (req, res, next) => {
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  let err = "hidden";
+  if(req.query.error==="true"){
+    err = "";
+  }
+  Drone.findById(req.params.id)
+      .then(droneFromDB => res.render("drones/update-form", { error: err, drone: droneFromDB } ))
+      .catch(err => console.log(`Error finding the drone with id ${req.params.id} in the database: `, err));
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  const { propellers, maxSpeed } = req.body;
+  Drone.findByIdAndUpdate(req.params.id, { propellers , maxSpeed })
+      .then(resDroneUpdated => {
+        console.log("User updated a new Drone.", resDroneUpdated);
+        res.redirect("/drones");
+      })
+      .catch(err => {
+        console.log("Error while creating a new drone: ", err);
+        res.redirect(`./${req.params.id}/edit?error=true`);
+      });
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
