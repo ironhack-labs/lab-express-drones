38bbde64dfa26b507371d6038006d48bdff130ff
Hollie Duncan <holduncan7@gmail.com>
2022-02-14 22:00:41 +0000

    Complete and styled, issue with footer

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,122 @@
 const express = require('express');
 const router = express.Router();
 
+const Drone = require('../models/Drone.model')
+
 // require the Drone model here
+
+// All drones
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+    Drone.find()
+    .then((allDrones) => {
+
+      res.render('./drones/list', {drones: allDrones})
+
+    })
+    .catch((err) => {
+
+      console.log('there has been an error', err)
+      next(err);
+    
+    })
 });
+
+
+// Create Drones
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('./drones/create-form')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+
+  Drone.create({ name, propellers, maxSpeed })
+  .then((createdDrone) => {
+
+      console.log('Drone Created:', createdDrone.name);
+      res.redirect('/drones')
+
+  })
+  .catch((err) => {
+
+    console.log('there has been an error', err)
+    next(err);
+    res.redirect('/drones/create')
+  
+  })
+
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+
+// Edit Drones
+
+router.get('/drones/:droneId/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {droneId} = req.params;
+
+  Drone.findById(droneId)
+  .then((editDrone) => {
+
+    res.render('./drones/update-form', {drone: editDrone})
+
+  })
+  .catch((err) => {
+    console.log('there has been an error', err)
+    next(err);
+  
+  })
+  
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:droneId/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {droneId} = req.params;
+  const {name, propellers, maxSpeed} = req.body;
+
+  Drone.findByIdAndUpdate(droneId, {name, propellers, maxSpeed})
+  .then((updatedDrone) => {
+
+    console.log(`${updatedDrone._id}`)
+
+    res.redirect(`/drones`)
+
+  })
+  .catch((err) => {
+
+    console.log('there has been an error', err)
+    next(err);
+    res.redirect(`/drones/${updatedDrone._id}/edit`)
+  
+  })
+
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+
+// Delete Drones
+
+router.post('/drones/:droneId/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const {droneId} = req.params;
+
+  Drone.findByIdAndDelete(droneId)
+  .then(()=> {
+
+    res.redirect('/drones')
+
+  })
+  .catch((err) => {
+
+    console.log('there has been an error', err)
+    next(err);
+  
+  })
+
+
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+
+const Drone = require('../models/Drone.model');
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+Drone.create(drones)
+.then((dronesFromDb) => {
+
+console.log(`added ${dronesFromDb.length} drones to db`)
+mongoose.disconnect(() => console.log('Database Closed'))
+
+})
+.catch((err) => console.log('there has been an error', err))
