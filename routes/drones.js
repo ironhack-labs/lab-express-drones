4895c6990cce5d4de7b669e0cb97b20fe49756f0
joeydegori <96920177+joeydegori@users.noreply.github.com>
2022-03-26 16:08:14 -0400

    done with 1 and 2

--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const Drone = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+  .then((dronesFromDB) => {
+    res.render("drones/list.hbs")
+  })
+  .catch((err) => console.log(`${err}`))
 });
 
 router.get('/drones/create', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require('../models/Drone.model')
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  Drone.create(drones)
+    .then((dronesFromDB) => {
+        console.log('This is the total number of drones in the DB: ', dronesFromDB.length)
+        mongoose.connection.close()
+    })
+    .catch((err) => console.log(`Error while seeding the database ${err}`))
