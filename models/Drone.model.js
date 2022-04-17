e6000fd7ff9da2a730b540d13064a7b2b46b7d58
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-04-17 21:45:54 +0200

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,13 @@
-// Iteration #1
+const { Schema, model } = require('mongoose')
+
+const droneSchema = new Schema(
+    {
+        name: String,
+        image: String,
+        propellers: Number,
+        maxSpeed: Number
+    },
+    { timestamps: true }
+)
+
+module.exports = model('Drone', droneSchema)
