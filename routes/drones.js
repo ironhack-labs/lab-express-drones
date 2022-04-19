8beef9cde78850413e0a60b1b01fbf051e30f84c
LucasPerezDev <LucasPerezDev@gmail.com>
2022-04-19 14:44:26 -0400

    completed all iterations

--- routes/drones.js
+++ routes/drones.js
@@ -37,17 +37,36 @@ router.post('/drones/create', (req, res, next) => {
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+ 
+  Drone.findById(id)
+    .then(droneToEdit => {
+      console.log(droneToEdit)
+      // console.log(bookToEdit);
+      res.render('drones/update-form.hbs', droneToEdit); // <-- add this line
+    })
+    .catch(error => next(error));
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+ 
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed }, { new: true })
+    .then(updatedDrone => {
+      console.log(updatedDrone)
+      res.redirect(`/drones`)})
+    .catch(error => next(error));
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+ 
+  Drone.findByIdAndDelete(id)
+    .then(() => res.redirect('/drones'))
+    .catch(error => next(error));
 });
 
 module.exports = router;
