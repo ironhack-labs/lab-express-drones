ccdc920e5cd2ba2c199020b24d5578680b53ba17
Leopoldo Navarrete <Leopoldo12@outlook.com>
2022-04-26 13:06:16 -0500

    Iteration #2 Completo

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,10 @@
 // Iteration #1
+const { schema, model } = require ("mongoose");
+
+const DroneSchema = new schema({
+    name: String,
+    propeller: Number,
+    maxSpeed: Number,
+});
+
+module.exports = model("drones", DroneSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,25 @@
 // Iteration #1
+const nuevosDrones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+        console.log("Conection complete");
+
+        Drone.create(nuevosDrones)
+        .then((drones) => {
+            console.log("Create", drones);
+        })
+        .catch(console.log)
+    })
+    .catch(console.log)
