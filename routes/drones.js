055ff43c142da88d930c42537ebe8cf7d7a78bef
niwatoriDev <esmunozdev@gmail.com>
2022-04-27 19:40:50 -0500

    Iteration 1, 2 and 3 ready. Iteration 4: Already conected to each id drone page, form needs to be created.

--- routes/drones.js
+++ routes/drones.js
@@ -1,34 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const droneModel = require("../models/Drone.model");
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+router.get("/drones", (req, res, next) => {
+  droneModel
+    .find()
+    .then((findedDrones) => {
+      res.render("drones/list", { data: findedDrones });
+    })
+    .catch((err) => console.log(err));
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.get("/drones/create", (req, res, next) => {
+  res.render("drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post("/drones/create", (req, res, next) => {
+  droneModel.create(req.body)
+  .then(nuevoDrone => {
+    console.log(nuevoDrone)
+    res.redirect("/drones")
+})
+.catch(err => {
+  alert("Hubo un error, vuelve a intentarlo")
+  res.render("/drones/create")
+})
+})
+
+router.get("/drones/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+
+  droneModel.findById(id)
+  .then(resultadoDrone => {
+    res.render("drones/update-form", { droneModel: resultadoDrone })
+
+  })
+  .catch(err => console.log(err))
+});
+router.post("/drones/:id/edit", (req, res, next) => {
+  
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
-
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
-
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
