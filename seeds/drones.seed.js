73ba400261ea0c5e012012d2f121b4df9e2a7162
niwatoriDev <esmunozdev@gmail.com>
2022-04-28 01:19:48 -0500

    added two awesome drones to the db seed

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -4,6 +4,9 @@ const drones = [
   { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
   { name: "Racer 57", propellers: 4, maxSpeed: 20 },
   { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+  { name: "Steve's Hell Rider",  propellers: 66,  maxSpeed: 999},
+  { name: "Vic's Propelled Kart",  propellers: 8,  maxSpeed: 99}
+  
 ];
 
 // ℹ️ package responsible to make the connection with mongodb
