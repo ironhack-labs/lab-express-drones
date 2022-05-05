6f890d7c81cf7287100c75edd2c684825b59c73a
Helena Saborit <helena.sb91@gmail.com>
2022-05-05 21:37:58 +0200

    stuck in it1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,25 @@
 // Iteration #1
+
+const Drone = require('../models/Drone.model');
+require('../db');
+
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+
+const droneSeed = async () => {
+    try {
+        await Drone.create(drones);
+        console.log(`${drones.length} drones created`)
+        await mongoose.connection.close();
+    }catch(error){
+        console.error(error);
+      }
+}
+
+
+droneSeed();
