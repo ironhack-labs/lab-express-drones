e8f1379e89b6999ccff14d2d9aa675d452429702
Jaison Jacob <jacob.jaison@hotmail.com>
2022-05-06 09:32:22 +0400

    Solved

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const {Schema, model} = require('mongoose');
+
+const droneSchema = new Schema({
+    name : {
+        type:String,
+        required:true
+    },
+    propellers : {
+        type: Number,
+        required:true
+    },
+    maxSpeed : {
+        type: Number,
+        required:true
+    }
+})
+
+const Drone = model('Drone',droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,26 @@
 // Iteration #1
+const drones = [
+    {name:"Creeper XL 500",propellers:3, maxSpeed:12},
+    {name:"Race 57", propellers:4, maxSpeed:20},
+    {name:"Courier 3000i",propellers:6, maxSpeed:18}
+];
+
+const  Mongoose  = require('mongoose');
+const Drone = require('../models/Drone.model');
+require('../db');
+
+const droneSeed = async () => {
+    try {
+        await Drone.deleteMany();
+        await Drone.create(drones);
+        console.log(`${drones.length} drones created`);
+
+        await Mongoose.connection.close();
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+droneSeed();
+
+
