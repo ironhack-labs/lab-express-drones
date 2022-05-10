a22a743ee996ad32c4a864bb1d305538188a149d
JoaoMorgado8 <joaoarmorgado@gmail.com>
2022-05-10 09:04:19 +0100

    80% completed

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,36 @@
 // Iteration #1
+
+//CONNECTION WITH DB ---------------------------------------------------
+
+const mongoose = require("mongoose");
+
+const Drone = require("../models/Drone.model");
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+// ----------------------------------------------------------
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+Drone.create(drones)
+  .then((createdDrones) => {
+    console.log(`It was created: ${createdDrones.length} drones`);
+    mongoose.disconnect(() => console.log("Disconected from DB"));
+  })
+  .catch((err) => console.log(err));
