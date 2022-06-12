1dbfa24474ee3269052f8039c4a50ff721fe4d4b
Vale Quiroga <valentinqramos@outlook.com>
2022-06-12 17:56:04 -0500

    Iteration 1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,18 @@
-// Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model')
+const MongoDB = "mongodb://localhost/lab-express-drones"
+
+mongoose.connect(MongoDB)
+
+const drones = [
+  {name:'General Atomics', propellers:6, maxSpeed: 14},
+  {name:'TurboDrone', propellers:4, maxSpeed: 12},
+  {name:'SpeedHawk', propellers:2, maxSpeed: 18}
+];
+
+Drone.create(drones)
+  .then(dronesDB => {
+    console.log(`Created ${dronesDB.length} drones`)
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(`Error while trying to create drones`, err))
