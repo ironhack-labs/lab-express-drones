4bad3ce2d093adcca50889a1a0477be82baddb60
Vale Quiroga <valentinqramos@outlook.com>
2022-06-12 19:29:04 -0500

    Iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -13,13 +13,18 @@ router.get('/drones', (req, res, next) => {
 })
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('../views/drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body
+
+  Drone.create({name, propellers, maxSpeed})
+    .then(() => res.redirect('/drones'))
+    .catch(error => {
+      console.log(`Error while trying to create a drone `, error)
+      next()
+    })
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
