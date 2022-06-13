321608204478bd1040e930ea7c0736675ddf4899
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-06-13 01:28:55 -0500

    iteracion1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,16 @@
 // Iteration #1
+
+const { Schema, model } = require("mongoose");
+
+const droneSchema = new Schema(
+    {
+      //aqui va la estructura:
+      name: String,
+      propellers: Number,
+      maxSpeed: Number
+    }
+  );
+  
+  //ultimo para exportar
+  
+  module.exports = model("DRONE", droneSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,25 @@
 // Iteration #1
+
+
+const mongoose = require("mongoose");
+
+const Drone = require("../models/Drone.model");
+const DB_NAME = "mongodb://localhost/lab-express-drones";
+
+mongoose.connect(DB_NAME)
+
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  Drone.create(drones, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`creamos ${drones.length} drones en drone models`) // tu estás pasando drones [] de la lína 12 de este documento al model en mongodb
+    mongoose.connection.close()
+  });
+  
