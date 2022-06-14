71a362aaeb6139de0fa97acbd27456d28e360856
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-06-14 18:27:23 +0200

    indentation

--- routes/drones.js
+++ routes/drones.js
@@ -32,11 +32,10 @@ router.post("/drones/create", (req, res, next) => {
   const { name, propellers, maxSpeed } = req.body;
 
   Drone.create({ name, propellers, maxSpeed })
-   
-    .then(() =>  res.redirect("/drones"))
-    .catch(() => res.render("drones/create-form"));
-  });
 
+    .then(() => res.redirect("/drones"))
+    .catch(() => res.render("drones/create-form"));
+});
 
 router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
@@ -54,7 +53,7 @@ router.get("/drones/:id/edit", (req, res, next) => {
 router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-  const {id} = req.params;
+  const { id } = req.params;
   const { name, propellers, maxSpeed } = req.body;
 
   Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed }, { new: true })
@@ -65,11 +64,11 @@ router.post("/drones/:id/edit", (req, res, next) => {
 router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
-  const {id} = req.params;
+  const { id } = req.params;
 
   Drone.findByIdAndDelete(id)
-  .then(() => res.redirect("/drones"))
-  .catch((error) => next(error));
+    .then(() => res.redirect("/drones"))
+    .catch((error) => next(error));
 });
 
 module.exports = router;
