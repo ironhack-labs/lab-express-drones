dc54fb5f51165aafec01c9747092c6e33f10ef1e
Santiago Moncada <santiago.m.a@hotmail.com>
2022-06-26 12:46:06 +0200

    added styling

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const droneSchema = new Schema({
-    name: String,
-    propellers: Number,
-    maxSpeed: Number,
+    name: {
+        type: String,
+        minlength: 1,
+        required: true,
+    },
+    propellers: {
+        type: Number,
+        min: 0
+    },
+    maxSpeed: {
+        type: Number,
+        min: 0
+    },
 });
 
 module.exports = mongoose.model("Drone", droneSchema);
