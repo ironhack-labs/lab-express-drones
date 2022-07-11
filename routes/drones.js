9500e32b8fd46ec129415f1b844bd00dcdcadb91
rubenanlo <85644491+rubenanlo@users.noreply.github.com>
2022-07-11 18:22:29 +0200

    completed

--- routes/drones.js
+++ routes/drones.js
@@ -28,15 +28,16 @@ router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   const droneDetails = {
     name: req.body.name,
-    propellers: req.body.propeller,
+    propellers: req.body.propellers,
     maxSpeed: req.body.maxSpeed,
   };
+  console.log(req.body.propeller)
   Drone.create(droneDetails)
     .then(() => {
       res.redirect("/drones")
     })
     .catch((error) => {
-      console.log("Error creating book in the DB", error);
+      console.log("Error creating drone in the DB", error);
       next(error);
     })  // ... your code here
 });
@@ -46,24 +47,26 @@ router.get('/drones/:id/edit', (req, res, next) => {
   const id = req.params.id;
   Drone.findById(id)
   .then((droneDetails) => {
+    
     res.render("drones/update-form", droneDetails)
   })
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  const id = req.params.id;
+  const id = req.params.id
   const update = {
     name: req.body.name,
     propellers: req.body.propellers,
     maxSpeed: req.body.maxSpeed,
   };
-  Book.findByIdAndUpdate(id, update)
-  .then(() => {
-    res.redirect("drones")
+  Drone.findByIdAndUpdate(id, {$set : req.body})
+  .then((data) => {
+    // console.log(data)
+    res.redirect("/drones")
   })
   .catch((error) => {
-    console.log("Error getting book details from DB", error);
+    console.log("Error getting drone details from DB", error);
     next(error);
   })
 
@@ -71,7 +74,15 @@ router.post('/drones/:id/edit', (req, res, next) => {
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const id = req.params.id;
+  Drone.findByIdAndRemove(id)
+  .then(() => {
+    res.redirect('/drones')
+  })
+  .catch((error) => {
+    console.log("Error deleting drone details from DB", error);
+    next(error);
+  })
 });
 
 module.exports = router;
