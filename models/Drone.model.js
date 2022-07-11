d1a1ee22611453dc1b76d1faaf95891f2d2a8ba8
rubenanlo <85644491+rubenanlo@users.noreply.github.com>
2022-07-11 17:08:32 +0200

    iterations 1-3 completed

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,19 @@
 // Iteration #1
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const droneSchema = new Schema(
+  {
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true, //--> this is going to store info when the book is created and updated
+  }
+);
+
+const Drone = model("Drone", droneSchema);
+
+module.exports = Drone;
--- routes/drones.js
+++ routes/drones.js
@@ -1,31 +1,72 @@
 const express = require('express');
+const Drone = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
+  Drone.find()
+  .then((dronesFromDB) => {
+    const data = {dronesFromDB};
+    console.log(data)
+    res.render('drones/list', data);
+  })
+  .catch((error) => {
+    console.log("Error in retrieving information from the DB", error);
+    next(error)
+  })
   // ... your code here
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form")
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const droneDetails = {
+    name: req.body.name,
+    propellers: req.body.propeller,
+    maxSpeed: req.body.maxSpeed,
+  };
+  Drone.create(droneDetails)
+    .then(() => {
+      res.redirect("/drones")
+    })
+    .catch((error) => {
+      console.log("Error creating book in the DB", error);
+      next(error);
+    })  // ... your code here
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const id = req.params.id;
+  Drone.findById(id)
+  .then((droneDetails) => {
+    res.render("drones/update-form", droneDetails)
+  })
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const id = req.params.id;
+  const update = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed,
+  };
+  Book.findByIdAndUpdate(id, update)
+  .then(() => {
+    res.redirect("drones")
+  })
+  .catch((error) => {
+    console.log("Error getting book details from DB", error);
+    next(error);
+  })
+
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
