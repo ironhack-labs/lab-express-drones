f7f1de5381a30e24de783af715da648d735b326b
Paul Tiry <tirypaul@gmail.com>
2022-07-11 18:31:07 +0200

    Iteration 3 done

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,50 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // require the Drone model here
+const Drone = require('../models/Drone.model')
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
-});
+})
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
-});
+  res.render('drones/create-form')
+})
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
-});
+  const newDrone = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed,
+  }
+
+  Drone.create(newDrone)
+    .then(() => {
+      console.log('success creating new drone')
+      res.redirect('/drones')
+    })
+    .catch((err) => {
+      console.log('A problem ocurred creating your drone...', err)
+    })
+})
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-});
+})
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-});
+})
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
-});
+})
 
-module.exports = router;
+module.exports = router
