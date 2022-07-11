98cfaf43ef286115d72753027d5ada4213faa8f9
Michael Oberst <mioberst@gmx.de>
2022-07-11 20:24:14 +0200

    done

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,86 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model.js")
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  const drones = Drone.find()
+    .then(dronesFromDB => {
+      const data = { data: dronesFromDB }
+      console.log("drones retrieved from DB: " + data);
+      res.render("drones/list", data)
+    })
+    .catch(error => {
+      console.log("error while retrieving all dones: " + error);
+      next(error)
+    })
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form")
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
-});
+  const { name, propellers, maxSpeed } = req.body
+  const data = { name, propellers, maxSpeed }
+  Drone.create(data)
+    .then(() => {
+      console.log("new drone created");
+      res.redirect("/drones")
+    })
+    .catch(error => {
+      console.log("error while creating a new drone: " + error);
+      res.redirect("/drones/create")
+    })
+  });
+  
+  router.get('/drones/:id/edit', (req, res, next) => {
+    // Iteration #4: Update the drone
+    const {id} = req.params
+    Drone.findById(id)
+    .then(droneDetails => {
+      res.render("drones/update-form", droneDetails)      
+    })
+    .catch(error => {
+      console.log("error while linking to update page: " + error)
+      next(error)
+    })
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params
+  const {name, propellers, maxSpeed} = req.body
+  const editedData = {name, propellers, maxSpeed}
+  console.log(editedData);
+  Drone.findByIdAndUpdate(id, editedData)
+    .then(() => {
+      console.log("data successfully updated")
+      res.redirect("/drones")
+    })
+    .catch(error => {
+      console.log("An error occurred while updating data: " + error);
+      res.redirect(`/drones/${id}/edit`)
+    })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const {id} = req.params
+  Drone.findByIdAndDelete(id)
+    .then(() => {
+      console.log("successfully deleted");
+      res.redirect("/drones")
+    })
+    .catch((error) => {
+      console.log("An error occurred while deleting: " + error);
+      res.redirect("/drones")
+    })
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,42 @@
 // Iteration #1
+const drones = [
+    {
+        name: "Flyer",
+        propellers: 2,
+        maxSpeed: 10,
+    },
+    {
+        name: "High Flyer",
+        propellers: 4,
+        maxSpeed: 16,
+    },
+    {
+        name: "Super Flyer",
+        propellers: 8,
+        maxSpeed: 22,
+    }
+]
+
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+        return Drone.create(drones)
+    })
+    .then((dronesFromDB) => {
+        console.log("number of drones created: " + dronesFromDB.length)
+        mongoose.connection.close()
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    });
