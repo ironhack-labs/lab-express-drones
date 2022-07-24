e83ca279fe64c7c09fe49972cd1ddf3dce2b1e3e
merJur <merjuradoperez@gmail.com>
2022-07-25 01:49:57 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const dronesController = require ('../controllers/drones.controller')
+
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
 
