9ba302b1a8594969f58a8ab4289125d5c0eef269
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-08-09 21:55:03 +0200

    finish iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,9 @@
-// Iteration #1
+const { Schema, model } = require("mongoose");
+
+const droneSchema = new Schema({
+  name: { type: String, required: true },
+  propellers: { type: Number, required: true },
+  maxSpeed: { type: Number, required: true },
+});
+
+module.exports = model("Drone", droneSchema);
