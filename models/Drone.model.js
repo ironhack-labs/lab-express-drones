68ce8de607e217802795d9312fed3911c0b6da02
Numan Torres <numantorresr@gmail.com>
2022-09-03 14:16:39 +0200

    solved lab

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,14 @@
 // Iteration #1
+const { Schema, model } = require('mongoose');
+
+const droneSchema = new Schema({
+    name: { type: String },
+    propellers: { type: Number },
+    maxSpeed: [{ type: String }],
+}, {
+    timestamps: true,
+});
+
+const droneModel = model('drones', droneSchema);
+
+module.exports = droneModel;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,29 @@
 // Iteration #1
+require('dotenv/config');
+const mongoose = require('mongoose');
+const DroneModel = require('../models/Drone.model');
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/drones'
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+mongoose
+    .connect(MONGO_URI)
+    .then((connectMongoose) => {
+        console.log('Connect DB: ', connectMongoose.connections[0].name);
+        return DroneModel.create(drones)
+    })
+    .then((drones) => {
+        console.log(drones.length)
+    })
+    .catch((err) => {
+        console.error('Error:', err);
+
+    })
+    .finally(() => {
+        mongoose.disconnect();
+    })
