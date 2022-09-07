f8eb6fe92175f8ad349bd518f65790d40f9249c5
Daniela Romero <danyrp45@gmail.com>
2022-09-06 22:32:42 -0500

    done

--- routes/drones.js
+++ routes/drones.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Drone = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
@@ -6,26 +7,56 @@ const router = express.Router();
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  Drone.find()
+  .then(drons =>{
+    console.log(drons)
+    res.render("drones/list", { drons: drons })
+  })
+  .catch(err =>{console.log(err)})
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render("drones/create-form")
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  Drone.create(req.body)
+  .then(dron =>{
+    console.log(dron);
+    res.redirect("/drones")
+
+  })
+  .catch(err =>{console.log(err)})
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  Drone.findById(req.params.id)
+  .then(drone =>{
+    console.log(drone);
+    res.render("drones/update-form", drone)
+  }
+    
+  )
+  .catch(err =>{console.log(err)})
+  
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+    Drone.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(newActualization =>{
+      console.log(newActualization)
+      res.redirect(`/drones`)
+    })
+    .catch(err =>{console.log(err)})
+
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,47 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+const drones = [
+    {
+        name: "Dronita",
+        propellers: 4,
+        maxSpeed: 8,
+    },
+    {
+        name: "Dronatella",
+        propellers: 8,
+        maxSpeed: 10,
+    },
+    {
+        name: "Super Dron",
+        propellers: 7,
+        maxSpeed: 12,
+    },
+]
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo database: "${x.connections[0].name}"`);
+    return Drone.create(drones);
+  })
+  .then((dronesCreados) => {
+    console.log(`Created ${dronesCreados.length} books`);
+
+    // Once the documents are created, close the DB connection
+    return mongoose.connection.close();
+  })
+  .then(() => {
+    // Once the DB connection is closed, print a message
+    console.log("DB connection closed!");
+  })
+  .catch((err) => {
+    console.log(`An error occurred while creating books from the DB: ${err}`);
+  });
+
+
+
