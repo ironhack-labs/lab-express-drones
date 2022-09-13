31959d0af705ea26e5ca599cfda4adaf6a55076a
Kamil Chomiak <kamil.chomiak@outlook.com>
2022-09-13 17:07:51 -0400

    done

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,81 @@
-const express = require('express');
+const express = require("express");
+const Drone = require("../models/Drone.model");
 const router = express.Router();
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then((drones) => {
+      console.log(drones);
+      res.render("drones/list", { drones });
+    })
+    .catch((err) => console.log(err));
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  const newDrone = new Drone({
+    name: name,
+    propellers: propellers,
+    maxSpeed: maxSpeed,
+  });
+
+  newDrone
+    .save()
+    .then((drone) => {
+      res.redirect("/drones");
+    })
+    .catch((err) => console.log(err));
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findById(req.params.id)
+    .then((drone) => {
+      res.render("drones/update-form", drone);
+    })
+    .catch((err) => console.log(err));
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  const newDrone = {
+    name: name,
+    propellers: propellers,
+    maxSpeed: maxSpeed,
+  };
+
+  Drone.findByIdAndUpdate(req.query.id, newDrone)
+    .then((drone) => {
+      res.redirect("/drones");
+    })
+    .catch((err) => {
+      console.log(err);
+      Drone.findById(req.params.id)
+        .then((drone) => {
+          res.render("drones/update-form", drone);
+        })
+        .catch((err) => console.log(err));
+    });
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  Drone.findByIdAndRemove(req.params.id)
+    .then((drone) => {
+      console.log("Deleted: " + drone);
+      res.redirect("/drones");
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,40 @@
 // Iteration #1
+//CREATE SEED
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+//CONNECT TO DATABASE
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+//SEED THE DATABASE
+Drone.create(drones)
+  .then((drones) => {
+    console.log("Database seeded with: " + drones);
+    //CLOSE THE DATABASE CONNECTION
+    mongoose.connection.close();
+    console.log("DATABASE SEEDING COMPLETE -- DB CONNECTION CLOSED!");
+  })
+  .catch((err) => console.log(err));
