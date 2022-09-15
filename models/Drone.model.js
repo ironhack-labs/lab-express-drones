f557b926f63631dc02942e93ccd80f2aa7f1d9e7
hvkst <bernd@hvkst.de>
2022-09-15 21:59:07 +0200

    Iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,20 @@
 // Iteration #1
+const mongoose = require('mongoose');
+
+const droneSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  propellers: {
+    type: Number,
+  },
+  maxSpeed: {
+    type: Number,
+  },
+});
+
+const Drone = mongoose.model('drone', droneSchema);
+
+module.exports = { Drone };
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
+const mongoose = require('mongoose');
+const { Drone } = require('../models/Drone.model');
 // Iteration #1
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+  { name: 'Creeper XXL 400', propellers: 8, maxSpeed: 36 },
+  { name: 'Racer 72', propellers: 4, maxSpeed: 6 },
+  { name: 'Courier 3600x', propellers: 12, maxSpeed: 36 },
+  { name: 'Creeper 600', propellers: 3, maxSpeed: 29 },
+  { name: 'Racer 90c', propellers: 16, maxSpeed: 42 },
+  { name: 'Courier 200', propellers: 4, maxSpeed: 16 },
+];
+
+require('../db');
+
+Drone.create(drones).then((mongoRes) => {
+  console.log('Drones created:', mongoRes.length);
+  mongoose.connection.close();
+});
