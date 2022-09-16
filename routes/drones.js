e2cb8aacc33d204ba13bd93ae299ee0e7ee4e2d1
hvkst <bernd@hvkst.de>
2022-09-16 21:59:02 +0200

    bootstraped everything and cleaned up

--- routes/drones.js
+++ routes/drones.js
@@ -7,7 +7,6 @@ const router = express.Router();
 router.get('/drones', (req, res, next) => {
   Drone.find()
     .then((data) => {
-      // console.log('This data', data);
       res.render('drones/list', { drones: data });
     })
     .catch((err) => {
@@ -19,14 +18,11 @@ router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   res.render('drones/create-form');
 });
-// .catch((err) => {
-//   console.error(err);
-// });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   Drone.create(req.body)
-    .then((data) => {
+    .then(() => {
       res.redirect('/drones');
     })
     .catch((err) => {
@@ -39,28 +35,32 @@ router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   Drone.findById(req.params.id)
     .then((data) => {
-      // console.log(data);
       res.render('drones/update-form', data);
     })
     .catch((err) => {
       console.log(err);
       res.redirect('drones/update-form');
-      // App crashes here if there is a duplicate key error
     });
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   Drone.findByIdAndUpdate(req.params.id, req.body).then(() => {
-    res.redirect('/drones');
+    res.redirect('/drones').catch((err) => {
+      console.log(err);
+      res.redirect('/drones');
+    });
   });
-  // console.log(req.params.id);
-  // console.log('BODY', req.body);
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  Drone.findByIdAndDelete(req.params.id).then(() => res.redirect('/drones'));
+  Drone.findByIdAndDelete(req.params.id)
+    .then(() => res.redirect('/drones'))
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/drones');
+    });
 });
 
 module.exports = router;
