91cd040e8e342c526097a342b17ef7d447e42bcf
Patrick-Groot <patrick@candis.io>
2022-09-20 10:44:13 +0200

    Finished Lab

--- routes/drones.js
+++ routes/drones.js
@@ -7,7 +7,6 @@ const { Drone } = require("../models/Drone.model");
 router.get("/", async (req, res, next) => {
   // Iteration #2: List the drones
   const allDrones = await Drone.find();
-  console.log(allDrones);
   res.render("drones/list", { allDrones });
 });
 
@@ -28,19 +27,41 @@ router.post("/create", async (req, res, next) => {
   }
 });
 
-router.get("/:id/edit", (req, res, next) => {
+router.get("/:id/edit", async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  try {
+    console.log("test");
+    console.log(req.params.id);
+    const droneToUpdate = await Drone.findById(req.params.id);
+    console.log(droneToUpdate);
+    res.render("drones/update-form", { drone: droneToUpdate });
+  } catch (err) {
+    console.log("Sorry, there was an error", err);
+  }
 });
 
-router.post("/:id/edit", (req, res, next) => {
+router.post("/:id/edit", async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  try {
+    console.log(req.body);
+    console.log(req.params)
+    const { name, propellers, maxSpeed } = req.body;
+    const editedDrone = await Drone.findByIdAndUpdate(req.params.id, {
+      name,
+      propellers,
+      maxSpeed,
+    });
+    res.redirect("/drones");
+  } catch (err) {
+    console.log("Sorry, there was an error", err);
+    res.redirect("/drones");
+  }
 });
 
-router.post("/:id/delete", (req, res, next) => {
+router.post("/:id/delete", async (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const droneToDelete = await Drone.findByIdAndDelete(req.params.id);
+  res.redirect("/drones");
 });
 
 module.exports = router;
