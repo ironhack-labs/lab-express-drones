074a4f7b6b99426c3f4b563a2e20067a063e149e
Matthew Schröder <matthewschroeder94@gmail.com>
2022-10-03 18:46:56 +0200

    solved mandatory iterations

--- routes/drones.js
+++ routes/drones.js
@@ -37,18 +37,43 @@ router.post('/drones/create', (req, res, next) => {
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  DroneModel.findById(req.params.id)
+  .then((droneDetails) => {
+    res.render("drones/update-form", droneDetails)
+  })
+  .catch( err => {
+    console.log("Error getting drone details from DB...", err);
+    next();
+  })
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const id = req.params.id;
+
+  const newDetails = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed,
+  }
+
+  DroneModel.findByIdAndUpdate(id, newDetails)
+  .then(() => {
+    res.redirect("/drones");
+  })
+  .catch(err => {
+    console.log("Error updating drone...", err);
+  });
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  DroneModel.findByIdAndDelete(req.params.id)
+  .then(() => {
+    res.redirect("/drones")
+  })
+  .catch(err => {
+    console.log("Error deleting drone...", err);
+    next();
+  });
 });
 
 module.exports = router;
