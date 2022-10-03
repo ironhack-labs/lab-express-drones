6ef6c616bd4d62489fd06ea7e56012a89d49ec4a
Jose Piras <giuseppe3piras@gmail.com>
2022-10-03 20:19:44 +0200

    Iteration 1,2,3 completed

--- routes/drones.js
+++ routes/drones.js
@@ -1,31 +1,49 @@
 const express = require('express');
 const router = express.Router();
-
+const Drone = require("../models/Drone.model")
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then((dataFromDB) => {
+      console.log(dataFromDB)
+      res.render("drones/list", { drones: dataFromDB })
+    })
+    .catch(err => console.log(" error getting data from DB", err))
+
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+
+
+  res.render("drones/create-form")
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+
+  const newDrone = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  }
+
+  Drone.create(newDrone)
+    .then(newDrone => {
+      res.redirect("/drones")
+    })
+    .catch(err => console.log("error creating new dron", err))
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findById(req.params.id)
+    .then((droneDetails) => {
+      res.render("drones/update-form.hbs", droneDetails)
+    })
+    .catch(err => console.log("error updating new dron", err))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,38 @@
 // Iteration #1
+const Drone = require('../models/Drone.model')
+
+const drones = [
+
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+        return Drone.create(drones)
+
+    }).then((check) => {
+        console.log(check.length)
+        return mongoose.disconnect()
+    })
+    .then(() => {
+        console.log("Db disconneted")
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    });
+
+
