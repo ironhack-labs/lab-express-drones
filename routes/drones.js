742204d005c03664d50de1f4d83f837d45b2d310
Saskia Peach <saskiapeach@gmail.com>
2022-10-03 17:30:20 +0100

    completed iteration 1&2

--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const DroneModel = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  DroneModel
+  .find()
+  .then((foundDrones) => {
+    res.render("drones/list", {drones:foundDrones})
+  })
+  .catch(err => console.log(err))
 });
 
 router.get('/drones/create', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,34 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/lab-express-drones';
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  mongoose
+    .connect(MONGO_URI)
+    .then(x => {
+        console.log(`Connected to Mongo database: "${x.connections[0].name}"`);
+
+        // Create new documents in the books collection
+        return Drone.create(drones);
+    })
+    .then(dronesFromDB => {
+        console.log(`Created ${dronesFromDB.length} drones`);
+
+        // Once the documents are created, close the DB connection
+        return mongoose.connection.close();
+    })
+    .then(() => {
+        // Once the DB connection is closed, print a message
+        console.log('DB connection closed!');
+    })
+    .catch(err => {
+        console.log(`An error occurred while creating the drones from the database: ${err}`);
+    });
