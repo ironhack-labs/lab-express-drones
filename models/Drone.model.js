c95e4eb06b010cafd684ee917673a0c6db0c1a0f
MoniIgnacio <moniignacio02@gmail.com>
2022-10-17 19:02:23 +0400

    It 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
 // Iteration #1
+const mongoose = require('mongoose');
+
+const droneSchema = new mongoose.Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+
+const Drone = mongoose.model('Drone', droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,20 @@
+require('../db');
+const Drone = require('../models/Drone.model.js')
+const mongoose = require("mongoose");
+
 // Iteration #1
+const droneArr = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+]
+
+
+Drone.insertMany(droneArr)
+.then(() =>{
+    console.log('Drones adheridos');
+    mongoose.connection.close()
+})
+.catch((err)=>{
+    console.log(err);
+})
