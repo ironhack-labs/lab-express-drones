fcb7cb4b79763521daa21cc0eab6d5bbf2f28aa3
julianarangelbarbosa <juliana.rangel.barbosa@gmail.com>
2022-10-31 17:37:46 +0000

    Labsday31oct

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const {Schema, model} = require('mongoose');
+
+const droneSchema = new Schema(
+{
+   name : { 
+    type: String,
+    require: true
+   },
+   propellers: String, 
+   maxSpeed: Number
+
+},
+{
+    timestamps: true
+}
+)
+
+const Drone = model("Drone", droneSchema)
+
+module.exports = Drone;
