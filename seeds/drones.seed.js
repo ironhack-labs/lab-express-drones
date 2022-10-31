01f1d34e62d6388c0cacddb57bdf396d1baa9cea
Bruno Rocha <bruno.miguel.rocha@hotmail.com>
2022-10-31 16:19:26 +0000

    done without styles

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,38 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+
+async function seeds() {
+
+    try {
+        const x = await mongoose.connect(MONGO_URI);
+        console.log(`Connected to: ${x.connections[0].name}`);
+
+        let createdDrones = await Drone.create(drones);
+        console.log(`Successfuly created ${createdDrones.length}`);
+    
+        x.disconnect();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+seeds();
