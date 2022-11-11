caf93c36bce8fcb90d69b9c04eb2ab2139a56016
NaomiBurguesMontesdeoca <naomi.burgues@gmail.com>
2022-11-11 19:29:31 +0100

    solved lab

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,11 @@
-// Iteration #1
+const mongoose = require('mongoose');
+
+const dronesSchema = new mongoose.Schema(
+    {
+        name: String,
+        propellers: Number,
+        maxSpeed: Number
+    }
+)
+
+module.exports = mongoose.model('Drone', dronesSchema);
