19f67ae602cac646fe4ee3ccf79f44129c9e8d2f
yoaomoreira <jpmoreiraes@gmail.com>
2022-11-13 22:20:53 +0100

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,16 @@
-// Iteration #1
+const mongoose = require('mongoose');
+
+const droneSchema = new mongoose.Schema(
+
+    {
+        name: String,
+        propellers: Number,
+        maxSpeed: Number,
+    },
+
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('Drone', droneSchema);
