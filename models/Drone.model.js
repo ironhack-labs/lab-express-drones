a34e8e9d0752e207b8e721c9bbbf90b4a4fd09d8
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2022-11-13 18:35:39 +0100

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,20 @@
-// Iteration #1
+const { Schema, model } = require('mongoose');
+
+const DroneSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    propellers: {
+        type: Number,
+        required: true
+    },
+    maxSpeed: {
+        type: Number,
+        required: true
+    }
+});
+
+const DroneModel = model('Drone', DroneSchema);
+module.exports = DroneModel;
