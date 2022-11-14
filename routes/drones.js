c244793c82e73fa90443b20cac0c028686d2501c
Catalina Sabogal <csabogalortiz@gmail.com>
2022-11-14 14:27:21 +0100

    Lab Drones

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,85 @@
 const express = require('express');
 const router = express.Router();
 
-// require the Drone model here
+const Drone = require('./../models/Drone.model')
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+
+
+// Lo que le estoy diciendo es que en la /drones en el url me muestre o renderice el drones/list y si quiero 
+// personalizar esa vista me toca es en drones/list porque eso es lo que este renderizando cuando le doy en esa url 
+
+
+router.get('/drones', (req, res) => {
+
+  Drone
+    .find()
+    .then(dronesFromDB => {
+      res.render('drones/list', { drones: dronesFromDB })
+    })
+    .catch(err => console.log(err))
+
+});
+// ******************************
+router.get('/drones/create', (req, res) => {
+  res.render('drones/create-form')
+})
+
+// ******************************
+router.post('/drones/create', (req, res) => {
+  const { name, propellers, maxSpeed } = req.body
+
+  Drone
+    .create({ name, propellers, maxSpeed })
+    .then(() => {
+      res.redirect(`/drones`)
+    })
+    .catch(err => console.log(err))
+});
+// ******************************
+
+
+router.get('/drones/:drone_id/edit', (req, res) => {
+
+  const { drone_id } = req.params
+
+  Drone
+    .findById(drone_id)
+    .then(drone => {
+      res.render('drones/update-form', drone)
+    })
+    .catch(err => console.log(err))
+
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+
+// ******************************
+
+router.post('/drones/:drone_id/edit', (req, res) => {
+  const { name, propellers, maxSpeed } = req.body
+  const { drone_id } = req.params
+
+  Drone
+    .findByIdAndUpdate(drone_id, { name, propellers, maxSpeed })
+    .then(() => res.redirect('/drones'))
+    .catch(err => console.log(err))
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
+// **********************
+router.post('/drones/:drone_id/delete', (req, res) => {
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
+  const { drone_id } = req.params
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  Drone
+
+    .findByIdAndDelete(drone_id)
+    .then(() => res.redirect('/drones'))
+    .catch(err => console.log(err))
+
 });
 
 module.exports = router;
+
+// Use the Mongoose.find() method to retrieve all the drones.Display all the drones on the drones / list.hbs view.Make sure you catch the error and output it to the terminal.
+// In the drones / list.hbs file, use a #each loop to display tags with each drone's name, propellers, and speed.
+// Add the link that goes to / drones route in the layout.hbs file to easier navigate to the list of drones.
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,39 @@
-// Iteration #1
+
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+// const Author = require('../models/Author.model.js');
+
+const dbtitle = 'lab-express-drones';
+mongoose.connect(`mongodb://localhost/${dbtitle}`);
+Drone.collection.drop();
+// **** Revisar
+// Author.collection.drop();
+
+const drones = [
+    {
+        name: "Creeper XL 500",
+        propellers: 4,
+        maxSpeed: 20,
+
+    },
+    {
+        name: "Racer 57",
+        propellers: 2,
+        maxSpeed: 100,
+
+    },
+    {
+        name: "Courier 3000i",
+        propellers: 6,
+        maxSpeed: 18,
+
+    },
+]
+
+Drone
+    .create(drones)
+    .then(dronesFromDB => {
+        console.log(`Created ${dronesFromDB.length} drones`);
+        mongoose.connection.close();
+    })
+    .catch(err => console.log(`An error occurred while creating drones from the DB: ${err}`));
