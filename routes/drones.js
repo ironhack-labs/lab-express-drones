61c5225eae6256f8f540f0aa50cff599055c5660
sijincodes <sijinmathews03@gmail.com>
2022-11-20 11:04:02 +0100

    iteration3

--- routes/drones.js
+++ routes/drones.js
@@ -10,14 +10,17 @@ router.get("/drones", (req, res, next) => {
   .catch((error) => next(error))
 });
 
-router.get("/drones/create", (req, res, next) => {
+router.get("/drones/create", (req, res) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+   res.render('drones/create-form')
 });
 
-router.post("/drones/create", (req, res, next) => {
+router.post("/drones/create", (req, res) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {name,propellers,maxSpeed}=req.body
+  Drone.create({name,propellers,maxSpeed})
+  .then((dronesfromDB) => res.redirect("/drones"))
+  .catch((error)=>res.render("drones/create-form"))
 });
 
 router.get("/drones/:id/edit", (req, res, next) => {
