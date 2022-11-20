3e711e0668bb3e266e1b5348c9eae9bd02744973
sijincodes <sijinmathews03@gmail.com>
2022-11-20 10:10:47 +0100

    iteration1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const MONGO_URI = "mongodb://localhost/lab-express-drones";
+mongoose
+	.connect(MONGO_URI)
+	.then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+	.catch((err) => console.error("Error connecting to mongo: ", err));
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  Drone.create(drones)
+	.then((drones) => {
+		console.log(`Created ${drones.length} drones`);
+		mongoose.connection.close();
+	})
+	.catch((err) => console.log(`An error occurred while creating drones from the DB: ${err}`));
