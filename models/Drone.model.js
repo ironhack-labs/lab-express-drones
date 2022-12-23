95899e46d77b5d611e5e1058959085ff53c8fee7
Thomas Augot <thomas.augot@hotmail.fr>
2022-12-23 13:01:54 +0100

    iteration 0 done, connecting & importing items to DB

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,14 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const droneSchema = new Schema(
+  {
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+  }
+);
+
+module.exports = model('Drone', droneSchema);
