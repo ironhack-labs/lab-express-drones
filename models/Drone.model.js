46daf8bd71e786c3b7410004143f7a09113e5c8d
David Mullally <mullallydavid7@gmail.com>
2022-12-28 13:20:50 +0100

    complete first iteration

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+
+const {Schema, model} = require('mongoose');
+
+const droneSchema = new Schema(
+    {
+        name: String,
+        propellers: Number,
+        maxSpeed: { type: Number, max: 18 }
+    }
+);
+
+
+
+module.exports = model('Drone', droneSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,55 @@
-// Iteration #1
+// bin/seeds.js
+
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+const drones = [
+    {
+        name: 'Highwind', 
+        propellers: 7,
+        maxSpeed: 7
+    },
+    {
+        name: 'Tiny Bronco',
+        propellers: 2,
+        maxSpeed: 7,
+    },
+    {
+        name: 'Shinra No.26',
+        propellers: 1,
+        maxSpeed: 17
+    }
+
+ 
+];
+
+mongoose
+.connect("mongodb://127.0.0.1/express-drones")
+  .then(x => {
+    console.log(`Connected to Mongo database: "${x.connections[0].name}"`);
+
+    // Create new documents in the books collection
+    return Drone.create(drones);
+  })
+  .then(dronesFromDB => {
+    console.log(`Created ${dronesFromDB.length} drones`);
+
+    // Once the documents are created, close the DB connection
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    // Once the DB connection is closed, print a message
+    console.log('DB connection closed!');
+  })
+  .catch(err => {
+    console.log(`An error occurred while creating drones from the DB: ${err}`);
+  });
+
+
+
+
+
+
+
