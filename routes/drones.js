5f45d0a0f3eb9e30fbf352850cc2a1e45a640043
Thalita dos Reis <dosreistha@gmail.com>
2022-12-28 17:48:44 +0100

    iteration one and two: done

--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const DroneModel = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model.js'); // <== add this line before your routes
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+  .then(allDronesFromDB => {
+    console.log(allDronesFromDB);
+
+    res.render('drones/list.hbs', {drones: allDronesFromDB});
+  })
+  .catch(error => {
+    console.log('Error while getting the drones from the DB: ', error);
+
+    // Call the error-middleware to display the error page to the user
+    next(error);
+  });
 });
 
 router.get('/drones/create', (req, res, next) => {
