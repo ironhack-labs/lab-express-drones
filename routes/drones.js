34302384635bf96e0a6d7a9f8e32d8d500882ca9
Rumeli Paul <rumelipaul@gmail.com>
2022-12-28 17:36:40 +0100

    completed iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // require the Drone model here
 const Drone = require("../models/Drone.model");
 
-// list the drones
+// Iteration #2: Listing the available drones
 router.get('/drones', (req, res, next) => {
  Drone.find()
       .then((dronesFromDb) => {
@@ -17,15 +17,29 @@ router.get('/drones', (req, res, next) => {
       });
   });
 
-
+// Iteration #3: Add a new drone
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form");
 });
+ 
+
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+ 
+  const droneDetail = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed,
+  }
+  Drone.create(droneDetail)
+    .then((droneDetails) => {
+      
+      res.redirect("/drones");
+    })
+    .catch((err) => {
+      console.log("err creating new drone to the db", err);
+      next();
+    })
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
