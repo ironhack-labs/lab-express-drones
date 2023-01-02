9ea48c0f8782605e6c1589c935feb55b0b3fe95a
Lai In Chang <lavender880331@gmail.com>
2023-01-02 02:30:45 +0100

    Finish iteration 1: Seed the database

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+const { Schema, model } = require('mongoose');
+
+const droneSchema = new Schema(
+  {
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+  },
+  {
+    timestamps: true //tell mongoose to store the time stamp of the data entry
+  }
+);
+
+module.exports = model('Drone', droneSchema);
