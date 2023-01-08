25154d4dad473e55b38e7c829613c374dde3a8c9
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2023-01-08 19:22:56 +0100

    add Drone model

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,18 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+let droneSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  propellers: {
+    type: Number,
+  },
+  maxSpeed: {
+    type: Number,
+  },
+});
+
+let Drone = mongoose.model("drone", droneSchema);
+
+module.exports = Drone;
