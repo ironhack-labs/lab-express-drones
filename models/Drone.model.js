350c055332c097240078763c3ebf604b3a5c1aea
pianattero <mariapianattero@gmail.com>
2023-01-24 18:29:38 +0100

    Iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,22 @@
 // Iteration #1
+const mongoose = require('mongoose');
+
+const droneSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name of the drone is required']
+        },
+        propellers: {
+            type: Number,
+            required: [true, 'Number of propellers is required']
+        },
+        maxSpeed: {
+            type: Number,
+            required: [true, 'Max speed is required']
+        },
+    }
+)
+
+const Drone = mongoose.model('Drone', droneSchema);
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,35 @@
 // Iteration #1
+const mongoose = require('mongoose')
+const Drone = require('../models/Drone.model');
+require('../config/db.config')
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+mongoose.connection.once('open', () => {
+    console.info(`*** Connected to the database ${mongoose.connection.db.databaseName} ***`);
+  
+    mongoose.connection.db
+      .dropDatabase()
+      .then(() => {
+        console.info('Db has been cleared')
+  
+        return Drone.create(drones)
+      })
+      .then(dronesCreated => {
+        dronesCreated.forEach(drone => {
+          console.log(`Drone ${drone.name} has been created`)
+        })
+        console.log(`A total of ${drones.length} drones has been created`)
+      })
+      .catch(err => console.error(err))
+      .finally(() => {
+        mongoose.connection.close(function () {
+          console.log("Mongoose disconnected");
+          process.exit(0);
+        });
+      })
+  })
