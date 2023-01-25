21a44485c0d3fa38d3426ec1ac67ce5a22e58cd5
unknown <roxan.m.alma18@gmail.com>
2023-01-25 08:08:36 +0100

    2º done litle

---
+++ controllers/drones.controllers.js
@@ -0,0 +1,44 @@
+//aqui metemos todo la funcion de routes.get,  lo que va dentro de la funcion de routes
+//exportamos tambien el modelo
+
+
+const { model } = require('mongoose')
+
+//1.llamamos al modelo
+const Drone = require('../models/Drone.model')
+
+//exportamos funciones
+//es un objeto y por ello podemos meter claves-valor, metemos un objetos con muchas funciones como:
+
+//tienen en comun req, res, next
+//dentro definimos la logica de cada una de las rutas
+
+
+//*******llamar este archivo a las rutas. routes/drones.js */
+
+//despues de que se vea ek archivo de drones.js, viene a estas rutas
+//para crear algo se llama al archivo drones/list.hbs
+//allí se trabaja las claves de los drones
+
+//2.creamos esto
+//5. se añade algo a las carpetas
+module.exports.list = (req, res, next )=> {
+Drone.find()//para encontrar todos lo drones
+//si todo va bien devuelve esto
+.then((drones) => {
+    res.render('drones/list', {drones})
+})
+//sino error
+.catch()
+
+}
+
+
+module.exports.detail = ( )=> {
+
+}
+
+
+module.exports.create = ( )=> {
+
+}
--- routes/drones.js
+++ routes/drones.js
@@ -1,34 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
+//3.requerimos la carpetas donde ponemos todas las funciones del las rutas
+const dronesControllers = require('../controllers/drones.controllers')
+
 // require the Drone model here
 
-router.get('/', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
-});
+//4.quitamos el req, res y ponemos el archivo completo de cada uno de las drones.controllers.js  list, create, etc
 
-router.get('/create', (req, res, next) => {
+
+//algo entra aquí, va donde los controladores y lo ejecuta
+
+// lo siguiente a la barra  "/" es lo que se ve en la pantalla de la web
+
+router.get('/drones', dronesControllers.list);
+
+router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
 });
 
-router.post('/create', (req, res, next) => {
+router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
 });
 
-router.get('/:id/edit', (req, res, next) => {
+router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
