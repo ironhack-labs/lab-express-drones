5703d416d275ba4fa0cda41f67989f6048f85535
patri <patricia.mareg@gmail.com>
2023-01-29 12:53:26 +0100

    iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,19 @@
-// Iteration #1
+const mongoose = require("mongoose")
+
+const droneSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String
+    },
+    propellers: {
+      type: Number
+    },
+    maxSpeed: {
+      type: Number
+    }
+  }
+)
+
+const Drone = mongoose.model("drone", droneSchema)
+
+ module.exports = Drone
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,15 @@
-// Iteration #1
+require("../db/index")
+const Drone = require("../models/Drone.model")
+const mongoose = require("mongoose")
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+Drone.insertMany(drones)
+  .then(() => {
+    mongoose.connection.close()
+  })
+  .catch(err => console.log(err))
