32a317f0a629616966f5c661abe9401893770891
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-02-05 12:14:53 +0100

    Iteration #2: List the drones

--- config/drones.config.js
+++ config/drones.config.js
@@ -4,6 +4,7 @@ const drones = require("../controllers/drones.controllers");
 const routes = express.Router();
 
 routes.get("/", drones.home);
+routes.get("/drones", drones.list);
 
 module.exports = (app) => {
   app.use("/", routes);
--- controllers/drones.controllers.js
+++ controllers/drones.controllers.js
@@ -1,3 +1,21 @@
+const dataBase = require("../config/db.config");
+const droneModel = require("../models/drones.model");
+
 module.exports.home = (req, res) => {
   res.render("pages/home");
+};
+
+module.exports.list = (req, res) => {
+  dataBase.connect(
+    droneModel
+      .find()
+      .then((drones) => {
+        res.render("pages/drones/list", { drones });
+        dataBase.disconnect();
+      })
+      .catch((error) => {
+        console.log(error);
+        dataBase.disconnect();
+      })
+  );
 };
