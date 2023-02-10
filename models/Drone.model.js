0541c1b84179c66ddf5fb8d90e828956d429542d
Juan R <juangnba@hotmail.com>
2023-02-10 17:42:11 +0100

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,20 @@
-// Iteration #1
+const { Schema, model } = require('mongoose')
+
+const droneSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    propellers: {
+        type: Number,
+        required: true,
+    },
+    maxSpeed: {
+        type: Number,
+    },
+
+},
+    { timestamps: true }
+)
+const Drone = model('drone', droneSchema)
+module.exports = Drone
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
+const Drone = require("../models/Drone.model")
+const mongoose = require("mongoose");
+const { collection } = require("../models/Drone.model");
+
 // Iteration #1
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+require("../db")
+
+Drone
+    .collection.drop()
+    .catch(err => console.log(err))
+Drone
+    .insertMany(drones)
+    .then(drones => console.log("Inserted into db:", drones))
+    .catch(err => console.log(err))
+    .finally(() => mongoose.connection.close())
