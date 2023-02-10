8a9b5b2c9e145e7a47ca75b4a874782078b16237
danisalo <your_github_email>
2023-02-10 18:17:02 +0100

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,24 @@
-// Iteration #1
+const { Schema, model } = require('mongoose');
+
+const droneSchema = new Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+            trim: true,
+        },
+        propellers: {
+            type: Number,
+            require: true,
+        },
+        maxSpeed: {
+            type: Number,
+            require: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('drone', droneSchema)
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,42 @@
-// Iteration #1
+const mongoose = require('mongoose')
+const Drone = require('./../models/Drone.model')
+
+const drones = [
+    {
+        name: 'Drone 3000',
+        propellers: 30,
+        maxSpeed: 120,
+    },
+    {
+        name: 'Droner Kebab',
+        propellers: 2,
+        maxSpeed: 80,
+    },
+    {
+        name: 'Papagayo',
+        propellers: 4,
+        maxSpeed: 88,
+    }
+]
+
+const connectionString = 'mongodb://127.0.0.1:27017/drones'
+
+mongoose
+    .set('strictQuery', false)
+    .connect(connectionString)
+    // vaciar colección
+    .then(() => {
+        return Drone.collection.drop()
+    })
+    // insertar colección
+    .then(() => {
+        return Drone.insertMany(drones)
+    })
+    .then(drones => {
+        console.log('Se han creado', drones.length, 'drones')
+    })
+    // cierre
+    .finally(() => {
+        mongoose.connection.close()
+    })
+    .catch(err => console.log('Error en conexión'))
