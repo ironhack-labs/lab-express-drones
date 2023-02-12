0088bf64032232dfcbf8b1011d5a5084dcf52310
aldidjamil <alditamadjamil@gmail.com>
2023-02-12 09:27:29 +0100

    done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require("mongoose")
+
+const Drone = require('../models/Drone.model')
+
+require('../db/index')
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+
+Drone
+    .insertMany(drones)
+    .then(drones => {
+        console.log('This drones has been added to the Collection', drones)
+    })
+    .catch(e => console.log(e))
+    .finally(() => mongoose.connection.close())
