167edac0502df7f44992f9d3ca991f5af6a0847f
Farid-97 <faridsubtil@gmail.com>
2023-02-13 17:28:58 +0000

    Done minus css

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,14 @@
 // Iteration #1
+const { Schema, model } = require("mongoose");
+
+const droneSchema = new Schema(
+  {
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+  },
+  {
+    timeStamps: true,
+  }
+);
+module.exports = model("Drone", droneSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,34 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const MONGO_URI=
+process.env.MONGO_URI || "mongodb://127.0.0.1:27017/lab-express-drones"
+
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+async function seed() {
+  try {
+    await Drone.create(drones);
+    mongoose.disconnect();
+  } catch (err) {
+    console.log(err);
+  }
+}
+seed();
