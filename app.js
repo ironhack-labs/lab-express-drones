6935f5aac28b0ed4b1426d8cdf339542fd3d211e
Jizzus <miguel.angelo.jesus@hotmail.com>
2023-02-13 22:00:39 +0000

    some css added

--- app.js
+++ app.js
@@ -34,4 +34,6 @@ app.use('/', droneRoutes)
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
+
+
 module.exports = app;
--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,5 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('lab-express-drones JS imported successfully!');
 
 }, false);
+
+
