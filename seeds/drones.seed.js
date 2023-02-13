e634c647521d9c459ea1ab52f1076e757939fc23
Francisco Manuel Pereira <70414789+FranciscoManuelPereira@users.noreply.github.com>
2023-02-13 17:08:39 +0000

    Lab Done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,26 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model')
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  async function seed() {
+    try {
+       await mongoose.connect(MONGO_URI) 
+       let dbDrones = await Drone.create(drones)
+       console.log(`Created ${dbDrones.length} drones on the DB`)
+       mongoose.connection.disconnect();
+    } catch (error) {
+        console.log(error);
+    
+    }
+  }
+
+  seed();
