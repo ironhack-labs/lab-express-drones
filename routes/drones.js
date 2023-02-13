e4b22f88f2a7cb537a67af7aae24ee5775e869be
paulocaetano96 <paulocaetano96@gmail.com>
2023-02-13 16:50:36 +0000

    solved lab

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,102 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drones = require('../models/Drone.model')
 
-router.get('/drones', (req, res, next) => {
+router.get('/drones', async (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+try {
+  
+  let drones = await Drones.find();
+
+  res.render("drones/list", { drones })
+
+/* ---------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1---------------------- */
+
+} catch (error) {
+  console.log(error);
+  next(error);
+}
+
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render("drones/create-form")
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  try {
+    
+    const {name, propellers, maxSpeed} = req.body;
+
+    await Drones.create ({name, propellers, maxSpeed});
+
+    res.redirect('/drones')
+
+  } catch (error) {
+    console.log(error)
+    next(error);
+  }
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+try {
+  
+  const {id} = req.params;
+  const drones = await Drones.findById(id);
+  res.render("drones/update-form", drones)
+
+} catch (error) {
+  console.log(error)
+  next(error); 
+}
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+
+  try {
+    const {id} = req.params;
+    const {name, propellers, maxSpeed} = req.body;
+
+    await Drones.findByIdAndUpdate(id, req.body)
+ 
+    res.redirect(`/drones`)
+
+  } catch (error){
+    console.log(error)
+    next(error);
+  }
+
+
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete', async (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+try {
+  
+  const {id} = req.params;
+  const drones = await Drones.findByIdAndRemove(id)
+
+
+  res.redirect("/drones")
+  
+} catch (error) {
+  console.log(error)
+  next(error)
+}
+
+
 });
+
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,64 @@
 // Iteration #1
+
+/* 
+const mongoose = require('mongoose')
+const drones = require('../models/Drone.model')
+
+const MONGO_URI = 
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones"
+ */
+
+// Code provided in db/index.js
+
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose")
+
+const Drones = require("../models/Drone.model")
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+/* const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones"; */
+
+  const MONGO_URI = 
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones"
+
+
+    
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+
+// Array of the 3 requested drones
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+async function seed() {
+    try {
+            //to connect:
+        await mongoose.connect(MONGO_URI)
+
+        let dbDrones = await Drones.create(drones) 
+
+        console.log(`Created ${dbDrones.length} drones on the database`)
+
+        mongoose.connection.close()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+seed();
