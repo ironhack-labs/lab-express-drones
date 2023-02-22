ebbbd42a0087a9de40ae677ba2b4eafb1e602b46
Wilkins Morales <wilkinscom@gmail.com>
2023-02-22 20:08:26 +0200

    Iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,13 @@
 // Iteration #1
+
+const { Schema, model } = require("mongoose");
+
+const droneSchema = new Schema({
+  name: String,
+  propellers: Number,
+  maxSpeed: Number,
+});
+
+const Drone = model("Drone", droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,32 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .set("strictQuery", false)
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`);
+
+      Drone.deleteMany({})
+        .then(Drone.create(droneSeed)
+          .then(data => {
+            console.log(`${data.length} drones created successfully!`);
+            mongoose.connection.close();
+            console.log("DB connection closed!");
+                        })
+              )
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+const droneSeed = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
