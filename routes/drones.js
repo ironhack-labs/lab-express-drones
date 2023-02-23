63df64b434293da3cf736c6cf593772f0429e4b1
Subarna Paul Vignarajah <subarnapaul@rocketmail.com>
2023-02-23 08:31:12 +0000

    solved lab

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,63 @@
 const express = require('express');
 const router = express.Router();
-
+const Drone = require('../models/Drone.model.js')
 // require the Drone model here
-
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
-});
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  console.log(req.body);
+  const {name, propellers, maxSpeed} = req.body;
+  Drone.create({name, propellers, maxSpeed})
+  .then(()=> res.redirect('/drones'))
+  .catch(error => next(error))
+  
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params;
+  Drone.findById(id)
+  .then(droneToEdit =>{
+    console.log(droneToEdit);;
+    res.render(`drones/update-form.hbs`,{drone:droneToEdit})
+  })
+  .catch(error=> next(error))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id}= req.params;
+  const {name, propellers, maxSpeed} = req.body
+  Drone.findByIdAndUpdate(id, {name,propellers,maxSpeed},{new:true})
+  .then(updatedDrone => res.redirect(`/drones`))
+  .catch(error=>next(error))
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const {id}= req.params;
+  Drone.findByIdAndDelete(id)
+  .then(()=>res.redirect('/drones'))
+  .catch(error=>next(error))
+
+});
+
+router.get('/drones', (req, res, next) => {
+  // Iteration #2: List the drones
+  Drone.find()
+  .then(allTheDronesFromDB=>{
+    console.log(`Retrieved drones from DB:`, allTheDronesFromDB)
+    res.render(`drones/list.hbs`,{drones: allTheDronesFromDB});
+  })
+  .catch(err=>{
+    console.log(`Error while getting drones from DB`, err);
+    next(err)  
+  })
 });
 
 module.exports = router;
