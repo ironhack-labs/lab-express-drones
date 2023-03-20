bcb0384256e6559a038e8d18fe03516684ab5433
Sotiris Ntokas <ntokas1995@live.com>
2023-03-20 16:54:41 +0100

    complete Iteration 1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,36 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/lab-express-drones";
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+
+    //return Book.deleteMany({}); //WARNING: this will delete all books in your DB !!
+
+  })
+  .then( (response) => {
+    console.log(response);
+
+    return Drone.insertMany(drones);
+  })
+  .then(dronesFromDb => {
+    console.log(`Created ${drones.length} drones`);
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.error("Error connecting to DB: ", err);
+  });
+
