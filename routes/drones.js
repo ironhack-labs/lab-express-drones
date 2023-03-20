9db282f4b0e090d353b54e715b7a85135e812ae2
drose123456 <acg_danial@hotmail.com>
2023-03-20 16:16:05 +0000

    First Second and Third Iteration Completed

--- routes/drones.js
+++ routes/drones.js
@@ -1,25 +1,48 @@
 const express = require('express');
+const Drone = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
 
+// Iteration #2
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+		.then((dronesArr) => {
+			const data = { drones: dronesArr };
+			res.render("drones/list", data);
+		})
+		.catch((e) => {
+			console.log("error getting drone from DB", e);
+			next(e);
+		});
 });
 
+
+// Iteration #3
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form");
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  console.log(req)
+  const droneDetails = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  }
+
+  Drone.create(droneDetails)
+  .then(droneFromDB => {
+    res.redirect("/drones");
+  })
+  .catch(e => {
+    console.log("error", e)
+    next(e);
+  });
 });
 
+// Iteration #4
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
   // ... your code here
 });
 
