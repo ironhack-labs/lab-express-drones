d59d8c4348618c019134a29dca86eaa265f76b95
Bartosz Dubinski <bartas.de@gmail.com>
2023-03-20 17:43:11 +0100

    iteration 1 done, seed the database

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,6 +1,38 @@
 // Iteration #1
+
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+const Drone = require('../models/Drone.model');
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
 const drones = [
     { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
     { name: "Racer 57", propellers: 4, maxSpeed: 20 },
     { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
   ];
+
+
+mongoose
+.connect(MONGO_URI)
+.then((x) => {
+  console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+})
+.then( (response) => {
+    console.log(response);
+
+    return Drone.create(drones);
+  })
+  .then(dronesFromDB => {
+    console.log(`Created ${dronesFromDB.length} drones`);
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+.catch((err) => {
+  console.error("Error connecting to mongo: ", err);
+});
