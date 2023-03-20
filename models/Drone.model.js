768dd614de9e16170138770cc0d7e77ef437e7e4
DavidRamires911 <124219071+DavidRamires911@users.noreply.github.com>
2023-03-20 15:40:00 +0000

    iteraction1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,14 @@
-// Iteration #1
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const droneSchema= new Schema({
+name:{
+    type: String,
+    propellers: Number,
+    maxSpeed: Number
+},
+
+})
+
+
+module.exports= model("drone",droneSchema)
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,27 @@
-// Iteration #1
+const express = require('express');
+const Drone = require("../models/drone")
+const connectDB = require("../db/index");
+const { default: mongoose } = require('mongoose');
+
+require("dotenv").config()
+
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/lab-express-drones';
+
+
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+mongoose.connect(MONGO_URI ) 
+
+
+Drone.insertMany(drones)
+  .then((x)=>{
+      console.log(`Connected to database:"${x.connections[0].name}"`);
+  })
+  .catch((err)=>{
+      console.log("Errors in inserting drones:", err)
+       })
