9badbc75ebc123b189cb2e9693a4ca9688dccde6
andrehcarvalho6 <andre.hcarvalho1@gmail.com>
2023-03-20 18:42:34 +0000

    cant run it

--- routes/drones.js
+++ routes/drones.js
@@ -1,64 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Drone = require ("../models/Drone.model")
+const Drone = require("../models/Drone.model");
 
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   Drone.find()
-  .then((dronesArr) => {
-    console.log(dronesArr)
+    .then((dronesArr) => {
+      console.log(dronesArr);
 
-    const data = {
-      data : dronesArr
-    }
-    res.render("drones/list", data)
-  })
-  .catch(error => {
-    console.log("error display drones list" , error)
-  })
+      const data = {
+        data: dronesArr,
+      };
+      res.render("drones/list", data);
+    })
+    .catch((error) => {
+      console.log("error display drones list", error);
+    });
 });
 
-router.get('/drones/create', (req, res, next) => {
-
+router.get("/drones/create", (req, res, next) => {
   res.render("drones/create-form");
 });
 
-router.post('/drones', (req, res, next) => {
-  const droneDetails = 
-  {name: req.body.name,
-  propellers: req.body.proppelers,
-  maxSpeed: req.body.maxSpeed,}
+router.post("/drones", (req, res, next) => {
+  const droneDetails = {
+    name: req.body.name,
+    propellers: req.body.proppelers,
+    maxSpeed: req.body.maxSpeed,
+  };
 
   Drone.create(droneDetails)
-  .then(()=>{
-    console.log(droneDetails)
-    res.redirect("/drones");
-  })
-  .catch((error) => {
-    console.log("error creating new drone", error);
-    next(error);
-  });
+    .then(() => {
+      console.log(droneDetails);
+      res.redirect("/drones");
+    })
+    .catch((error) => {
+      console.log("error creating new drone", error);
+      next(error);
+    });
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
+  const { droneId } = req.params;
 
-  Drone.findById(req.params)
-    .then(droneToEdit => {
+  Drone.findById(droneId)
+    .then((droneToEdit) => {
       console.log("droneToEdit", droneToEdit);
 
-    res.render("drones/update-form.hbs", {drone: droneToEdit})
+      res.render("drones/update-form.hbs", { drone: droneToEdit });
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  const { droneId } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+  Book.findByIdAndUpdate(droneId, { name, propellers, maxSpeed }, { new: true })
+    .then((updatedDrone) => res.redirect(`/drones/${updatedDrone.id}`))
+    .catch((error) => next(error));
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+router.post("/drones/:id/delete", (req, res, next) => {
+  const { droneId } = req.params;
+  Drone.findByIdAndDelete(droneId)
+    .then(() => res.redirect("/drones"))
+    .catch((error) => next(error));
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -4,26 +4,24 @@ const Drone = require("../models/Drone.model");
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
 
-  const drones = [
-    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
-    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
-    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
-  ];
-
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
 
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
-      );
-    return Drone.create(drones)
+    );
+    return Drone.create(drones);
   })
   .then((dronesFromDB) => {
     console.log(`Created ${dronesFromDB.length} drones`);
 
-    
-   return mongoose.connection.close();
+    return mongoose.connection.close();
   })
   .catch((err) =>
     console.log(`An error occurred while creating drones from the DB: ${err}`)
