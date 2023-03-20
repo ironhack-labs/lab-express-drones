ab604d2f162de974f8a5b820678a3528ca5fb585
Christoph Weber <WeberChristoph85@gmail.com>
2023-03-20 18:23:47 +0100

    iteration 2 done

--- routes/drones.js
+++ routes/drones.js
@@ -2,21 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model');
+
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then(dronesArr => {
+
+      const data = {
+        drones: dronesArr
+      };
+
+      res.render("drones/list", data);
+    })
+    .catch(e => {
+      console.log("error getting drone from DB", e);
+      next(e);
+    });
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  router.get("/drones/create", (req, res, next) => {
+    res.render("drones/create-form");
+  });
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
