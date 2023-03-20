05f6d28d5bb6950896cd2798d2078c2392d12972
drose123456 <acg_danial@hotmail.com>
2023-03-20 15:05:09 +0000

    Some of First Iteration Implemented, Testing. Commit.

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+const Drone = require('../models/Drone.model');
+
+const drones = [
+    {
+        name: "Creeper XL 500",
+        propellers: 3,
+        maxSpeed: 12
+    },
+    {
+        name: "Racer 57",
+        propellers: 4,
+        maxSpeed: 20
+    },
+    {
+        name: "Courier 3000i",
+        propellers: 6,
+        maxSpeed: 18
+    }
+];
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
