cbc7348ad324bcf0902481e62a2dd1633dda2c6c
AZRGUI-MOHAMED <azrgui.m@gmail.com>
2023-04-15 16:19:09 +0200

    itteration 1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,22 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+// Connects to the database
+require("../db");
+
+Drone.create(drones) 
+.then((dronesFromDB)=>{
+    console.log(`Created ${dronesFromDB.length} drones.`)
+    return  mongoose.connection.close();
+        })
+.then(()=>console.log("DB connection closed"))
+.catch((error) => console.error("Error to connecting to the DB", error));
+  
+
