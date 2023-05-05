926a3be3d482a7401cfedfa06c506ee85ddd7d7b
Gustavo Gesto <gestogarciagustavo@gmail.com>
2023-05-05 17:14:00 +0200

    DONEEEEEEEEEEE

--- routes/drones.js
+++ routes/drones.js
@@ -1,34 +1,74 @@
 const express = require('express');
 const router = express.Router();
 
-// require the Drone model here
 
+const Drone = require('../models/Drone.model')
+
+
+// READ DRONES
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+
+  Drone
+    .find()
+    .then(allDrones => res.render('drones/list', { drones: allDrones }))
+    .catch(error => console.log(error));
+
 });
 
+// CREATE DRONES
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+
+  res.render("drones/create-form")
+
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+
+  const { name, propellers, maxSpeed } = req.body
+
+  Drone
+    .create({ name, propellers, maxSpeed })
+    .then(newDrone => res.redirect('/drones'))
+    .catch(err => {
+      // alert('TRY AGAIN')
+      res.redirect('/drones/create')
+    })
+
 });
 
+// EDIT DRONES
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+
+  const { id } = req.params
+
+  Drone
+    .findById(id)
+    .then(drone => res.render("drones/update-form", drone))
+    .catch(err => console.log(err))
+
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+
+  const { name, propellers, maxSpeed } = req.body
+  const { id } = req.params      // ID para .findByIdAndUpdate()
+
+  Drone
+    .findByIdAndUpdate(id, { name, propellers, maxSpeed })
+    .then(() => res.redirect('/drones'))
+    .catch(err => console.log(err))
+
 });
 
+// DELETE DRONES
 router.post('/drones/:id/delete', (req, res, next) => {
+
+  const { id } = req.params
+
+  Drone
+    .findByIdAndDelete(id)
+    .then(() => res.redirect(`/drones`))
+    .catch(err => console.log(err))
   // Iteration #5: Delete the drone
   // ... your code here
 });
