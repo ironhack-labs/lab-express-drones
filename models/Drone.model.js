d805c0e716a9343dcbf16d1572248a0c63e42408
JLMarse <martinezserrano.joseluis@gmail.com>
2023-05-05 20:32:05 +0200

    Done JLMS

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,18 @@
 // Iteration #1
+
+const { Schema, model } = require('mongoose');
+
+const droneSchema = new Schema(
+    {
+        name: {
+            type: String
+        },
+        propellers: Number,
+        maxSpeed: Number
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('Drone', droneSchema)
