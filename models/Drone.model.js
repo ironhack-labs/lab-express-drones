56d01d741e670df251f327c9d35a81af7d42959b
Gan-Dev <60898077+Gan-dev@users.noreply.github.com>
2023-05-05 19:35:19 +0200

    somethings

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,11 @@
 // Iteration #1
+const { Schema, model } = require('mongoose')
+
+const dronesSchema = new Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+
+module.exports = model("Drone", dronesSchema)
+
