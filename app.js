322374eb233cd6752993c1b436ab8f743e8630ef
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-05-05 19:28:39 +0200

    in process

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
+
 require('dotenv/config');
 
-// ℹ️ Connects to the database
 require('./db');
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require('express');
 
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
 
 const app = express();
