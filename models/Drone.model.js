0a328ae8f320e508c5d61292e97a7f85f052c17f
Belen Sanchez <belensezrami@gmail.com>
2023-05-05 17:57:09 +0200

    lab done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,17 @@
 // Iteration #1
+
+const { Schema, model } = require('mongoose');
+
+const droneSchema = new Schema(
+    {
+        name: { type: String },
+        propellers: { type: Number },
+        maxSpeed: { type: Number },
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Drone = model('Drone', droneSchema);
+module.exports = Drone;
