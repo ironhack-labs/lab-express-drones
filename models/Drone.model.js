dfcddb00a644bf3041ec5d4cdc78ced4fe484b8d
Victor Villalba <villalbabesada@gmail.com>
2023-05-05 17:58:14 +0200

    first commit

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,16 @@
-// Iteration #1
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        name: { type: String },
+        propellers: { type: Number },
+        maxSpeed: { type: Number },
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Drone = model("Drone", userSchema);
+
+module.exports = Drone;
