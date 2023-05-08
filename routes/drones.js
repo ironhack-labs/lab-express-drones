fae1e190f1eb149cae83ff8addaeb551733703ab
Lucas Telles <lltellesss@gmail.com>
2023-05-08 18:41:01 -0300

    lab done

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,100 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model.js');
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  async function findAllDronesFromDb() {
+    try {
+      // Find all the drones inside the collection
+      let allDronesFromDb = await Drone.find()
+      //Feedback regarding to found drones
+      // console.log('Retrieved drones from DB:' , allDronesFromDb)
+      res.render('drones/list.hbs', { drones: allDronesFromDb })
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  findAllDronesFromDb()
+
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+
+  const { name, propellers, maxSpeed } = req.body;
+
+  async function createDroneInDb() {
+    try {
+      // Creating drone in DB
+      let createDrone = await Drone.create({ name, propellers, maxSpeed })
+      // Feedback regarding the drone creation
+      console.log(`Created drone: ${createDrone.name}`)
+      res.redirect('/drones')
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  createDroneInDb()
 });
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { droneId } = req.params
+  console.log(droneId)
+
+  async function findInfoFromADrone() {
+    try {
+      let droneToEdit = await Drone.findById(droneId)
+      res.render('drones/update-form.hbs', { drone: droneToEdit })
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  findInfoFromADrone()
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { droneId } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+
+  async function updateADroneFromDb() {
+    try {
+      let updateDrone = await Drone.findByIdAndUpdate(droneId, { name, propellers, maxSpeed }, { new: true })
+      res.redirect(`/drones/${updateDrone._id}`)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  updateADroneFromDb()
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { droneId } = req.params;
+
+  async function DeleteADroneFromDb() {
+    try {
+      let deletedDrone = await Drone.findByIdAndDelete(droneId);
+      res.redirect('/drones')
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  DeleteADroneFromDb()
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,33 @@
 // Iteration #1
+//Require mongoose
+const mongoose = require('mongoose')
+
+//Require drone model
+const Drone = require('../models/Drone.model.js')
+
+// Establish connection
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+async function createDrones() {
+    try {
+        let db = await mongoose.connect(MONGO_URI);
+        // Feedback about connection
+        // console.log(`Connected to MongoDB: ${db.connections[0].name}`)
+        // Create new documents inside drones collection
+        let dronesCreated = await Drone.create(drones)
+        // Feedback about the drone creation
+        // console.log(`Created ${dronesCreated.length} drones!`)
+        // Closing the connection
+        await mongoose.connection.close();
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+createDrones()
