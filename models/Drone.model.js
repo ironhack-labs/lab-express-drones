65ef6bf385c5154b8f4f8b4ff247afd3fb1c3b95
avelarvicky <victoriahelenaavelar@gmail.com>
2023-05-08 17:43:03 +0100

    Lab done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+const { Schema, model } = require("mongoose");
+
+const droneSchema = new Schema(
+  {
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+    image: String,
+  },
+  { timestamps: true }
+);
+
+const Drone = model("Drone", droneSchema);
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,34 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+
+const Drone = require("../models/Drone.model.js");
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+async function createDrones() {
+    try {
+      let db = await mongoose.connect(MONGO_URI);
+  
+      // feedback about the connection
+      console.log(`Connected to MongoDB:${db.connections.name}`);
+  
+      // create new document inside drones collection
+      let dronesCreated = await Drone.create(drones);
+  
+      // feedback regarding books creation
+      console.log(`Created ${dronesCreated.length} drones!`);
+  
+      // closing the connection
+      await mongoose.connection.close();
+    } catch (error) {
+      console.log("Error while connecting to Db", error);
+    }
+  }
+  createDrones();
