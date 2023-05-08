d4f991ac762cfd56f965b7e69b72d0d44fc2bd7f
Margarida Pereira <map.margaridapereira@gmail.com>
2023-05-08 18:08:47 +0100

    mandatory lab done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,14 @@
 // Iteration #1
+
+// Require Schema and model methods of mongoose
+const {Schema, model} = require('mongoose');
+
+const droneSchema = new Schema(
+    {
+        name: String,
+        propellers: Number,
+        maxSpeed: Number
+    }
+);
+
+module.exports = model("Drone", droneSchema);
