912de14d7a32c93b782ea548284ad3ec6a9c9064
Ola Bonati <olabonati@gmail.com>
2023-06-05 22:47:20 +0200

    stuck on database population

--- db/index.js
+++ db/index.js
@@ -6,11 +6,11 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
-
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    const databaseName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${databaseName}"`);
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
