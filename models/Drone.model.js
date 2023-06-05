182866ee39aa59294a8652412cef1629edfb99c5
hymced <hymced@gmail.com>
2023-06-05 23:16:28 +0200

    it1 seed

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,11 @@
-// Iteration #1
+const { Schema, model } = require('mongoose');
+
+const droneSchema = new Schema(
+  {
+    title: String,
+    propellers: Number,
+    maxSpeed: Number,
+  },
+);
+
+module.exports = model("Drone", droneSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,38 @@
-// Iteration #1
+
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', true)
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+const droneModel = require("../models/Drone.model")
+
+const drones = [
+	{ name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+	{ name: "Racer 57", propellers: 4, maxSpeed: 20 },
+	{ name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .then(() => {
+	return droneModel.deleteMany({});
+  })
+  .then((valueObj) => {
+	console.log("Number of drones deleted: ", valueObj.deletedCount)
+	return droneModel.create(drones)
+  })
+  .then(dronesArrFromDB => {
+	console.log("Number of drones created: ", dronesArrFromDB.length);
+    // mongoose.connection.close();
+	mongoose.disconnect()
+  })
+  .catch((err) => {
+    console.error("Error seeding: ", err);
+  });
