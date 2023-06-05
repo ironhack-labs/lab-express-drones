ac0ccfb25253ceb9e0c572b2a1eb418995b0d295
Philippe Miossec <pmiossec@gmail.com>
2023-06-05 17:28:58 +0200

    It1: Seed the database

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,11 @@
 // Iteration #1
+
+const {Schema, model } = require("mongoose")
+
+const schema = new Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+})
+
+module.exports = model("Drone", schema);
