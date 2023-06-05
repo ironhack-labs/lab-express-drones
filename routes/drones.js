04afa242f3f6f710adacc455c6b7d4d31c3e0662
Maria Friedemann <friedemann.maria@gmail.com>
2023-06-05 22:18:09 +0200

    lab incomplete, done until iteration 3, but 2 not complete

--- routes/drones.js
+++ routes/drones.js
@@ -1,21 +1,61 @@
 const express = require('express');
 const router = express.Router();
 
+const Drone = require('../models/Drone.model');
+
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+    Drone.find()
+    .then((result) => {
+      console.log(`This is the result: ${result}`);
+    })
+    .catch()
+  Drone.find()
+  .then((dronesFromDB) => {
+
+    const data = {
+  drones: dronesFromDB  
+    };
+
+    res.render("drones/list", data);
+  })
+
+  
+  .catch ( (err) => {
+    console.log("error getting list of books from DB", err);
+    next(err);
+  });
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form")
+
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const newDrone = {
+    name:req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed,
+}
+
+
+Drone.create(newDrone)
+    .then( (newDrone) => {
+        console.log(newDrone);
+
+        res.redirect("/drones") 
+
+    })
+    .catch( e => {
+        console.log("error creating new book", e);
+        next(e);
+    });
+
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
