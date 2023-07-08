a0b7f1ed353d58501181a306893f4fe8fbbea5cb
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-07-08 11:52:02 +0200

    Iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,32 @@
 // Iteration #1
+const mongoose = require('mongoose');
+
+const REQUIRED_ERROR = 'Required field';
+
+// Schema
+
+const DroneSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, REQUIRED_ERROR],
+    },
+    propellers: {
+      type: Number,
+      required: [true, REQUIRED_ERROR],
+    },
+    maxSpeed: {
+        type: Number,
+        required: [true, REQUIRED_ERROR],
+      },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// Model
+
+const Drone = mongoose.model('Drone', DroneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,27 @@
 // Iteration #1
+
+require('../db/index');
+
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+const drones = require('../drones.json');
+
+mongoose.connection.once('open', () => {
+    mongoose.connection.dropCollection('drones')
+      .then(() => {
+        console.log('DB cleared');
+  
+        return Drone.create(drones)
+      })
+      .then(dronesDB => {
+        dronesDB.forEach(drone => {
+          console.log(`${drone.name} has been created`)
+        })
+  
+        console.log(`${dronesDB.length} games have been created`);
+      })
+      .catch(err => console.error(err))
+      .finally(() => {
+        mongoose.disconnect();
+      })
+  })
