1b528784f74b9b400e00eccf8385bb105048fd20
Juan Cruz <j.cruzb@outlook.com>
2023-07-11 16:12:20 +0200

    update

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,28 @@
 // Iteration #1
+require ('../db');
+
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+const drones = require('../data/drones.json');
+
+mongoose.connection.once('open', () => {
+    mongoose.connection.db.dropDatabase()
+        .then(() => {
+            console.log('DB cleared');
+            return Drone.create(drones);
+        }
+        )
+        .then((dronesDB) => {
+            dronesDB.forEach((drone) => {
+                console.log(`${drone.name} created`);
+            });
+        })
+        .catch((err) => console.log(`An error occurred while creating drones from the DB: ${err}`))
+        .finally(() => {
+            mongoose.disconnect();
+        });
+
+});
+
+
+
