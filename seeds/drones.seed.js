0b23f0deac4133f3a32695b249e182ebf9bf9223
Claudia Alvarez Posada <daibhaith@hotmail.com>
2023-08-04 20:51:50 +0200

    Done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,36 @@
+const { default: mongoose } = require("mongoose");
+
 // Iteration #1
+const Drones = require('../models/Drone.model')
+
+const drones = [
+
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+const connectionString = 'mongodb://127.0.0.1:27017/drones-database'
+
+mongoose
+
+    .connect(connectionString)
+    .then(x => {
+        console.log(`Conectado a Mongo database: "${x.connections[0].name}"`)
+        return Drones.create(drones)
+    })
+
+    .then(dronesFromDB => {
+        console.log(`Created ${dronesFromDB.length} drones`)
+        return mongoose.connection.close
+    })
+
+    .then(() => {
+
+        console.log('DB connection close')
+
+    })
+
+    .catch(err => {
+        console.log(`An error occurred while creating books from the DB: ${err}`)
+    })
