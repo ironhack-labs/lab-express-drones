528b76cad9bee1799deebf75bdf34c9f3ab6fc4f
FERGAZA <fernandomgzapata@gmail.com>
2023-08-06 17:06:11 +0200

    Task done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,28 @@
 // Iteration #1
+
+const mongoose = require('mongoose')
+const Drone = require('./../models/Drone.model')
+
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+const databaseUrl = 'mongodb://127.0.0.1:27017/lab-express-drones'
+
+mongoose
+    .connect(databaseUrl)
+    .then((x) => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+        return Drone.create(drones)
+    })
+    .then(dronesFromDB => {
+        console.log(`Created ${dronesFromDB.length} drones`)
+        return mongoose.connection.close()
+    })
+    .then(() => console.log('Db  connection closed!'))
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    });
