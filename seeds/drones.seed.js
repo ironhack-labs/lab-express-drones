a64da63de710870fbfe7673c58a3009135e91139
H1V35 <higesjorge@gmail.com>
2023-08-06 22:33:25 +0200

    Done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,32 @@
-// Iteration #1
+const mongoose = require('mongoose')
+
+const Drone = require('./../models/Drone.model')
+
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+  { name: 'SpeedTronicDrone', propellers: 3, maxSpeed: 10 },
+  { name: 'UltraTurboSpeedTronicDrone5000', propellers: 10, maxSpeed: 50 },
+  { name: 'MegaSonicUltraTurboSpeedTronicDrone10000', propellers: 20, maxSpeed: 80 },
+  { name: 'NanoDrone', propellers: 3, maxSpeed: 33 },
+]
+
+const connectionString = 'mongodb://127.0.0.1:27017/lab-express-drones'
+
+mongoose
+  .connect(connectionString)
+  .then((x) => {
+    console.log(`Connected to Mongo database: "${x.connections[0].name}"`)
+    return Drone.create(drones)
+  })
+  .then((dronesFromDB) => {
+    console.log(`Created: ${dronesFromDB}`)
+    return mongoose.connection.close()
+  })
+  .then(() => {
+    console.log('DB connection closed!')
+  })
+  .catch((err) => {
+    console.log(`An error occurred while creating books from the DB: ${err}`)
+  })
