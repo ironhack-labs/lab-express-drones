f9261133ab76161cbef0a6eb25ad6cb4517079af
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-08-07 17:17:25 +0100

    Lab-Done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,28 @@
 // Iteration #1
+const mongoose = require('mongoose')
+
+const Drone = require('../models/Drone.model.js')
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+async function insertDrones(){
+    try {
+        let db = await mongoose.connect(MONGO_URI)
+        console.log('DataBase is Connected')
+        let dronesCreated = await Drone.create(drones)
+        console.log(`Created ${dronesCreated.length} drones!`)
+
+        await mongoose.connection.close();
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+insertDrones();
