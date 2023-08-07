dc98efda19885108b4ba13b1c66bc1708bb9f345
Filipa Flora <filipa.flora@gmail.com>
2023-08-07 17:31:25 +0100

    Lab solved

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use('/', index);
 const droneRoutes = require('./routes/drones')
 app.use('/', droneRoutes)
 
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,38 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+// Require Book Model
+const Drone = require("../models/Drone.model.js");
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+async function insertDrones() {
+  try {
+    // establish the connection with our DB
+    let db = await mongoose.connect(MONGO_URI);
+
+    //feedback regarding our connection
+    console.log("Database is now connected");
+
+    // create drones in our db with the seeds array
+    let dronesCreated = await Drone.create(drones);
+
+    // feedback about drones creation
+    console.log(`Created ${dronesCreated.length} drones!`);
+
+    //Close the connection
+    await mongoose.connection.close();
+    
+  } catch (error) {
+    console.log("An error ocurred while connecting to DB", error);
+  }
+}
+
+insertDrones();
