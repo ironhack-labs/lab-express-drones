1bfc17c39d6d411e60c3ddd5450a9b3e4ffa4f4a
Mehdi Nader <mehdinader1079@gmail.com>
2023-08-08 09:10:11 +0100

    lab solved

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,50 @@
-// Iteration #1
+//Require Mongoose
+const mongoose = require ('mongoose');
+
+// require Model
+const Drone = require ('../models/Drone.model.js');
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+const drones = [
+    {
+        name:"Fly MEHDI 3000",
+        propellers: 6,
+        maxSpeed: 18,
+    },
+    {
+        name:"Zeca Galhão XZ-7 PLOP",
+        propellers: 4,
+        maxSpeed: 14,
+    },
+    {
+        name:"Bin Nader 9-11",
+        propellers: 8,
+        maxSpeed: 18,
+    },
+    
+]
+
+async function insertDrones(){
+    try{
+        // establishing connection with our DB
+        let db = await mongoose.connect(MONGO_URI);
+
+        // feedback regard our connection
+        console.log("Database is now connected");
+
+        // create books in our db with the seeds array
+        let dronesCreated = await Drone.create(drones);
+
+        // feedback about books creation
+       console.log(`Created ${dronesCreated.length} drones!`)
+
+        //Close the connection
+        await mongoose.connection.close();
+        
+        }
+    catch (error){
+        console.log("An error ocurred while connecting to DB, error", error)
+    }
+}
+insertDrones()
