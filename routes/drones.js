1b0285130572109f37d269834dd0c9a34fc77445
maureen-treacy <mtreacy07@gmail.com>
2023-08-08 08:01:08 +0100

    [LIS-FT-JULY23] maureen_treacy

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,77 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model.js");
 
-router.get('/drones', (req, res, next) => {
+router.get('/drones', async (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+try{
+  let allDronesfromDB = await Drone.find()
+  res.render("drones/list.hbs", {drones: allDronesfromDB})
+}
+catch(error){
+  console.log(error)
+}
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+ res.render("drones/create-form.hbs")
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  try{
+    const { name, propellers, maxSpeed } = req.body
+    await Drone.create({name, propellers, maxSpeed })
+    res.redirect("/drones")
+  }
+  catch(error){
+    console.log(error)
+  }
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+//Note for TA: I'm getting a 404 error when I try and save the changes to editing a drone. What am I doing wrong?
+
+router.get('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  try {
+    const { id } = req.params;
+    let foundDrone = await Drone.findById(id)
+    res.render("drones/update-form.hbs",foundDrone);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  try{
+    //Destructure the req.params object to get the droneId
+    const {id} = req.params
+    const {name, propellers, maxSpeed} = req.body
+
+    //update the same document with new content
+    await Drone.findByIdAndUpdate(id, {name, propellers, maxSpeed}, {new:true})
+
+    //redirect to books list page
+    res.redirect("/drones")
+}
+catch(error){
+    console.log(error)
+}
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete', async (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+try{
+  const {id} = req.params
+   await Drone.findByIdAndDelete(id)
+   res.redirect("/drones")
+}
+catch(error){
+  console.log(error)
+}
 });
 
 module.exports = router;
