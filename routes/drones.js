eeeeedf4984fe59efdbca16739f745d97d52d842
VanyaMark <markova.vanya@gmail.com>
2023-08-08 08:54:38 +0200

    Iteration 4 updating a drone done - get and post route and an updare form

--- routes/drones.js
+++ routes/drones.js
@@ -34,12 +34,30 @@ router.post('/drones/create', (req, res, next) => {
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params;
+  Drone.findById(id)
+    .then((droneToUpdate) => {
+      res.render('drones/update-form.hbs', {droneToUpdate})
+    })
+    .catch(err => {
+      console.log('Error get route updating drone: ', err)
+      next(err)
+    })
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params;
+  const {name, propellers, maxSpeed} = req.body;
+  Drone.findByIdAndUpdate(id, {name, propellers, maxSpeed}, {new: true})
+    .then((updatedDrone) => {
+      console.log('Updated drone: ', updatedDrone)
+      res.redirect('/drones')
+    })
+    .catch(err => {
+      console.log('Error updating a drone: ', err)
+      next(err)
+    })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
