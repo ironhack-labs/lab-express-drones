c9e8ce3e4193bcd3bdae9d3fe86d4cc21398d2b4
Fabio García <fabiogcaam@gmail.com>
2023-11-03 17:50:55 +0100

    lab-solved

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,30 @@
 // Iteration #1
+const mongoose = require('mongoose')
+
+const Drone = require('../models/Drone.model')
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+]
+
+const connectionString = 'mongodb://127.0.0.1:27017/lab-express-drones'
+
+
+mongoose
+    .connect(connectionString)
+    .then(x => {
+        console.log(`Connected to Mongo database: "${x.connections[0].name}"`)
+        return Drone.create(drones)
+    })
+    .then(dronesFromDB => {
+        console.log(`Created ${dronesFromDB.length} drones`)
+        return mongoose.connection.close()
+    })
+    .then(() => {
+        console.log('DB connection closed!')
+    })
+    .catch(err => {
+        console.log(`An error occurred while creating drones from the DB: ${err}`)
+    })
