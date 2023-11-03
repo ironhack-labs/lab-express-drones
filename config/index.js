64de392f168baa3c8f5f6ee08d4e118b94738d83
Lucia Sanz <lu.sanz.galvez@gmail.com>
2023-11-03 18:57:15 +0100

    dooone

--- config/index.js
+++ config/index.js
@@ -1,39 +1,37 @@
-// We reuse this import in order to have access to the `body` property in requests
-const express = require("express");
 
-// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
-// https://www.npmjs.com/package/morgan
-const logger = require("morgan");
+const express = require("express")
 
-// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
-// https://www.npmjs.com/package/cookie-parser
-const cookieParser = require("cookie-parser");
 
-// ℹ️ Serves a custom favicon on each request
-// https://www.npmjs.com/package/serve-favicon
-const favicon = require("serve-favicon");
+const logger = require("morgan")
 
-// ℹ️ global package used to `normalize` paths amongst different operating systems
-// https://www.npmjs.com/package/path
-const path = require("path");
 
-// Middleware configuration
+const cookieParser = require("cookie-parser")
+
+
+const favicon = require("serve-favicon")
+
+
+const path = require("path")
+
+
 module.exports = (app) => {
-  // In development environment the app logs
-  app.use(logger("dev"));
+
+  // use y set son metodos de app, por eso lo hago aqui dentro
+
+  app.use(logger("dev"))
 
   // To have access to `body` property in the request
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(cookieParser())
 
-  // Normalizes the path to the views folder
-  app.set("views", path.join(__dirname, "..", "views"));
-  // Sets the view engine to handlebars
-  app.set("view engine", "hbs");
-  // Handles access to the public folder
-  app.use(express.static(path.join(__dirname, "..", "public")));
+  
+  app.set("views", path.join(__dirname, "..", "views"))
 
-  // Handles access to the favicon
+  app.set("view engine", "hbs")
+
+  app.use(express.static(path.join(__dirname, "..", "public")))
+
+
   app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
-};
+}
--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,73 @@
 const express = require('express');
 const router = express.Router();
 
-// require the Drone model here
+const Drone = require('./../models/Drone.model')
 
+//listado de drones
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
-});
+  
+  Drone
+    .find()
+    .then(drones => res.render('drones/list', {drones}))
+    .catch(err => console.log(err))
 
+})
+
+
+// creaccion de drone (from)
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
+    res.render('drones/create-form')
+
+})
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
+console.log("--------ESTO ES LO QUE TE METE EL USUARIO EN EL FROMULARIO------", req.body)
+
+// const name = req.body.name
+// const propellers = req.body.propellers
+// const maxSpeed = req.body.maxSpeed
+
+     const { name , propellers, maxSpeed} = req.body
+
+      Drone
+      .create({name: name , propellers: propellers, maxSpeed: maxSpeed}) //hay que respetar la estructura del modelo
+      .then(() => res.redirect('/drones'))
+      .catch(err => res.redirect('/drones'))
+  
+})
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
+  
+  const {id} = req.params
+
+  Drone
+  .findById(id)
+  .then(drones => res.render ('drones/update-form', drones))
+  .catch(err => console.log(err))
+
+})
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
+  
+  const {name, propellers, maxSpeed} = req.body
+  const {id} = req.params
+
+  Drone
+  .findByIdAndUpdate(id, {name, propellers, maxSpeed})
+  .then(()=> res.redirect('/drones'))     // a redirect le tengo que pasar solo la url a la que tiene que ir
+  //.catch(err => console.log(err))
+  .catch(err => res.redirect('/drones'))
+})
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
-});
+  
+  const {id} = req.params
+
+    Drone
+    .findByIdAndDelete(id)
+    .then(()=> res.redirect('/drones'))
+    .catch(err => res.redirect('/drones'))
+
+})
 
 module.exports = router;
