bf09ef5c9c28b6e695c4e6eadf5437733f1a8931
Tania Zubrienko <tatiana.zubrienko@gmail.com>
2023-11-03 16:18:20 +0100

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
-// Iteration #1
+const { Schema, model, default: mongoose } = require('mongoose')
+
+const droneSchema = mongoose.Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+},
+    {
+        timestamps: true
+    })
+
+module.exports = model("Drone", droneSchema)
