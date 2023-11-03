a4065585221b29999139e142eccf124e0209b7f9
alvaro sanchez <alvaro_piro_@hotmail.com>
2023-11-03 16:42:10 +0100

    finish

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,17 @@
-// Iteration #1
+const { Schema, model } = require('mongoose')
+
+const droneSchema = new Schema(
+    {
+        name: String,
+        propellers: Number,
+        maxSpeed: {
+            type: Number,
+            max: 2000
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+module.exports = model('Drone', droneSchema)
+
