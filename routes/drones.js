b24d900c512d13e00678024a6045dd10a05526d3
Pablo Portillo <paabloportillo@gmail.com>
2023-11-03 17:49:35 +0100

    Lab Solved

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
-// require the Drone model here
+const Drone = require('./../models/Drone.model');
+const DroneModel = require('./../models/Drone.model');
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  Drone
+    .find()
+    .then(drone => {
+      res.render('drones/list', { drone })
+    })
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form')
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
+  const { name, propellers, maxSpeed } = req.body
+  Drone
+    .create({ name, propellers, maxSpeed })
+    .then(() => res.redirect('/drones'))
+    .catch(() => res.render('drones/create-form'))
+})
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params
+
+  Drone
+    .findById(id)
+    .then(drone => {
+      res.render('drones/update-form', drone)
+    })
+    .catch(error => console.log(error))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body
+  const { id } = req.params
+  Drone
+    .findByIdAndUpdate(id, { name, propellers, maxSpeed })
+    .then(() => res.redirect('/drones'))
+    .catch(() => res.redirect(`/drones/${id}/edit`))
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params
+  Drone
+    .findByIdAndDelete(id)
+    .then(() => res.redirect('/drones'))
+    .catch(error => console.log(error))
 });
 
 module.exports = router;
