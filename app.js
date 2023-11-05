5417bb0e85e2a02a2e97104e447b0805eb5e00bb
ricardo <richu1094@gmail.com>
2023-11-05 18:24:28 +0100

    ≽^•⩊•^≼

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
-require('dotenv/config');
+require("dotenv/config");
+require("./db");
 
-// ℹ️ Connects to the database
-require('./db');
-
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
-const express = require('express');
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
-const hbs = require('hbs');
-
+const express = require("express");
+const hbs = require("hbs");
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+require("./config")(app);
 
-// default value for title local
-const projectName = 'lab-express-drones';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+app.locals.title = `Drones`;
 
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+const index = require("./routes/index");
+app.use("/", index);
 
-// 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+const droneRoutes = require("./routes/drones");
+app.use("/", droneRoutes);
 
-const droneRoutes = require('./routes/drones')
-app.use('/', droneRoutes)
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
+require("./error-handling")(app);
 
 module.exports = app;
--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// require the Drone model here
+const Dron = require("./../models/Drone.model");
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+router.get("/drones", (req, res, next) => {
+  Dron.find()
+    .then((foundDrones) => {
+      res.render("drones/list", { foundDrones });
+    })
+    .catch((err) => console.log(err));
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.get("/drones/create", (req, res, next) => {
+  res.render("drones/create-form");
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post("/drones/create", (req, res, next) => {
+  const { name, propellers, maxSpeed } = req.body;
+  Dron.create({ name, propellers, maxSpeed })
+    .then(() => res.redirect("/drones"))
+    .catch((err) => console.log(err));
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.get("/drones/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  Dron.findById(id)
+    .then((dron) => res.render("drones/update-form", dron))
+    .catch((err) => console.log(err));
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post("/drones/:id/edit", (req, res, next) => {
+  const { name, propellers, maxSpeed } = req.body;
+  const { id } = req.params;
+  Dron.findByIdAndUpdate(id, { name, propellers, maxSpeed })
+    .then(() => res.redirect("/drones"))
+    .catch((err) => console.log(err));
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+router.post("/drones/:id/delete", (req, res, next) => {
+  const { id } = req.params;
+  Dron.findByIdAndDelete(id)
+    .then(() => res.redirect("/drones"))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
