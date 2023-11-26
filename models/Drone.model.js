426f8488d6ed1c49b890419a13eaecd7da6da91e
Sergio Esteban <Sergioss772022@gmail.com>
2023-11-27 00:18:53 +0100

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,19 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+
+const dronSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    propellers: {
+        type: Number
+    },
+    maxSpeed: {
+        type: Number
+    },
+});
+
+const Dron = mongoose.model('Dron', dronSchema);
+
+module.exports = Dron;
