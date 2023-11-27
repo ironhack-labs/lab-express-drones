b2db42f0b561d83f21ae4b43e213b0d36b9dbc3f
HisokaHX <luca.tobiello92@gmail.comgit config --global user.email luca.tobiello92@gmail.com>
2023-11-27 16:46:57 +0100

    [MAD-PT-WD-092023] Luca Tobiello

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,23 @@
 // Iteration #1
+const mongoose = require('mongoose');
+
+const droneSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String
+        },
+        propellers: {
+            type: Number
+        },
+        maxSpeed: {
+            type: Number
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Drone = mongoose.model('Drone', droneSchema);
+
+module.exports = Drone;
