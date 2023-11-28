448380d6c91b34d18c7cfa6bc66e3fdd775b5b92
Victor Junquera <victorjunquera@eventalista.es>
2023-11-28 16:58:58 +0100

    step 1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,34 @@
 // Iteration #1
+mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+const dronesData = require("../constants/drones.json");
+
+require("../db/index");
+
+mongoose.connection.once("connected", () => {
+  console.log("Seeds connected to DB");
+
+  mongoose.connection
+    .dropDatabase()
+    .then(() => {
+      console.log("DB has been cleared");
+    })
+    .then(() => {
+      return Drone.create(dronesData);
+    })
+    .then((dronesDB) => {
+      dronesDB.forEach((drone) => console.log(drone));
+    })
+    .catch((err) => console.error(err))
+    .finally(() => {
+      mongoose.connection
+        .close()
+        .then(() => {
+          console.log("End of seeds");
+        })
+        .catch((err) => console.error(err))
+        .finally(() => {
+          process.exit(0);
+        });
+    });
+});
