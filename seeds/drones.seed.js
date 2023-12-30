5107fcdb8f3d6cfa98ace61f54459e5299612a23
Ana Zamora <anaa.zamoran@gmail.com>
2023-12-30 20:37:24 +0100

    finished

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,27 @@
-// Iteration #1
+require('dotenv').config()
+const mongoose = require('mongoose')
+const Drone = require('../models/Drone.model')
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    })
+    .then(() => {
+    return Drone.create(drones)
+    })
+    .then( data => {
+    console.log('Se han creado', data.length, 'drones')
+    })
+    .finally(() => {
+        mongoose.connection.close()
+    })
+    .catch(err => console.log(err))
